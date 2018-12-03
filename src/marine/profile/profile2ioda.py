c82d53a9485d7a27440f92938f3766ae3e3c19ca
Rahul Mahajan <aerorahul@users.noreply.github.com>
2018-12-02 23:20:39 -0500

    make profile a class; clean-up interface; make interactive
    Change-Id: Ie6324eb7aea0d2e229c788c50eb270ff0d575933

--- src/marine/profile/profile2ioda.py
+++ src/marine/profile/profile2ioda.py
@@ -1,199 +1,246 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-import os
-import sys
 import numpy as np
+from datetime import datetime
 from scipy.io import FortranFile
 from netCDF4 import Dataset
-
-def rd_prof(fname, datetime='YYYYMMDDHH'):
-
-    try:
-        fh = FortranFile(fname, mode='r', header_dtype='>i4')
-    except IOError:
-        raise IOError('%s file not found!' % fname)
-    except Exception:
-        raise Exception('Unknown error opening %s' % fname)
-
-    # Dictionary to hold all data from file
-    odata = {}
-
-    odata['datetime'] = datetime
-
-    odata['n_obs'], odata['n_lvl'], odata['n_vrsn'] = fh.read_ints('>i4')
-
-    print('    number profiles: %d' % odata['n_obs'])
-    print('  max number levels: %d' % odata['n_lvl'])
-    print('file version number: %d' % odata['n_vrsn'])
-
-    if odata['n_obs'] <= 0:
-        print('No profile observations to process from %s' % fname)
-        return odata
-
-    odata['ob_btm'] = fh.read_reals('>f4')
-    odata['ob_lat'] = fh.read_reals('>f4')
-    odata['ob_lon'] = fh.read_reals('>f4')
-    odata['ob_ls'] = fh.read_reals('>i4')
-    odata['ob_lt'] = fh.read_reals('>i4')
-    odata['ob_ssh'] = fh.read_reals('>f4')
-    odata['ob_sst'] = fh.read_reals('>f4')
-    odata['ob_sal_typ'] = fh.read_reals('>i4')
-    odata['ob_sal_qc'] = fh.read_reals('>f4')
-    odata['ob_tmp_typ'] = fh.read_reals('>i4')
-    odata['ob_tmp_qc'] = fh.read_reals('>f4')
-
-    odata['ob_lvl'] = []
-    odata['ob_sal'] = []
-    odata['ob_sal_err'] = []
-    odata['ob_sal_prb'] = []
-    odata['ob_tmp'] = []
-    odata['ob_tmp_err'] = []
-    odata['ob_tmp_prb'] = []
-
-    for n in range(odata['n_obs']):
-        odata['ob_lvl'].append(fh.read_reals('>f4'))
-        odata['ob_sal'].append(fh.read_reals('>f4'))
-        odata['ob_sal_err'].append(fh.read_reals('>f4'))
-        odata['ob_sal_prb'].append(fh.read_reals('>f4'))
-        odata['ob_tmp'].append(fh.read_reals('>f4'))
-        odata['ob_tmp_err'].append(fh.read_reals('>f4'))
-        odata['ob_tmp_prb'].append(fh.read_reals('>f4'))
-
-    odata['ob_dtg'] = fh.read_record('>S12').astype('U12')
-    odata['ob_rcpt'] = fh.read_record('>S12').astype('U12')
-    odata['ob_scr'] = fh.read_record('>S1').astype('U1')
-    odata['ob_sign'] = fh.read_record('>S7').astype('U7')
-
-    odata['ob_clm_sal'] = []
-    odata['ob_clm_tmp'] = []
-    odata['ob_clm_ssd'] = []
-    odata['ob_clm_tsd'] = []
-    odata['ob_glb_sal'] = []
-    odata['ob_glb_tmp'] = []
-    odata['ob_glb_ssd'] = []
-    odata['ob_glb_tsd'] = []
-    odata['ob_mds_sal'] = []
-    odata['ob_mds_tmp'] = []
-    odata['ob_rgn_sal'] = []
-    odata['ob_rgn_tmp'] = []
-    odata['ob_rgn_ssd'] = []
-    odata['ob_rgn_tsd'] = []
-
-    for n in range(odata['n_obs']):
-        odata['ob_clm_sal'].append(fh.read_reals('>f4'))
-        odata['ob_clm_tmp'].append(fh.read_reals('>f4'))
-        odata['ob_clm_ssd'].append(fh.read_reals('>f4'))
-        odata['ob_clm_tsd'].append(fh.read_reals('>f4'))
-        odata['ob_glb_sal'].append(fh.read_reals('>f4'))
-        odata['ob_glb_tmp'].append(fh.read_reals('>f4'))
-        odata['ob_glb_ssd'].append(fh.read_reals('>f4'))
-        odata['ob_glb_tsd'].append(fh.read_reals('>f4'))
-        odata['ob_mds_sal'].append(fh.read_reals('>f4'))
-        odata['ob_mds_tmp'].append(fh.read_reals('>f4'))
-        odata['ob_rgn_sal'].append(fh.read_reals('>f4'))
-        odata['ob_rgn_tmp'].append(fh.read_reals('>f4'))
-        odata['ob_rgn_ssd'].append(fh.read_reals('>f4'))
-        odata['ob_rgn_tsd'].append(fh.read_reals('>f4'))
-
-    if odata['n_vrsn'] > 1:
-        odata['ob_sal_xvl'] = fh.read_reals('>f4')
-        odata['ob_sal_xsd'] = fh.read_reals('>f4')
-        odata['ob_tmp_xvl'] = fh.read_reals('>f4')
-        odata['ob_tmp_xsd'] = fh.read_reals('>f4')
-
-        #if odata['n_vrsn'] > 2:
-        #    For some reason ob_id has characters that don't conform.
-        #    ob_id is not used, so comment out for now
-        #    odata['ob_id'] = fh.read_record('>S10').astype('U10')
-
-    fh.close()
-
-    return odata
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 
-def to_nlocs(idata):
+class profile(object):
+
+    def __init__(self, filename, date):
+
+        self.filename = filename
+        self.date = date
+
+        # Read profile data
+        self._rd_prof()
+
+        return
+
+    def _rd_prof(self):
+
+        try:
+            fh = FortranFile(self.filename, mode='r', header_dtype='>i4')
+        except IOError:
+            raise IOError('%s file not found!' % self.filename)
+        except Exception:
+            raise Exception('Unknown error opening %s' % self.filename)
+
+        # odata is the dictionary with data structure as in ocn_obs.f
+        odata = {}
+
+        odata['n_obs'], odata['n_lvl'], odata['n_vrsn'] = fh.read_ints('>i4')
+
+        print('    number profiles: %d' % odata['n_obs'])
+        print('  max number levels: %d' % odata['n_lvl'])
+        print('file version number: %d' % odata['n_vrsn'])
+
+        if odata['n_obs'] <= 0:
+            print('No profile observations to process from %s' % self.filename)
+            return
+
+        odata['ob_btm'] = fh.read_reals('>f4')
+        odata['ob_lat'] = fh.read_reals('>f4')
+        odata['ob_lon'] = fh.read_reals('>f4')
+        odata['ob_ls'] = fh.read_reals('>i4')
+        odata['ob_lt'] = fh.read_reals('>i4')
+        odata['ob_ssh'] = fh.read_reals('>f4')
+        odata['ob_sst'] = fh.read_reals('>f4')
+        odata['ob_sal_typ'] = fh.read_reals('>i4')
+        odata['ob_sal_qc'] = fh.read_reals('>f4')
+        odata['ob_tmp_typ'] = fh.read_reals('>i4')
+        odata['ob_tmp_qc'] = fh.read_reals('>f4')
+
+        odata['ob_lvl'] = []
+        odata['ob_sal'] = []
+        odata['ob_sal_err'] = []
+        odata['ob_sal_prb'] = []
+        odata['ob_tmp'] = []
+        odata['ob_tmp_err'] = []
+        odata['ob_tmp_prb'] = []
+
+        for n in range(odata['n_obs']):
+            odata['ob_lvl'].append(fh.read_reals('>f4'))
+            odata['ob_sal'].append(fh.read_reals('>f4'))
+            odata['ob_sal_err'].append(fh.read_reals('>f4'))
+            odata['ob_sal_prb'].append(fh.read_reals('>f4'))
+            odata['ob_tmp'].append(fh.read_reals('>f4'))
+            odata['ob_tmp_err'].append(fh.read_reals('>f4'))
+            odata['ob_tmp_prb'].append(fh.read_reals('>f4'))
+
+        odata['ob_dtg'] = fh.read_record('>S12').astype('U12')
+        odata['ob_rcpt'] = fh.read_record('>S12').astype('U12')
+        odata['ob_scr'] = fh.read_record('>S1').astype('U1')
+        odata['ob_sign'] = fh.read_record('>S7').astype('U7')
+
+        odata['ob_clm_sal'] = []
+        odata['ob_clm_tmp'] = []
+        odata['ob_clm_ssd'] = []
+        odata['ob_clm_tsd'] = []
+        odata['ob_glb_sal'] = []
+        odata['ob_glb_tmp'] = []
+        odata['ob_glb_ssd'] = []
+        odata['ob_glb_tsd'] = []
+        odata['ob_mds_sal'] = []
+        odata['ob_mds_tmp'] = []
+        odata['ob_rgn_sal'] = []
+        odata['ob_rgn_tmp'] = []
+        odata['ob_rgn_ssd'] = []
+        odata['ob_rgn_tsd'] = []
+
+        for n in range(odata['n_obs']):
+            odata['ob_clm_sal'].append(fh.read_reals('>f4'))
+            odata['ob_clm_tmp'].append(fh.read_reals('>f4'))
+            odata['ob_clm_ssd'].append(fh.read_reals('>f4'))
+            odata['ob_clm_tsd'].append(fh.read_reals('>f4'))
+            odata['ob_glb_sal'].append(fh.read_reals('>f4'))
+            odata['ob_glb_tmp'].append(fh.read_reals('>f4'))
+            odata['ob_glb_ssd'].append(fh.read_reals('>f4'))
+            odata['ob_glb_tsd'].append(fh.read_reals('>f4'))
+            odata['ob_mds_sal'].append(fh.read_reals('>f4'))
+            odata['ob_mds_tmp'].append(fh.read_reals('>f4'))
+            odata['ob_rgn_sal'].append(fh.read_reals('>f4'))
+            odata['ob_rgn_tmp'].append(fh.read_reals('>f4'))
+            odata['ob_rgn_ssd'].append(fh.read_reals('>f4'))
+            odata['ob_rgn_tsd'].append(fh.read_reals('>f4'))
+
+        if odata['n_vrsn'] > 1:
+            odata['ob_sal_xvl'] = fh.read_reals('>f4')
+            odata['ob_sal_xsd'] = fh.read_reals('>f4')
+            odata['ob_tmp_xvl'] = fh.read_reals('>f4')
+            odata['ob_tmp_xsd'] = fh.read_reals('>f4')
+
+            # if odata['n_vrsn'] > 2:
+            #    For some reason ob_id has characters that don't conform.
+            #    ob_id is not used, so comment out for now
+            #    odata['ob_id'] = fh.read_record('>S10').astype('U10')
+
+        fh.close()
+
+        self.odata = odata
+
+        return
+
+    def _to_nlocs(self):
+
+        # idata is the dictionary containing IODA friendly data structure
+        idata = {}
+
+        idata['ob_lat'] = []
+        idata['ob_lon'] = []
+        idata['ob_lvl'] = []
+        idata['ob_dtg'] = []
+        idata['ob_sal'] = []
+        idata['ob_sal_err'] = []
+        idata['ob_tmp'] = []
+        idata['ob_tmp_err'] = []
+
+        for n in range(self.odata['n_obs']):
+
+            lat = self.odata['ob_lat'][n]
+            lon = self.odata['ob_lon'][n]
+            dtg = datetime.strptime(self.odata['ob_dtg'][n], '%Y%m%d%H%M')
+
+            # Diff. between ob. time and time in the file (hrs)!
+            ddtg = (dtg - self.date).seconds/3600.
+
+            for l, lvl in enumerate(self.odata['ob_lvl'][n]):
+
+                idata['ob_lat'].append(lat)
+                idata['ob_lon'].append(lon)
+                idata['ob_dtg'].append(ddtg)
+                idata['ob_lvl'].append(lvl)
+                idata['ob_sal'].append(self.odata['ob_sal'][n][l])
+                idata['ob_sal_err'].append(self.odata['ob_sal_err'][n][l])
+                idata['ob_tmp'].append(self.odata['ob_tmp'][n][l])
+                idata['ob_tmp_err'].append(self.odata['ob_tmp_err'][n][l])
+
+        idata['nlocs'] = len(idata['ob_dtg'])
+
+        self.idata = idata
+
+        return
+
+    def to_ioda(self):
+
+        if self.odata['n_obs'] <= 0:
+            print('No profile observations for IODA!')
+            return
+
+        self._to_nlocs()
+
+        ncid = Dataset(self.filename + '.nc4', 'w', format='NETCDF4')
+
+        ncid.createDimension('nlocs', self.idata['nlocs'])
+        ncid.createDimension('nvars', 2)
+
+        longitudes = ncid.createVariable('longitude', 'f4', ('nlocs'))
+        longitudes.units = 'degrees_east'
+
+        latitudes = ncid.createVariable('latitude', 'f4', ('nlocs'))
+        latitudes.units = 'degrees_north'
+
+        times = ncid.createVariable('time', 'f4', ('nlocs'))
+        times.long_name = 'observation time from date_time'
+        times.units = 'hours'
+        times.description = 'why does this have to be with a reference to, why not absolute?'
+
+        depths = ncid.createVariable('ocean_depth', 'f4', ('nlocs'))
+        depths.units = 'meters'
+        depths.positive = 'down'
+
+        sals = ncid.createVariable('ocean_salinity@ObsValue', 'f4', ('nlocs'))
+        sals.units = 'g/kg'
+        sals.description = ''
+
+        sal_errs = ncid.createVariable(
+            'ocean_salinity@ObsError', 'f4', ('nlocs'))
+        sal_errs.units = '(g/kg)^2'
+        sal_errs.description = 'Standard deviation of observation error'
+
+        tmps = ncid.createVariable(
+            'insitu_temperature@ObsValue', 'f4', ('nlocs'))
+        tmps.units = 'degree_celcius'
+        tmps.description = ''
+
+        tmp_errs = ncid.createVariable(
+            'insitu_temperature@ObsError', 'f4', ('nlocs'))
+        tmp_errs.units = '(degree_celcius)^2'
+        tmp_errs.description = 'Standard deviation of observation error'
+
+        ncid.date_time = datetime.strftime(self.date, '%Y%m%d%H%M')
+
+        longitudes[:] = np.asarray(self.idata['ob_lon'], dtype=np.float32)
+        latitudes[:] = np.asarray(self.idata['ob_lat'], dtype=np.float32)
+        depths[:] = np.asarray(self.idata['ob_lvl'], dtype=np.float32)
+        times[:] = np.asarray(self.idata['ob_dtg'], dtype=np.float32)
+        sals[:] = np.asarray(self.idata['ob_sal'], dtype=np.float32)
+        sal_errs[:] = np.asarray(self.idata['ob_sal_err'], dtype=np.float32)
+        tmps[:] = np.asarray(self.idata['ob_tmp'], dtype=np.float32)
+        tmp_errs[:] = np.asarray(self.idata['ob_tmp_err'], dtype=np.float32)
+
+        ncid.close()
+
+        return
 
 
-    odata = {}
+if __name__ == '__main__':
 
-    odata['datetime'] = idata['datetime']
+    parser = ArgumentParser(
+        description='Read GODAE binary profile file and convert to IODA netCDF4 format',
+        formatter_class=ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        '--name', help='name of the binary GODAE profile file (path)',
+        type=str, required=True)
+    parser.add_argument('--date', help='file date', type=str, required=True)
 
-    odata['ob_lat'] = []
-    odata['ob_lon'] = []
-    odata['ob_lvl'] = []
-    odata['ob_dtg'] = []
-    odata['ob_sal'] = []
-    odata['ob_sal_err'] = []
-    odata['ob_tmp'] = []
-    odata['ob_tmp_err'] = []
+    args = parser.parse_args()
 
-    for n in range(idata['n_obs']):
+    profname = args.name
+    profdate = datetime.strptime(args.date, '%Y%m%d%H')
 
-        lat = idata['ob_lat'][n]
-        lon = idata['ob_lon'][n]
-        dtg = idata['ob_dtg'][n]
-
-        for l, lvl in enumerate(idata['ob_lvl'][n]):
-
-            odata['ob_lat'].append(lat)
-            odata['ob_lon'].append(lon)
-            odata['ob_dtg'].append(dtg)
-            odata['ob_lvl'].append(lvl)
-            odata['ob_sal'].append(idata['ob_sal'][n][l])
-            odata['ob_sal_err'].append(idata['ob_sal_err'][n][l])
-            odata['ob_tmp'].append(idata['ob_tmp'][n][l])
-            odata['ob_tmp_err'].append(idata['ob_tmp_err'][n][l])
-
-    return odata
-
-
-def toioda(fname, odata):
-
-    ncid = Dataset(fname, 'w', format='NETCDF4')
-
-    ncid.createDimension('nlocs', len(odata['ob_lat']))
-
-    longitudes = ncid.createVariable('longitude', 'f4', ('nlocs'))
-    longitudes.units = 'degrees_east'
-    latitudes = ncid.createVariable('latitude', 'f4', ('nlocs'))
-    latitudes.units = 'degrees_north'
-    times = ncid.createVariable('time', 'f4', ('nlocs'))
-    times.long_name = 'observation_time_YYYYMMDDHHMMSS'
-    levels = ncid.createVariable('level', 'f4', ('nlocs'))
-    levels.units = 'meters'
-    levels.positive = 'down'
-    sals = ncid.createVariable('ocean_salinity@ObsValue', 'f4', ('nlocs'))
-    sals.units = 'g/kg?'
-    sal_errs = ncid.createVariable('ocean_salinity@ObsError', 'f4', ('nlocs'))
-    sal_errs.units = 'sal.units^2'
-    tmps = ncid.createVariable('insitu_temperature@ObsValue', 'f4', ('nlocs'))
-    tmps.units = 'degree_celcius'
-    tmp_errs = ncid.createVariable('insitu_temperature@ObsError', 'f4', ('nlocs'))
-    tmp_errs.units = 'degree_celcius^2'
-
-    ncid.date_time = odata['datetime']
-
-    longitudes[:] = np.asarray(odata['ob_lon'], dtype=np.float32)
-    latitudes[:] = np.asarray(odata['ob_lat'], dtype=np.float32)
-    levels[:] = np.asarray(odata['ob_lvl'], dtype=np.float32)
-    times[:] = np.asarray(odata['ob_lvl'], dtype=np.float32) * 0.0 # To do!
-    sals[:] = np.asarray(odata['ob_sal'], dtype=np.float32)
-    sal_errs[:] = np.asarray(odata['ob_sal_err'], dtype=np.float32)
-    tmps[:] = np.asarray(odata['ob_tmp'], dtype=np.float32)
-    tmp_errs[:] = np.asarray(odata['ob_tmp_err'], dtype=np.float32)
-
-    ncid.close()
-
-    return
-
-
-profname = sys.argv[1]
-iodaname = sys.argv[2]
-datetime = sys.argv[3]
-
-odata = rd_prof(profname, datetime=datetime)
-odata2 = to_nlocs(odata)
-toioda(iodaname, odata2)
-
-
+    prof = profile(profname, profdate)
+    prof.to_ioda()
