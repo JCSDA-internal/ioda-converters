1520b5472dfd19a5e7bd9d8d954e038a52735ac0
Stephen Herbener <stephen.herbener@gmail.com>
2018-12-14 22:58:37 +0000

    Netcdf writer supports both current and new netcdf output formats.

--- src/lib-python/ioda_conv_ncio.py
+++ src/lib-python/ioda_conv_ncio.py
@@ -10,9 +10,12 @@ from netCDF4 import Dataset
 ###################################################################################
 
 # Output format
-OUT_NC_FORMAT = 1    # 1 - old format with list of vectors
-                     # 2 - new format with groups for meta data, 2D tables for obs
-                     #     data, etc.
+OUT_NC_VERSION = 1   # 1 - all variables form a list of vectors in the top
+                     #     level group
+                     # 2 - groups for meta data, 2D tables for obs data, etc.
+
+OUT_NC_1_DESCRIP = "Each variable is a vector, all variables in top-level group"
+OUT_NC_2_DESCRIP = "Obs data are 2D arrays, metatdata are vectors in subgroups"
 
 # Names assigned to obs values, error estimates and qc marks
 OVAL_NAME = "ObsValue"
@@ -40,9 +43,9 @@ REC_NUM_NAME = "record_number"
 VLIST_NAME = "variable_names"
 
 # Names for metadata groups
-REC_MD_NAME = "/RecMetaData"
-LOC_MD_NAME = "/LocMetaData"
-VAR_MD_NAME = "/VarMetaData"
+REC_MD_NAME = "RecMetaData"
+LOC_MD_NAME = "LocMetaData"
+VAR_MD_NAME = "VarMetaData"
 
 # Misc
 MAX_STR_LEN = 20
@@ -82,43 +85,89 @@ def WriteNcAttr(Fid, AttrData, Nrecs, Nvars, Nlocs):
     for Aname, Aval in AttrData.items():
         Fid.setncattr(Aname, Aval)
 
-def WriteNcObsVars(Fid, Nvars, Nlocs, Nrecs, ObsVars):
+def NumpyToNcDtype(NumpyDtype):
+    ############################################################
+    # This method converts the numpy data type to the
+    # corresponding netcdf datatype
+
+    if (NumpyDtype == np.dtype('float64')):
+        NcDtype = 'f4'    # convert double to float
+    elif (NumpyDtype == np.dtype('float32')):
+        NcDtype = 'f4'
+    elif (NumpyDtype == np.dtype('int64')):
+        NcDtype = 'i4'    # convert long to int
+    elif (NumpyDtype == np.dtype('int32')):
+        NcDtype = 'i4'
+    elif (NumpyDtype == np.dtype('S1')):
+        NcDtype = 'c'
+    else:
+        print("ERROR: Unrecognized numpy data type: ", NumpyDtype)
+        exit(-2)
+    
+    return NcDtype
+
+def WriteNcObsVars(Fid, Nvars, Nlocs, Nrecs, ObsVars, VarMdata):
     ############################################################
     # This method will create dimensions and variables in the
     # output netcdf file for the obs variables.
 
+    # Dimensions used by any group can be placed at in the top
+    # level (root) group. This is convenient if we decide to
+    # rearrange the group structure.
     Fid.createDimension(NVARS_DIM_NAME, Nvars)
     Fid.createDimension(NLOCS_DIM_NAME, Nlocs)
     Fid.createDimension(NRECS_DIM_NAME, Nrecs)
     Fid.createDimension(NSTR_DIM_NAME, MAX_STR_LEN)
 
-    for Vname, Vvals in ObsVars.items():
-        if ((Vvals.dtype == np.dtype('float32')) or (Vvals.dtype == np.dtype('float64'))):
-            Fid.createVariable(Vname, 'f4', (NVARS_DIM_NAME, NLOCS_DIM_NAME))
-        elif ((Vvals.dtype == np.dtype('int32')) or (Vvals.dtype == np.dtype('int64'))):
-            Fid.createVariable(Vname, 'i4', (NVARS_DIM_NAME, NLOCS_DIM_NAME))
+    if (OUT_NC_VERSION == 1):
+        for Gname, Vvals in ObsVars.items():
+            for i in range(Nvars):
+                # The rstrip trims off the null bytes that correspond to '' chars
+                # in the character array.
+                Vname = VarMdata['variable_names'][i,:].tostring().rstrip('\x00')
+                NcVname = "{0:s}@{1:s}".format(Vname, Gname)
+                NcVvals = Vvals[i,:]
 
-        Fid[Vname][:] = Vvals
+                Fid.createVariable(NcVname, NumpyToNcDtype(NcVvals.dtype), (NLOCS_DIM_NAME))
+                Fid[NcVname][:] = NcVvals
 
-def WriteNcMetadata(Fid, MdataGroup, DimName, DimSize, RecMdata):
+    elif (OUT_NC_VERSION == 2):
+        for Vname, Vvals in ObsVars.items():
+            Fid.createVariable(Vname, NumpyToNcDtype(Vvals.dtype), (NVARS_DIM_NAME, NLOCS_DIM_NAME))
+            Fid[Vname][:] = Vvals
+
+def WriteNcMetadata(Fid, MdataGroup, DimName, Mdata):
     ############################################################
-    # This method will create dimensions and variables in the
-    # output netcdf file for the given metadata group.
+    # This method will create variables in the output netcdf
+    # file for the given metadata group.
 
-    MdGroup = Fid.createGroup(MdataGroup)
+    if (OUT_NC_VERSION == 1):
+        if (MdataGroup == LOC_MD_NAME):
+            Gname = "MetaData"
+        else:
+            Gname = MdataGroup
 
-    #MdGroup.createDimension(DimName, DimSize)
-    #MdGroup.createDimension(NSTR_DIM_NAME, MAX_STR_LEN)
+        for Vname, Vvals in Mdata.items():
+            NcVname = "{0:s}@{1:s}".format(Vname, Gname)
+            NcDtype = NumpyToNcDtype(Vvals.dtype)
+            if (NcDtype == 'c'):
+                Fid.createVariable(NcVname, NcDtype, (DimName, NSTR_DIM_NAME))
+            else:
+                Fid.createVariable(NcVname, NcDtype, (DimName))
+       
+            Fid[NcVname][:] = Vvals
 
-    for Vname, Vvals in RecMdata.items():
-        if ((Vvals.dtype == np.dtype('float32')) or (Vvals.dtype == np.dtype('float64'))):
-            MdGroup.createVariable(Vname, 'f4', (DimName))
-        elif ((Vvals.dtype == np.dtype('int32')) or (Vvals.dtype == np.dtype('int64'))):
-            MdGroup.createVariable(Vname, 'i4', (DimName))
-        elif (Vvals.dtype == np.dtype('S1')):
-            MdGroup.createVariable(Vname, 'c', (DimName, NSTR_DIM_NAME))
+    elif (OUT_NC_VERSION == 2):
+        MdGroup = Fid.createGroup(MdataGroup)
 
-        MdGroup[Vname][:] = Vvals
+        for Vname, Vvals in Mdata.items():
+            NcDtype = NumpyToNcDtype(Vvals.dtype)
+            if (NcDtype == 'c'):
+                MdGroup.createVariable(Vname, NcDtype, (DimName, NSTR_DIM_NAME))
+            else:
+                MdGroup.createVariable(Vname, NcDtype, (DimName))
+       
+            MdGroup[Vname][:] = Vvals
 
 def ExtractObsData(ObsData):
     ############################################################
@@ -246,6 +295,20 @@ def BuildNetcdf(NcFname, ObsData, AttrData):
     #         station_id
     #         analysis_date_time
 
+    # Check the output format setting
+    if (OUT_NC_VERSION == 1):
+       print("BuildNetcdf: Writing output netcdf version 1")
+       print("  ", OUT_NC_1_DESCRIP)
+    elif (OUT_NC_VERSION == 2):
+       print("BuildNetcdf: Writing output netcdf version 2")
+       print("  ", OUT_NC_2_DESCRIP)
+    else:
+       print("ERROR: Undefined version number output netcdf file: ", OUT_NC_VERSION)
+       print("ERROR:   Recognized version numbers:")
+       print("ERROR:     1:", OUT_NC_1_DESCRIP) 
+       print("ERROR:     2:", OUT_NC_2_DESCRIP)
+       exit(-1)
+    print("")
 
     # The structure of the input ObsData dictionary is:
     #     ObsData[Record][Location][Variable]
@@ -263,10 +326,10 @@ def BuildNetcdf(NcFname, ObsData, AttrData):
 
     WriteNcAttr(Fid, AttrData, Nrecs, Nvars, Nlocs)
 
-    WriteNcObsVars(Fid, Nvars, Nlocs, Nrecs, ObsVars)
+    WriteNcObsVars(Fid, Nvars, Nlocs, Nrecs, ObsVars, VarMdata)
 
-    WriteNcMetadata(Fid, REC_MD_NAME, NRECS_DIM_NAME, Nrecs, RecMdata)
-    WriteNcMetadata(Fid, LOC_MD_NAME, NLOCS_DIM_NAME, Nlocs, LocMdata)
-    WriteNcMetadata(Fid, VAR_MD_NAME, NVARS_DIM_NAME, Nvars, VarMdata)
+    WriteNcMetadata(Fid, REC_MD_NAME, NRECS_DIM_NAME, RecMdata)
+    WriteNcMetadata(Fid, LOC_MD_NAME, NLOCS_DIM_NAME, LocMdata)
+    WriteNcMetadata(Fid, VAR_MD_NAME, NVARS_DIM_NAME, VarMdata)
 
     Fid.close()
