dd3a3be16f9d8f8b19297fb2b6290d0bf7d4c45f
mpagowski <mariusz.pagowski@noaa.gov>
2019-02-21 15:52:08 -0700

    add py script for subsetting of files for aod

--- src/gsi-ncdiag_aod/subset.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/ksh
-ncks -F -O -d nlocs,1,100 aod_geoval_2018041500_f.nc4  aod_geoval_2018041500_m.nc4
-ncks -F -O -d nlocs,1,10 aod_geoval_2018041500_f.nc4  aod_geoval_2018041500_s.nc4
-ncks -F -O -d nlocs,1,100 aod_obs_2018041500_f.nc4  aod_obs_2018041500_m.nc4
-ncks -F -O -d nlocs,1,10 aod_obs_2018041500_f.nc4  aod_obs_2018041500_s.nc4
