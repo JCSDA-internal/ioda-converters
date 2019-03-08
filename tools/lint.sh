a9ea93859775b8fa375112617dde0a4a3af21ac2
Rahul Mahajan <aerorahul@users.noreply.github.com>
2019-03-08 13:25:46 -0500

    add godae tests for python code check
    Change-Id: I554adbda9ff8174d4bf3990fbfb51cee13e221ed

--- tools/lint.sh
+++ tools/lint.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
-set -xe
+set -ex
 
-pycodestyle .
-python run-pyflakes.py
-python run-mccabe.py
-pylint mymodule
+SRC_DIR=${1:-"./"}
+PYCODESTYLE_CFG_PATH=${2:-"./.pycodestyle"}
+
+cd $SRC_DIR
+
+pycodestyle --config=$PYCODESTYLE_CFG_PATH .
+
+# These require some more work, and may not be worth the effort.
+#python bin/run-pyflakes.py
+#python bin/run-mccabe.py
+#pylint modulename
