ca00eb936041f9dfe0daaea45317541c09fb75bc
Anna Shlyaeva <anna.v.shlyaeva@noaa.gov>
2019-03-15 03:20:14 +0000

    added scripts for ozone

---
+++ src/gsi-ncdiag/oz_dicts.py
@@ -0,0 +1,21 @@
+loc_metadata_dict = {'Latitude':   'latitude',     \
+                     'Longitude':  'longitude',    \
+                     'Reference_Pressure':  'air_presure',       \
+                     'Time':   'time',         \
+                     'Scan_Position':       'scan_position',     \
+	             'Solar_Zenith_Angle':  'sol_zenith_angle',  \
+                     'Row_Anomaly_Index':   'row_anomaly_index'}
+
+geovals_metadata_dict = {'Latitude':   'latitude',     \
+                         'Longitude':  'longitude',    \
+                         'Time':   'time'}
+
+variables_dict = {'Observation'       : 'ObsValue',   \
+                  'Obs_Minus_Forecast_adjusted' : 'GsiBc', \
+                  'Obs_Minus_Forecast_unadjusted' : 'GsiHofX', \
+                  'Analysis_Use_Flag' : 'GsiUseFlag', \
+                  'Inverse_Observation_Error': 'GsiFinalObsError'}
+
+geovals_dict = {'air_pressure_levels': 'air_pressure_levels', \
+                'mass_concentration_of_ozone_in_air': 'mass_concentration_of_ozone_in_air'}
+
