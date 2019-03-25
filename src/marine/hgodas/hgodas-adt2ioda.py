026d7e75da9811b1e36331a7cc52e2cac602172d
Travis Sluka <travissluka@gmail.com>
2019-03-25 09:53:49 -0600

    start of hgodas observation conversion

---
+++ src/marine/hgodas/hgodas-adt2ioda.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import argparse
+import ioda_conv_ncio as iconv
+import netCDF4 as nc
+from datetime import datetime, timedelta
+from collections import defaultdict
+
+# TODO set the date/time correctly
+
+
+class Observation(object):
+
+    def __init__(self, filename, date, writer):
+        self.filename = filename
+        self.date = date
+        self.data = defaultdict(lambda: defaultdict(dict))
+        self.writer = writer
+        self._read()
+
+    def _read(self):
+        ncd = nc.Dataset(self.filename)
+        time = ncd.variables['time'][:]
+        lons = ncd.variables['lon'][:]
+        lats = ncd.variables['lat'][:]
+        hrs = ncd.variables['hr'][:]
+        vals = ncd.variables['val'][:]
+        errs = ncd.variables['err'][:]
+        qcs = ncd.variables['qc'][:]
+        ncd.close()
+
+        base_date = datetime(1970, 1, 1) + timedelta(seconds=int(time[0]))
+
+        valKey = vName, self.writer.OvalName()
+        errKey = vName, self.writer.OerrName()
+        qcKey = vName, self.writer.OqcName()
+
+        count = 0
+        for i in range(len(hrs)):
+            if qcs[i] != 0:
+                continue
+
+            count += 1
+            dt = base_date + timedelta(hours=float(hrs[i]))
+            locKey = lats[i], lons[i], dt.strftime("%Y-%m-%dT%H:%M:%SZ")
+            self.data[0][locKey][valKey] = vals[i]
+            self.data[0][locKey][errKey] = errs[i]
+            self.data[0][locKey][qcKey] = qcs[i]
+
+
+vName = "obs_absolute_dynamic_topography",
+
+locationKeyList = [
+    ("latitude", "float"),
+    ("longitude", "float"),
+    ("date_time", "string")
+    ]
+
+AttrData = {
+  'odb_version': 1,
+   }
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description=('')
+    )
+    parser.add_argument('-i',
+                        help="name of HGODAS obs input file",
+                        type=str, required=True)
+    parser.add_argument('-o',
+                        help="name of ioda output file",
+                        type=str, required=True)
+    parser.add_argument('-d', '--date',
+                        help="base date", type=str, required=True)
+    args = parser.parse_args()
+    fdate = datetime.strptime(args.date, '%Y%m%d%H%M')
+
+    writer = iconv.NcWriter(args.o, [], locationKeyList)
+
+    # Read in the profiles
+    prof = Observation(args.i, fdate, writer)
+
+    # write them out
+    AttrData['date_time_string'] = fdate.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    writer.BuildNetcdf(prof.data, AttrData)
