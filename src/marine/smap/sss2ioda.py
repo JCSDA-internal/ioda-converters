a570efcc2631570441e1625f4e3a694dbf4cdcbf
Hamideh <Hamideh@macbook.fl-guest.ucar.edu>
2019-03-25 19:18:34 -0400

    cleaned up sss2ioda.py

--- src/marine/smap/sss2ioda.py
+++ src/marine/smap/sss2ioda.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
-
+from __future__ import print_function
+import argparse
+import ioda_conv_ncio as iconv
 import scipy.io.netcdf as netcdf
 import numpy as np
 import matplotlib.pyplot as plt
@@ -7,31 +9,19 @@ import matplotlib.cm as cm
 import dateutil.parser
 import datetime
 import glob
-from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import h5py
-import ioda_conv_ncio as iconv
 from collections import defaultdict
 
 
-class preobs:
-    def __init__(self, filename, window_length=24.0, midwindow_date='2018041500',writer):
 
-        # Set DA reftime
-        self.yyyy = midwindow_date[0:4]
-        self.mm = midwindow_date[4:6]
-        self.dd = midwindow_date[6:8]
-        self.hh = midwindow_date[8:10]
-
+class Profile(object):
+    def __init__(self, filename, date ,writer):
+        self.filename = filename
+        self.date     = date
+        self.data     = defaultdict(lambda: defaultdict(dict))
+        self.writer   = writer
         # Save observation source 
-        self.source = filename
-        self.writer = writer
-
-        # Set the reference time for the DA window
-        ref_da_time = datetime.datetime(int(self.yyyy),int(self.mm),int(self.dd),int(self.hh))
-        self.ref_da_time =  ref_da_time
-
-        # Observation file name (redundant? needed?)
-        self.filename=filename
+        self.source   = filename
 
         # Open obs file and read/load relevant info
         smap          =  h5py.File(self.filename,'r')
@@ -40,28 +30,12 @@ class preobs:
         self.times    =  np.squeeze(smap['/row_time'][:]) #1.03684024e+08
         self.sss      =  np.squeeze(smap['/smap_sss'][:])
         self.sss_u    =  np.squeeze(smap['/smap_sss_uncertainty'][:])
-        self.sss_qc    =  np.squeeze(smap['/quality_flag'][:])
+        self.sss_qc   =  np.squeeze(smap['/quality_flag'][:])
         self.tb_h_aft =  np.squeeze(smap['/tb_h_aft'][:])
         self.tb_h_fore=  np.squeeze(smap['/tb_h_fore'][:])
         self.tb_v_aft =  np.squeeze(smap['/tb_v_aft'][:])
         self.tb_v_fore= np.squeeze(smap['/tb_v_fore'][:])
 
-        # Get ref time for julian date
-        #units = ncfile.variables['time_mjd'].units[-23:-4]
-        #reftime = dateutil.parser.parse(units)
-
-        # Set time in hrs referenced to center DA window
-        for i in range(len(self.times)):
-            #print(self.times[0])
-            base_date=datetime.datetime(2015, 1, 1, 0, 0) + datetime.timedelta(seconds=np.double(self.times[i]))
-            ref_time = datetime.datetime(2018, 4, 15, 0, 0)
-            dt=base_date-ref_time
-            if (dt.days<0):
-                sgn = -1
-            else:
-                sgn = 1
-            self.times[i]=sgn*(np.abs(dt.days*24.0) + dt.seconds/3600.0)
-        
         # Trivial QC, might not be needed in the future
         self.validobs = np.where( (self.sss<50) & (self.sss>0))
         self.num_validobs = np.shape(self.sss[self.validobs])[0]
@@ -80,40 +54,26 @@ class preobs:
         self.tb_v_aft  = self.tb_v_aft[self.validobs]
         self.tb_v_fore = self.tb_v_fore[self.validobs]
 
-    def append(self, other):
-        # Append other to self
-        self.lon       = np.append(self.lon, other.lon)        
-        self.lat       = np.append(self.lat, other.lat)        
-        self.times     = np.append(self.times, other.times)
-        self.sss       = np.append(self.sss, other.sss)
-        self.sss_u     = np.append(self.sss_u, other.sss_u)
-        self.sss_qc    = np.append(self.sss_qc, other.sss_qc)
-        self.tb_h_aft  = np.append(self.tb_h_aft, other.tb_h_aft)
-        self.tb_h_fore = np.append(self.tb_h_fore, other.tb_h_fore)
-        self.tb_v_aft  = np.append(self.tb_v_aft, other.tb_v_aft)
-        self.tb_v_fore = np.append(self.tb_v_fore, other.tb_v_fore)
-    
-    def plot(self):
-        # Simple plot method for sanity checks
-        plt.scatter(self.lon[self.validobs],self.lat[self.validobs],c=self.sss[self.validobs],vmin=25,vmax=40)
-
-    def toioda(self, fname='sss_test.nc'):
         # Write in ioda netcdf format
         valKey = vName['SSS'], self.writer.OvalName()
         errKey = vName['SSS'], self.writer.OerrName()
         qcKey = vName['SSS'], self.writer.OqcName()
 
         count = 0
-        for i in range(len(hh)):
+        for i in range(len(lat)):
             if qcs[i] != 0:
                 continue
 
             count += 1
-            dt = base_date + timedelta(hours=float(hh[i]))
+            base_date=datetime.datetime(2015, 1, 1, 0, 0) + datetime.timedelta(seconds=np.double(self.times[i]))
+            #ref_time = datetime.datetime(2018, 4, 15, 0, 0)
+            #dt = base_date - ref_time
+            dt = base_date
+            #dt = base_date + timedelta(hours=float(hh[i]))
             locKey = lats[i], lons[i], dt.strftime("%Y-%m-%dT%H:%M:%SZ")
             self.data[0][locKey][valKey] = sss[i]
             self.data[0][locKey][errKey] = sss_u[i]
-            self.data[0][locKey][qcKey] = sss_qc[i]
+            self.data[0][locKey][qcKey]  = sss_qc[i]
 
 
 vName = {
--- test/test_smap.py
+++ test/test_smap.py
@@ -1,9 +1,9 @@
 #import sss2ioda
-
+import ioda_conv_ncio as iconv
 import imp
 foo = imp.load_source('preobs', '../src/marine/smap/sss2ioda.py')
 
 #from /Users/Hamideh/Documents/mine/SOCA/ioda-converters/src/marine/smap/sss2ioda import preobs
 filename='./testinput/SMAP_L2B_SSS_17104_20180415T010704_R16010_V4.2.h5'
-zz=foo.preobs(filename, window_length=24.0, midwindow_date='2018041500')
+zz=foo.preobs(filename, window_length=24.0, midwindow_date='2018041500',writer)
 zz.toioda(fname='smap_test.nc')
