3750164850e444e67dfd8b5f343409716c0969a8
Rahul Mahajan <aerorahul@users.noreply.github.com>
2019-03-25 18:14:23 -0600

    ioda_conv_ncio compatible FNMOC profiles
    Change-Id: I696032d56ae40ec0e64b16d4918dd20199ce8a9b

--- src/marine/godae/profile2ioda.py
+++ src/marine/godae/profile2ioda.py
@@ -9,8 +9,6 @@ from collections import defaultdict
 import ioda_conv_ncio as iconv
 
 
-vName = "obs_absolute_dynamic_topography",
-
 class profile(object):
 
     def __init__(self, filename, date):
@@ -136,124 +134,30 @@ class profile(object):
 
         return
 
-    def _to_nlocs(self):
+    def to_ioda(self):
+
         '''
         Selectively (e.g. salinity, temperature) convert odata into idata
-        idata is the IODA required data structure, where profiles are stacked
-        on top of each other into a single vector.
+        idata is the IODA required data structure
         '''
-
-        # idata is the dictionary containing IODA friendly data structure
-        idata = {}
-
-        idata['ob_lat'] = []
-        idata['ob_lon'] = []
-        idata['ob_lvl'] = []
-        idata['ob_dtg'] = []
-        idata['ob_sal'] = []
-        idata['ob_sal_err'] = []
-        idata['ob_tmp'] = []
-        idata['ob_tmp_err'] = []
-
-        for n in range(self.odata['n_obs']):
-
-            lat = self.odata['ob_lat'][n]
-            lon = self.odata['ob_lon'][n]
-            dtg = datetime.strptime(self.odata['ob_dtg'][n], '%Y%m%d%H%M')
-
-            # Diff. between ob. time and time in the file (hrs)!
-            #ddtg = (dtg - self.date).total_seconds()/3600.
-
-            for l, lvl in enumerate(self.odata['ob_lvl'][n]):
-
-                idata['ob_lat'].append(lat)
-                idata['ob_lon'].append(lon)
-                #idata['ob_dtg'].append(ddtg)
-                idata['ob_dtg'].append(dtg)  # provide absolute time
-                idata['ob_lvl'].append(lvl)
-                idata['ob_sal'].append(self.odata['ob_sal'][n][l])
-                idata['ob_sal_err'].append(self.odata['ob_sal_err'][n][l])
-                idata['ob_tmp'].append(self.odata['ob_tmp'][n][l])
-                idata['ob_tmp_err'].append(self.odata['ob_tmp_err'][n][l])
-
-        idata['nlocs'] = len(idata['ob_dtg'])
-
-        self.idata = idata
-
-        return
-
-    def to_ioda(self):
 
         if self.odata['n_obs'] <= 0:
             print('No profile observations for IODA!')
             return
 
-        self._to_nlocs()
+        locationKeyList = [
+            ("latitude", "float"),
+            ("longitude", "float"),
+            ("level", "float"),
+            ("date_time", "string")
+        ]
 
-        ncid = netcdf.netcdf_file(self.filename + '.nc', mode='w')
+        AttrData = {
+            'odb_version': 1,
+            'date_time_string': self.date.strftime("%Y-%m-%dT%H:%M:%SZ")
+        }
 
-        ncid.createDimension('nlocs', self.idata['nlocs'])  # no. of obs per var type
-        ncid.createDimension('nrecs', 1)  # no. of profiles
-        ncid.createDimension('nvars', 1)  # no. of variables
-        ncid.createDimension('nobs', 1*self.idata['nlocs'])  # total no. of obs
-
-        longitudes = ncid.createVariable('longitude@MetaData', 'f4', ('nlocs',))
-        longitudes.units = 'degrees_east'
-
-        latitudes = ncid.createVariable('latitude@MetaData', 'f4', ('nlocs',))
-        latitudes.units = 'degrees_north'
-
-        times = ncid.createVariable('time@MetaData', 'f4', ('nlocs',))
-        times.long_name = 'observation time from date_time'
-        times.units = 'hours'
-        times.description = 'why does this have to be with a reference to, why not absolute?'
-
-        depths = ncid.createVariable('ocean_depth@MetaData', 'f4', ('nlocs',))
-        depths.units = 'meters'
-        depths.positive = 'down'
-
-        sals = ncid.createVariable('ocean_salinity@ObsValue', 'f4', ('nlocs',))
-        sals.units = 'g/kg'
-        sals.description = ''
-
-        sal_errs = ncid.createVariable(
-            'ocean_salinity@ObsError', 'f4', ('nlocs',))
-        sal_errs.units = '(g/kg)'
-        sal_errs.description = 'Standard deviation of observation error'
-
-        tmps = ncid.createVariable(
-            'insitu_temperature@ObsValue', 'f4', ('nlocs',))
-        tmps.units = 'degree_celcius'
-        tmps.description = ''
-
-        tmp_errs = ncid.createVariable(
-            'insitu_temperature@ObsError', 'f4', ('nlocs',))
-        tmp_errs.units = '(degree_celcius)'
-        tmp_errs.description = 'Standard deviation of observation error'
-
-        ncid.date_time = int(datetime.strftime(self.date, '%Y%m%d%H%M')[0:10])
-
-        longitudes[:] = np.asarray(self.idata['ob_lon'], dtype=np.float32)
-        latitudes[:] = np.asarray(self.idata['ob_lat'], dtype=np.float32)
-        depths[:] = np.asarray(self.idata['ob_lvl'], dtype=np.float32)
-        times[:] = np.asarray(self.idata['ob_dtg'], dtype=np.float32)
-
-        sals[:] = np.asarray(self.idata['ob_sal'], dtype=np.float32)
-        sal_errs[:] = np.asarray(self.idata['ob_sal_err'], dtype=np.float32)
-
-        tmps[:] = np.asarray(self.idata['ob_tmp'], dtype=np.float32)
-        tmp_errs[:] = 0.5 + np.asarray(self.idata['ob_tmp_err'], dtype=np.float32)
-
-        ncid.close()
-
-        return
-
-    def _to_NcWriter(self, writer):
-        '''
-        Selectively (e.g. salinity, temperature) convert odata into idata
-        idata is the IODA required data structure, where profiles are stacked
-        on top of each other into a single vector.
-        '''
+        writer = iconv.NcWriter(self.filename+'.nc', [], locationKeyList)
 
         # idata is the dictionary containing IODA friendly data structure
         idata = defaultdict(lambda: defaultdict(dict))
@@ -281,8 +185,6 @@ class profile(object):
 
                 locKey = lat, lon, lvl, dtg.strftime("%Y-%m-%dT%H:%M:%SZ")
 
-                print("val = %f" % self.odata['ob_tmp'][n][l])
-
                 idata[n][locKey][tmp_valKey] = self.odata['ob_tmp'][n][l]
                 idata[n][locKey][tmp_errKey] = self.odata['ob_tmp_err'][n][l]
                 idata[n][locKey][tmp_qcKey] = tmp_qc
@@ -291,33 +193,7 @@ class profile(object):
                 idata[n][locKey][sal_errKey] = self.odata['ob_sal_err'][n][l]
                 idata[n][locKey][sal_qcKey] = sal_qc
 
-        self.idata = idata
-
-        return
-
-    def to_iodaNcWriter(self):
-
-        if self.odata['n_obs'] <= 0:
-            print('No profile observations for IODA!')
-            return
-
-        locationKeyList = [
-            ("latitude", "float"),
-            ("longitude", "float"),
-            ("level", "float"),
-            ("date_time", "string")
-            ]
-
-        AttrData = {
-          'odb_version': 1,
-          'date_time_string': self.date.strftime("%Y-%m-%dT%H:%M:%SZ")
-           }
-
-        writer = iconv.NcWriter(self.filename+'.nc', [], locationKeyList)
-
-        self._to_NcWriter(writer)
-
-        writer.BuildNetcdf(self.idata, AttrData)
+        writer.BuildNetcdf(idata, AttrData)
 
         return
 
@@ -340,5 +216,4 @@ if __name__ == '__main__':
     fdate = datetime.strptime(args.date, '%Y%m%d%H%M')
 
     prof = profile(fname, fdate)
-    #prof.to_ioda()
-    prof.to_iodaNcWriter()
+    prof.to_ioda()
