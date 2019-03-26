2cbce7f53e3168fdfa8613d5f560f95b0f2fe352
Hamideh Ebrahimi <hebrahi1@discover16.prv.cube>
2019-03-26 02:21:19 -0400

    sss2ioda.py modified to fix the date

--- src/marine/smap/sss2ioda.py
+++ src/marine/smap/sss2ioda.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import dateutil.parser
-import datetime
+from datetime import datetime, timedelta
 import glob
 import h5py
 from collections import defaultdict
@@ -22,37 +22,39 @@ class Profile(object):
         self.writer   = writer
         # Save observation source 
         self.source   = filename
+        self._read()
 
         # Open obs file and read/load relevant info
+    def _read(self):
         smap          =  h5py.File(self.filename,'r')
-        self.lat      =  np.squeeze(smap['/lat'][:])
-        self.lon      =  np.squeeze(smap['/lon'][:])
-        self.times    =  np.squeeze(smap['/row_time'][:]) #1.03684024e+08
-        self.sss      =  np.squeeze(smap['/smap_sss'][:])
-        self.sss_u    =  np.squeeze(smap['/smap_sss_uncertainty'][:])
-        self.sss_qc   =  np.squeeze(smap['/quality_flag'][:])
-        self.tb_h_aft =  np.squeeze(smap['/tb_h_aft'][:])
-        self.tb_h_fore=  np.squeeze(smap['/tb_h_fore'][:])
-        self.tb_v_aft =  np.squeeze(smap['/tb_v_aft'][:])
-        self.tb_v_fore= np.squeeze(smap['/tb_v_fore'][:])
+        lat      =  np.squeeze(smap['/lat'][:])
+        lon      =  np.squeeze(smap['/lon'][:])
+        times    =  np.squeeze(smap['/row_time'][:]) #1.03684024e+08
+        sss      =  np.squeeze(smap['/smap_sss'][:])
+        sss_u    =  np.squeeze(smap['/smap_sss_uncertainty'][:])
+        sss_qc   =  np.squeeze(smap['/quality_flag'][:])
+        tb_h_aft =  np.squeeze(smap['/tb_h_aft'][:])
+        tb_h_fore=  np.squeeze(smap['/tb_h_fore'][:])
+        tb_v_aft =  np.squeeze(smap['/tb_v_aft'][:])
+        tb_v_fore= np.squeeze(smap['/tb_v_fore'][:])
 
         # Trivial QC, might not be needed in the future
-        self.validobs = np.where( (self.sss<50) & (self.sss>0))
-        self.num_validobs = np.shape(self.sss[self.validobs])[0]
+        validobs = np.where( (sss<50) & (sss>0))
+        num_validobs = np.shape(sss[validobs])[0]
 
         # Store obs info in memory
 
-        self.lon    = self.lon[self.validobs]
-        self.times  = np.tile(self.times,(self.sss_u.shape[0],1))
-        self.lat    = self.lat[self.validobs]
-        self.sss    = self.sss[self.validobs]
-        self.sss_u  = self.sss_u[self.validobs]
-        self.sss_qc = self.sss_qc[self.validobs]
-        self.times  = self.times[self.validobs]
-        self.tb_h_aft  = self.tb_h_aft[self.validobs]
-        self.tb_h_fore = self.tb_h_fore[self.validobs]
-        self.tb_v_aft  = self.tb_v_aft[self.validobs]
-        self.tb_v_fore = self.tb_v_fore[self.validobs]
+        lon    = lon[validobs]
+        times  = np.tile(times,(sss_u.shape[0],1))
+        lat    = lat[validobs]
+        sss    = sss[validobs]
+        sss_u  = sss_u[validobs]
+        sss_qc = sss_qc[validobs]
+        times  = times[validobs]
+        tb_h_aft  = tb_h_aft[validobs]
+        tb_h_fore = tb_h_fore[validobs]
+        tb_v_aft  = tb_v_aft[validobs]
+        tb_v_fore = tb_v_fore[validobs]
 
         # Write in ioda netcdf format
         valKey = vName['SSS'], self.writer.OvalName()
@@ -61,16 +63,16 @@ class Profile(object):
 
         count = 0
         for i in range(len(lat)):
-            if qcs[i] != 0:
+            if sss_qc[i] != 0:
                 continue
 
             count += 1
-            base_date=datetime.datetime(2015, 1, 1, 0, 0) + datetime.timedelta(seconds=np.double(self.times[i]))
+            base_date=datetime(2015, 1, 1, 0, 0) + timedelta(seconds=np.double(times[i]))
             #ref_time = datetime.datetime(2018, 4, 15, 0, 0)
             #dt = base_date - ref_time
             dt = base_date
             #dt = base_date + timedelta(hours=float(hh[i]))
-            locKey = lats[i], lons[i], dt.strftime("%Y-%m-%dT%H:%M:%SZ")
+            locKey = lat[i], lon[i], dt.strftime("%Y-%m-%dT%H:%M:%SZ")
             self.data[0][locKey][valKey] = sss[i]
             self.data[0][locKey][errKey] = sss_u[i]
             self.data[0][locKey][qcKey]  = sss_qc[i]
