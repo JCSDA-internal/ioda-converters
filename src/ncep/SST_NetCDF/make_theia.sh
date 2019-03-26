87615eb189eae230d1f0d7e7347e00c750d0d55a
Shastri Paturi <shastri.paturi@noaa.gov>
2019-03-26 22:01:13 +0000

    Remove directory SST_NetCDF

--- src/ncep/SST_NetCDF/make_theia.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-module load netcdf
-
-ifort -c  read_nc_combine_sst_IODA.f  -I/apps/w3lib/2.0.2-intel/lib `nc-config --fflags --flibs --libs`
-ifort -o read_nc_sst_IODA read_nc_combine_sst_IODA.o -L/scratch3/NCEPDEV/nwprod/lib -lw3nco_4 -lw3emc_4  `nc-config --fflags --flibs --libs`
