1b0314f95234c14ae6196622634ac60195b508c2
Travis Sluka <travissluka@gmail.com>
2019-04-02 14:32:32 -0600

    generalized gds2.0 converter

--- src/marine/gds2/gds2_sst2ioda.py
+++ src/marine/gds2/gds2_sst2ioda.py
@@ -9,8 +9,17 @@ import dateutil.parser
 from collections import defaultdict
 import numpy as np
 
+
 sst_name = "sea_surface_temperature",
 sst_skin_name = "sea_surface_skin_temperature",
+
+
+input_vars = (
+    'quality_level',
+    'sst_dtime',
+    'sses_bias',
+    'sses_standard_deviation',
+    'sea_surface_temperature')
 
 
 class Observation(object):
@@ -23,9 +32,8 @@ class Observation(object):
         self.writer = writer
         self._read()
 
-        
     def _read(self):
-        
+
         sst_valKey = sst_name, self.writer.OvalName()
         sst_errKey = sst_name, self.writer.OerrName()
         sst_qcKey = sst_name, self.writer.OqcName()
@@ -37,29 +45,44 @@ class Observation(object):
         for f in self.filename:
             ncd = nc.Dataset(f, 'r')
             lvl = ncd.processing_level
-            time = ncd.variables['time'][0]
-            time_units = ncd.variables['time'].units[-20:]
-            basetime = dateutil.parser.parse(time_units)
-            basetime += timedelta(seconds=float(time))
+            time_base = ncd.variables['time'][:]
+            basetime = dateutil.parser.parse(ncd.variables['time'].units[-20:])
 
+            print(f)
+
+            # other global attributes we might want saved to the output file
+            for v in ('source', 'platform', 'sensor'):
+                AttrData[v] = ncd.getncattr(v)
+
+            # Determine the lat/lon grid.
+            # If L3, we need to convert 1D lat/lon to 2D lat/lon.
+            # If len(time) > 1, also need to repeat the lat/lon vals
             lons = ncd.variables['lon'][:].ravel()
             lats = ncd.variables['lat'][:].ravel()
-            sst_qc   = ncd.variables['quality_level'][:].ravel()
-
             if lvl[:2] == 'L3':
-                # If L3, we need to convert 1D lat/lon to 2D lat/lon
-                lons,lats=np.meshgrid(lons,lats)
-                lons=lons.ravel()
-                lats=lats.ravel()
-                mask = np.logical_not(sst_qc.mask)
-            else:
-                mask = sst_qc >= 0
-                
+                lons, lats = np.meshgrid(lons, lats)
+                lons = lons.ravel()
+                lats = lats.ravel()
+            lons = np.tile(lons, len(time_base)).ravel()
+            lats = np.tile(lats, len(time_base)).ravel()
+
+            # calculate the time offsets
+            time = np.tile(np.zeros(len(lons) / len(time_base)),
+                           (len(time_base), 1))
+            for i in range(len(time_base)):
+                time[i, :] = time_base[i]
+            time = time.ravel()
+
+            # load in all the other data
+            data = {}
+            for v in input_vars:
+                data[v] = ncd.variables[v][:].ravel()
+
             # TODO: this is a currently a hack.
             #  all questionable obs are removed.
             #  This should be changed to include all obs and their qc flags
             #  once the preqc filter works
-            mask = np.logical_and(mask, sst_qc == 5)
+            mask = data['quality_level'] == 5
 
             # TODO: this is also currently a hack.
             #  jedi is not handling thinning the way I need, so
@@ -68,36 +91,39 @@ class Observation(object):
             np.random.seed(0)
             mask_thin = np.random.uniform(size=len(mask)) > args.thin
             mask = np.logical_and(mask, mask_thin)
-            
+
+            # sometimes the input variables have masks
+            for v in input_vars:
+                if np.ma.is_masked(data[v]):
+                    mask = np.logical_and(mask,
+                                          np.logical_not(data[v].mask))
+
+            # apply the masks
+            time = time[mask]
             lons = lons[mask]
             lats = lats[mask]
-            sst_qc = sst_qc[mask]
+            for v in input_vars:
+                data[v] = data[v][mask]
 
-            dtime = ncd.variables['sst_dtime'][:].ravel()[mask]
-            sst_skin = ncd.variables['sea_surface_temperature'][:].ravel()[mask] -273.15
-            sst_bias = ncd.variables['sses_bias'][:].ravel()[mask]
-            sst_err  = ncd.variables['sses_standard_deviation'][:].ravel()[mask]
-                
-            sst_bulk = sst_skin - sst_bias
+            # calculate output values
+            sst_skin = data['sea_surface_temperature'] - 273.15
+            sst_bulk = sst_skin - data['sses_bias']
 
-            # for each observation
+            # for each observation, save it to the dictionary
             for i in range(len(lons)):
-                # I'm not sure why the L3 sets have bad sst err fields... investigate
-                if sst_err[i] is np.ma.masked:
-                    continue
-                
-                obs_date = basetime + timedelta(seconds=float(dtime[i]))
-                locKey = lats[i], lons[i], obs_date.strftime("%Y-%m-%dT%H:%M:%SZ")
+                obs_date = basetime + \
+                    timedelta(seconds=float(time[i]+data['sst_dtime'][i]))
+                locKey = lats[i], lons[i], obs_date.strftime(
+                    "%Y-%m-%dT%H:%M:%SZ")
 
-                #print(sst_err[i], sst_bulk[i], sst_skin[i])
-                
                 self.data[0][locKey][sst_valKey] = sst_bulk[i]
-                self.data[0][locKey][sst_errKey] = sst_err[i]
-                self.data[0][locKey][sst_qcKey] = 0 # TODO, fill this in
+                self.data[0][locKey][sst_errKey] = data['sses_standard_deviation'][i]
+                self.data[0][locKey][sst_qcKey] = 5 - data['quality_level'][i]
 
                 self.data[0][locKey][sst_skin_valKey] = sst_skin[i]
-                self.data[0][locKey][sst_skin_errKey] = sst_err[i]
-                self.data[0][locKey][sst_skin_qcKey] = 0 # TODO, fill this in
+                self.data[0][locKey][sst_skin_errKey] = data['sses_standard_deviation'][i]
+                self.data[0][locKey][sst_skin_qcKey] = 5 - \
+                    data['quality_level'][i]
 
 
 locationKeyList = [
@@ -116,7 +142,7 @@ if __name__ == '__main__':
         description=('')
     )
     parser.add_argument('-i', '--input',
-                        help="NESDIS SST obs input files (wild cards or list)",
+                        help="GHRSST GDS2.0 generic SST obs input files (wild cards or list)",
                         type=str, nargs='+', required=True)
     parser.add_argument('-o', '--output',
                         help="name of ioda output file",
@@ -132,7 +158,7 @@ if __name__ == '__main__':
 
     if args.thin != 0.0:
         AttrData['thinning'] = args.thin
-    
+
     # Read in the altimeter
     altim = Observation(args.input, fdate, writer)
 
