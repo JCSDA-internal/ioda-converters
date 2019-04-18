65600b054a23efd85ecd5aa9ac7c9cdd310c1977
Travis Sluka <travissluka@gmail.com>
2019-04-18 11:23:09 -0600

    ctests for the marine ioda converters closes #135

---
+++ test/iodaconv_comp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# use nccmp to compare the output of a ioda-converter
+#
+# argument 1: the command to run the ioda converter
+# argument 2: the filename to test
+
+$1 && \
+nccmp testrun/$2 testoutput/$2 -d -m -g -f -S
---
+++ test/testinput/smap_sss_rss.nc
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e18c7629fe7601a17a7704c9a8f5b1157b0300c6b5c48b97d4426a9c4ed8a9fe
+size 55955
