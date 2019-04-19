7a3c7b2fd46a934194952137064b43fc42caddde
Stephen Herbener <stephen.herbener@gmail.com>
2019-04-19 16:00:09 -0600

    Consolidated the dependency/target generators into a common macro

--- cmake/iodaconv_extra_macros.cmake
+++ cmake/iodaconv_extra_macros.cmake
@@ -8,3 +8,17 @@ macro( COPY_FILES filelist source destination )
 		 ${destination}/${FILENAME} )
 	endforeach(FILENAME)
 endmacro()
+
+# macro to set up targest and dependencies from a list of files
+macro( SET_TARGETS_DEPS filelist source destination deplist)
+    foreach( FILENAME ${filelist} )
+        set ( SOURCE_FILE ${source}/${FILENAME} )
+        set ( DEST_FILE ${destination}/bin/${FILENAME} )
+        list( APPEND ${deplist} ${DEST_FILE} )
+        add_custom_command(
+            OUTPUT ${DEST_FILE}
+            DEPENDS ${SOURCE_FILE}
+            COMMAND cp ${SOURCE_FILE} ${DEST_FILE}
+        )
+    endforeach(FILENAME)
+endmacro( SET_TARGETS_DEPS )
