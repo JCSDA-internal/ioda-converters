11dc786999bda141c6fac1fa2dd7b33d1cbc11dc
Rahul Mahajan <aerorahul@users.noreply.github.com>
2019-04-22 12:19:41 -0400

    demonstrate the use of configure_file, to seperate bin, lib
    Change-Id: I1c9fdc25c0ee9a84709d236b848961956250c2e1

--- cmake/iodaconv_extra_macros.cmake
+++ cmake/iodaconv_extra_macros.cmake
@@ -1,24 +1,35 @@
-# Extra macros to eliminate repetitioN
+# Extra macros to eliminate repetition
 
 # Macro to copy list of files from source to destination
 macro( COPY_FILES filelist source destination )
-	foreach(FILENAME ${filelist})
-	  execute_process( COMMAND ${CMAKE_COMMAND} -E copy
-		 ${source}/${FILENAME}
-		 ${destination}/${FILENAME} )
-	endforeach(FILENAME)
+  foreach(FILENAME ${filelist})
+    execute_process( COMMAND ${CMAKE_COMMAND} -E copy
+      ${source}/${FILENAME}
+      ${destination}/${FILENAME}
+    )
+  endforeach(FILENAME)
 endmacro()
 
-# macro to set up targest and dependencies from a list of files
+# macro to set up target and dependencies from a list of files
 macro( SET_TARGETS_DEPS filelist source destination deplist)
-    foreach( FILENAME ${filelist} )
-        set ( SOURCE_FILE ${source}/${FILENAME} )
-        set ( DEST_FILE ${destination}/bin/${FILENAME} )
-        list( APPEND ${deplist} ${DEST_FILE} )
-        add_custom_command(
-            OUTPUT ${DEST_FILE}
-            DEPENDS ${SOURCE_FILE}
-            COMMAND cp ${SOURCE_FILE} ${DEST_FILE}
-        )
-    endforeach(FILENAME)
+  foreach( FILENAME ${filelist} )
+    set ( SOURCE_FILE ${source}/${FILENAME} )
+    set ( DEST_FILE ${destination}/${FILENAME} )
+    list( APPEND ${deplist} ${DEST_FILE} )
+    add_custom_command(
+      OUTPUT ${DEST_FILE}
+      DEPENDS ${SOURCE_FILE}
+      COMMAND cp ${SOURCE_FILE} ${DEST_FILE}
+    )
+  endforeach(FILENAME)
 endmacro( SET_TARGETS_DEPS )
+
+# macro to configure target dependencies from a list of files
+macro( CONF_TARGETS_DEPS filelist source destination deplist)
+  foreach( FILENAME ${filelist} )
+    set ( SOURCE_FILE ${source}/${FILENAME} )
+    set ( DEST_FILE ${destination}/${FILENAME} )
+    list( APPEND ${deplist} ${DEST_FILE} )
+    configure_file( ${SOURCE_FILE}.in ${DEST_FILE} @ONLY )
+  endforeach(FILENAME)
+endmacro( CONF_TARGETS_DEPS )
