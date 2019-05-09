b6a1a62de6aefc1e6e533c9758e604674f703a0b
Rahul Mahajan <aerorahul@users.noreply.github.com>
2019-05-08 21:45:15 -0400

    replace sys.path.extend with append.
    Change-Id: I31aeba137d7128fe02a987946b5cf15287fbc58c

--- tools/lint.sh
+++ tools/lint.sh
@@ -7,7 +7,8 @@ PYCODESTYLE_CFG_PATH=${2:-"./"}
 
 cd $SRC_DIR
 
-pycodestyle --config="$PYCODESTYLE_CFG_PATH/.pycodestyle" *.py *.py.in
+pycodestyle --config="$PYCODESTYLE_CFG_PATH/.pycodestyle" .
+pycodestyle --config="$PYCODESTYLE_CFG_PATH/.pycodestyle" *.py.in
 
 # These require some more work, and may not be worth the effort.
 #python bin/run-pyflakes.py
