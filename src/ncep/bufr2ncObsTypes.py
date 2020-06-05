6e565f1864a2637db8267a8602317f1541442a7f
Stephen Herbener <stephen.herbener@gmail.com>
2020-06-05 08:28:59 -0600

    Fixed coding norm error.

--- src/ncep/bufr2ncObsTypes.py
+++ src/ncep/bufr2ncObsTypes.py
@@ -1406,8 +1406,7 @@ class GpsroObsType(ObsType):
                 BNDA_err = np.ma.array([0.0], mask=[True])
                 for i in range(BangleFreqCnts[irep]):
                     m = 6 * (i + 1) - 3
-                    if ((int(BangleBvals[m, irep]) == 0)
-                            and (not BangleBvals.mask[m, irep])):
+                    if ((int(BangleBvals[m, irep]) == 0) and (not BangleBvals.mask[m, irep])):
                         # This replication has zero frequency which is not masked
                         # (that is, not marked as missing).
                         MEFR = np.ma.array(BangleBvals[m, irep],
