8eec7c25c5f47a722e82b8ec1205ca72d6c75644
Rahul Mahajan <aerorahul@users.noreply.github.com>
2020-08-20 14:10:00 -0400

    wip: bring upto speed with bufr library

--- cmake/iodaconv_compiler_flags.cmake
+++ cmake/iodaconv_compiler_flags.cmake
@@ -1,8 +1,9 @@
 # (C) Copyright 2009-2016 ECMWF.
-# 
+# (C) Copyright 2020-2020 NOAA/NWS/NCEP/EMC
+#
 # This software is licensed under the terms of the Apache Licence Version 2.0
-# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0. 
-# In applying this licence, ECMWF does not waive the privileges and immunities 
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
 # granted to it by virtue of its status as an intergovernmental organisation nor
 # does it submit to any jurisdiction.
 
@@ -26,3 +27,21 @@ else()
   message( STATUS "Fortran compiler with ID ${CMAKE_CXX_COMPILER_ID} will be used with CMake default options")
 endif()
 
+
+#######################################################################################
+# C++
+#######################################################################################
+
+if( CMAKE_CXX_COMPILER_ID MATCHES "GNU" )
+  include( compiler_flags_GNU_CXX )
+elseif( CMAKE_CXX_COMPILER_ID MATCHES "Intel" )
+  include( compiler_flags_Intel_CXX )
+elseif( CMAKE_CXX_COMPILER_ID MATCHES "XL" )
+  include( compiler_flags_XL_CXX )
+elseif( CMAKE_CXX_COMPILER_ID MATCHES "Cray" )
+  include( compiler_flags_Cray_CXX )
+elseif( CMAKE_CXX_COMPILER_ID MATCHES "Clang" )
+  include( compiler_flags_Clang_CXX )
+else()
+  message( STATUS "C++ compiler with ID ${CMAKE_CXX_COMPILER_ID} will be used with CMake default options")
+endif()
