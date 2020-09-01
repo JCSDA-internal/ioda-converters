62cd8c1c717ee84f23b17ca99cc4b35b939b5ef5
gthompsnJCSDA <gthompsn@ucar.edu>
2020-09-01 22:42:15 +0000

    adjusted for coding norms

--- src/lib-python/meteo_utils.py
+++ src/lib-python/meteo_utils.py
@@ -11,6 +11,7 @@
 
 import math
 
+
 class meteo_utils(object):
 
     # Constructor
@@ -25,15 +26,15 @@ class meteo_utils(object):
         self.inHg_2_Pa = 3386.39                 # Inches of mercury to Pascals
         self.STP_P = 29.92 * self.inHg_2_Pa      # Standard pressure, convert to Pascals
         self.STP_T = 15.0 + self.C_2_K           # Standard temperature, convert to Kelvin
-        self.STP_rho_air = self.STP_P / (287.0*self.STP_T) # Air density at STP (standard temperature and pressure)
+        self.STP_rho_air = self.STP_P/(287.0*self.STP_T)  # Air density at STP (standard temperature and pressure)
         self.FT_2_M = 0.3048                     # Feet to meters
         self.R = 287.04                          # Gas constant
         self.Cp = 1004.0                         # Specific heat capacity
         self.LAPSE = 0.0065                      # Standard lapse rate 6.5C per kilometer
         self.g = 9.80665                         # Gravity (m/s^2)
         self.RHO_WATER = 1000.                   # Density of water (kg/m^3)
-
-#---+---+---+---+
+#
+# --+---+---+---+
 
     def specific_humidity(self, dewp_K, pres_Pa):
 
@@ -48,8 +49,8 @@ class meteo_utils(object):
         sh = r / (1.0 + r)
 
         return sh
-
-#---+---+---+---+
+#
+# --+---+---+---+
 
     def r_sub_s(self, pres_Pa, temp_K):
 
@@ -70,8 +71,8 @@ class meteo_utils(object):
         rs = 0.622*es/(pres_Pa-es)
 
         return rs
-
-#---+---+---+---+
+#
+# --+---+---+---+
 
     def e_sub_s(self, temp_K):
 
@@ -80,8 +81,8 @@ class meteo_utils(object):
         polynomial fit of Goff-Gratch (1946) formulation. (Walko, 1991)
         '''
 
-        c = [610.5851,44.40316,1.430341,0.2641412e-1,0.2995057e-3,0.2031998e-5,0.6936113e-8,0.2564861e-11,-0.3704404e-13]
-        x = max(-80.,temp_K-self.C_2_K)
+        c = [610.5851, 44.40316, 1.430341, 0.2641412e-1, 0.2995057e-3, 0.2031998e-5, 0.6936113e-8, 0.2564861e-11, -0.3704404e-13]
+        x = max(-80., temp_K-self.C_2_K)
         es = c[0]+x*(c[1]+x*(c[2]+x*(c[3]+x*(c[4]+x*(c[5]+x*(c[6]+x*(c[7]+x*c[8])))))))
 
         '''
@@ -100,8 +101,8 @@ class meteo_utils(object):
         '''
 
         return es
-
-#---+---+---+---+
+#
+# --+---+---+---+
 
     def r_sub_i(self, pres_Pa, temp_K):
 
@@ -122,8 +123,8 @@ class meteo_utils(object):
         ri = 0.622*esi/(pres_Pa-esi)
 
         return ri
-
-#---+---+---+---+
+#
+# --+---+---+---+
 
     def e_sub_i(self, temp_K):
 
@@ -132,8 +133,8 @@ class meteo_utils(object):
         polynomial fit of Goff-Gratch (1946) formulation. (Walko, 1991)
         '''
 
-        c = [.609868993E03,.499320233E02,.184672631E01,.402737184E-1,.565392987E-3,.521693933E-5,.307839583E-7,.105785160E-9,.161444444E-12]
-        x = max(-80.,temp_K-self.C_2_K)
+        c = [.609868993E03, .499320233E02, .184672631E01, .402737184E-1, .565392987E-3, .521693933E-5, .307839583E-7, .105785160E-9, .161444444E-12]
+        x = max(-80., temp_K-self.C_2_K)
         esi = c[0]+x*(c[1]+x*(c[2]+x*(c[3]+x*(c[4]+x*(c[5]+x*(c[6]+x*(c[7]+x*c[8])))))))
 
         '''
@@ -150,8 +151,8 @@ class meteo_utils(object):
         '''
 
         return esi
-
-#---+---+---+---+
+#
+# --+---+---+---+
 
     def std_atmos(self, pres_Pa):
 
@@ -162,8 +163,8 @@ class meteo_utils(object):
         height = 44307.692 * (1.0 - (pr/1013.25)**0.190)
 
         return height
-
-#---+---+---+---+
+#
+# --+---+---+---+
 
     def std_atmos_p(self, height):
 
@@ -174,8 +175,8 @@ class meteo_utils(object):
         pr = math.exp(math.log(1.0-height/44307.692)/0.19)*101325.0
 
         return pr
-
-#---+---+---+---+
+#
+# --+---+---+---+
 
     def precipitable_water(self, pres_Pa, w_non):
 
@@ -186,18 +187,18 @@ class meteo_utils(object):
         returned precipitable water value in meters only below 150mb
         '''
 
-        sum=0.
+        sum = 0.
         k = 1
         while k < len(pres_Pa):
             if (pres_Pa[k] > 15000.0):
-                sum = sum + ( (w_non[k]+w_non[k-1])*0.5) * abs(pres_Pa[k]-pres_Pa[k-1])
+                sum = sum + ((w_non[k]+w_non[k-1])*0.5) * abs(pres_Pa[k]-pres_Pa[k-1])
             k = k + 1
 
-        answer = sum / (self.g*self.RHO_WATER)
+        answer = sum/(self.g*self.RHO_WATER)
 
         return answer
-
-#---+---+---+---+
+#
+# --+---+---+---+
 
     def dir_speed_2_uv(self, wdir, wspd):
 
@@ -208,9 +209,9 @@ class meteo_utils(object):
         u = -wspd * math.sin(wdir*self.DEG_2_RAD)
         v = -wspd * math.cos(wdir*self.DEG_2_RAD)
 
-        return u,v
-
-#---+---+---+---+
+        return u, v
+#
+# --+---+---+---+
 
     def altim_2_sfcPressure(self, altim, elev):
 
@@ -219,12 +220,12 @@ class meteo_utils(object):
         compute surface pressure returned in Pascals.
         '''
 
-        psfc = altim * ( (self.STP_T - self.LAPSE*elev) / self.STP_T)**5.2561
+        psfc = altim * ((self.STP_T - self.LAPSE*elev) / self.STP_T)**5.2561
         psfc = psfc * self.inHg_2_Pa
 
         return psfc
-
-#---+---+---+---+
+#
+# --+---+---+---+
 
     def theta_e(self, pres_Pa, temp_K, w_non, tlcl_K):
 
@@ -242,7 +243,7 @@ class meteo_utils(object):
         rr = w_non + 1.e-8
         tlc = tlcl_K
 
-        power=(0.2854*(1.0 - (0.28*rr) ))
+        power = (0.2854*(1.0 - (0.28*rr)))
         xx = tt * (100000.0/pp)**power
 
         p1 = (3.376/tlc) - 0.00254
@@ -251,8 +252,8 @@ class meteo_utils(object):
         thetae = xx * math.exp(p1*p2)
 
         return thetae
-
-#---+---+---+---+
+#
+# --+---+---+---+
 
     def t_lcl(self, temp_K, tdew_K):
 
@@ -264,13 +265,13 @@ class meteo_utils(object):
         '''
 
         tt = temp_K
-        tttd= tdew_K
-        denom= ( 1.0/(tttd-56.0) ) + (math.log(tt/tttd)/800.)
-        tlcl = ( 1.0 / denom ) + 56.0
+        tttd = tdew_K
+        denom = (1.0/(tttd-56.0)) + (math.log(tt/tttd)/800.)
+        tlcl = (1.0/denom) + 56.0
 
         return tlcl
-
-#---+---+---+---+
+#
+# --+---+---+---+
 
     def t_dew(self, pres_Pa, w_non):
 
@@ -287,8 +288,8 @@ class meteo_utils(object):
         tdew = (35.86*esln-4947.2325)/(esln-23.6837)
 
         return tdew
-
-#---+---+---+---+
+#
+# --+---+---+---+
 
     def theta_wetb(self, thetae_K):
 
@@ -298,23 +299,23 @@ class meteo_utils(object):
          and Theta-w
         '''
 
-        c = [-1.00922292e-10,-1.47945344e-8,-1.7303757e-6,-0.00012709,1.15849867e-6,-3.518296861e-9,3.5741522e-12]
-        d = [0.0,-3.5223513e-10,-5.7250807e-8,-5.83975422e-6,4.72445163e-8,-1.13402845e-10,8.729580402e-14]
+        c = [-1.00922292e-10, -1.47945344e-8, -1.7303757e-6, -0.00012709, 1.15849867e-6, -3.518296861e-9, 3.5741522e-12]
+        d = [0.0, -3.5223513e-10, -5.7250807e-8, -5.83975422e-6, 4.72445163e-8, -1.13402845e-10, 8.729580402e-14]
 
-        x = min(475.0,thetae_K)
+        x = min(475.0, thetae_K)
 
         if (x <= 335.5):
-           answer = c(0)+x*(c(1)+x*(c(2)+x*(c(3)+x*(c(4)+x*(c(5)+x*c(6))))))
+            answer = c(0)+x*(c(1)+x*(c(2)+x*(c(3)+x*(c(4)+x*(c(5)+x*c(6))))))
         else:
-           answer = d(0)+x*(d(1)+x*(d(2)+x*(d(3)+x*(d(4)+x*(d(5)+x*d(6))))))
+            answer = d(0)+x*(d(1)+x*(d(2)+x*(d(3)+x*(d(4)+x*(d(5)+x*d(6))))))
 
         th_wetb = answer + self.C_2_K
 
         return th_wetb
+#
+# --+---+---+---+
 
-#---+---+---+---+
-
-    def compT_fr_The(self, thelcl_K,pres_Pa):
+    def compT_fr_The(self, thelcl_K, pres_Pa):
 
         '''
         pres_Pa = Pressure in Pascals
@@ -325,34 +326,33 @@ class meteo_utils(object):
         along moist adiabat described by theta-e.
         '''
 
-        guess = (thelcl_K - 0.5 * (max(thelcl_K-270.,0.))**1.05) * (pres_Pa/100000.0)**.2
-        epsilon=0.01
+        guess = (thelcl_K - 0.5 * (max(thelcl_K-270., 0.))**1.05) * (pres_Pa/100000.0)**.2
+        epsilon = 0.01
 
         iter = 1
         while iter < 100:
-            w1 = self.r_sub_s(pres_Pa,guess)
-            w2 = self.r_sub_s(pres_Pa,guess+1.)
-            tenu = self.theta_e(pres_Pa,guess,w1,guess)
-            tenup = self.theta_e(pres_Pa,guess+1,w2,guess+1.)
+            w1 = self.r_sub_s(pres_Pa, guess)
+            w2 = self.r_sub_s(pres_Pa, guess+1.)
+            tenu = self.theta_e(pres_Pa, guess, w1, guess)
+            tenup = self.theta_e(pres_Pa, guess+1, w2, guess+1.)
             cor = (thelcl_K - tenu) / (tenup - tenu)
             guess = guess + cor
-            if( (cor < epsilon) and (-cor < epsilon) ):
-               answer = guess
-               return answer
-            #endif
+            if((cor < epsilon) and (-cor < epsilon)):
+                answer = guess
+                return answer
+            # endif
 
             iter = iter + 1
-        #endwhile
+        # endwhile
 
         '''
         If we come out of the iteration without a solution, then it did not converge. This is not good.
         '''
 
-        print ("WARNING: solution did not converge in compT_fr_The, " + str(thelcl_K) + ", " + str(pres_Pa))
+        print("WARNING: solution did not converge in compT_fr_The, " + str(thelcl_K) + ", " + str(pres_Pa))
 
         thwlcl_K = self.theta_wetb(thelcl_K)
         answer = thwlcl_K*((pres_Pa/100000.0)**0.286)
         return answer
-
-#---+---+---+---+
-
+#
+# --+---+---+---+
