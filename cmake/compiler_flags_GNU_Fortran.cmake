02d5d8c522d3b55ba07abb14893edcb7149056ec
Rahul Mahajan <aerorahul@users.noreply.github.com>
2020-09-12 11:32:06 -0400

    add dep checks and compiler flags for bufr

---
+++ cmake/compiler_flags_GNU_Fortran.cmake
@@ -0,0 +1,45 @@
+# (C) Copyright 2020-2020 NOAA/NWS/NCEP/EMC
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+
+####################################################################
+# FLAGS COMMON TO ALL BUILD TYPES
+####################################################################
+
+if( OpenMP_Fortran_FOUND )
+  set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} OpenMP::OpenMP_Fortran")
+endif()
+
+####################################################################
+# RELEASE FLAGS
+####################################################################
+
+set(CMAKE_Fortran_FLAGS_RELEASE "-O3 -funroll-all-loops -finline-functions")
+
+####################################################################
+# DEBUG FLAGS
+####################################################################
+
+set(CMAKE_Fortran_FLAGS_DEBUG "-O0 -g -fcheck=bounds -ffpe-trap=invalid,zero,overflow,underflow -fbacktrace")
+
+####################################################################
+# BIT REPRODUCIBLE FLAGS
+####################################################################
+
+set(CMAKE_Fortran_FLAGS_BIT "-O2 -funroll-all-loops -finline-functions")
+
+####################################################################
+# LINK FLAGS
+####################################################################
+
+set(CMAKE_Fortran_LINK_FLAGS "")
+
+####################################################################
+
+# Meaning of flags
+# ----------------
+# -fstack-arrays     : Allocate automatic arrays on the stack (needs large stacksize!!!)
+# -funroll-all-loops : Unroll all loops
+# -fcheck=bounds     : Bounds checking
+
---
+++ cmake/compiler_flags_Intel_Fortran.cmake
@@ -0,0 +1,43 @@
+# (C) Copyright 2020-2020 NOAA/NWS/NCEP/EMC
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+
+####################################################################
+# FLAGS COMMON TO ALL BUILD TYPES
+####################################################################
+
+if( OpenMP_Fortran_FOUND )
+  set(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} OpenMP::OpenMP_Fortran")
+endif()
+
+####################################################################
+# RELEASE FLAGS
+####################################################################
+
+set(CMAKE_Fortran_FLAGS_RELEASE "-O3 -ip -unroll -inline -no-heap-arrays")
+
+####################################################################
+# DEBUG FLAGS
+####################################################################
+
+set(CMAKE_Fortran_FLAGS_DEBUG "-O0 -g -check bounds -traceback -warn -heap-arrays -fpe-all=0 -fpe:0 -check all")
+
+####################################################################
+# BIT REPRODUCIBLE FLAGS
+####################################################################
+
+set(CMAKE_Fortran_FLAGS_BIT "-O2 -ip -ipo -unroll -inline -no-heap-arrays")
+
+####################################################################
+# LINK FLAGS
+####################################################################
+
+set(CMAKE_Fortran_LINK_FLAGS "")
+
+####################################################################
+
+# Meaning of flags
+# ----------------
+# todo
+
