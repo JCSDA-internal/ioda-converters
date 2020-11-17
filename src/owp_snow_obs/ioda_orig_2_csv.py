ad11109d6526cfa95ceaf10431bee5000f5c46da
James <jamesmcc@ucar.edu>
2020-11-17 21:27:29 +0000

    csv converter prototype

---
+++ src/owp_snow_obs/ioda_orig_2_csv.py
@@ -0,0 +1,14 @@
+import xarray as xr
+ds = xr.open_dataset('../../test/testoutput/owp_snow_obs.nc')
+df = ds.to_dataframe().rename(columns={
+    "snow_depth@ObsValue": "ObsValue",
+    "snow_depth@ObsError": "ObsError",
+    "snow_depth@PreQC": "PreQC",
+    "time@MetaData": "time",
+    "latitude@MetaData": "latitude",
+    "longitude@MetaData": "longitude",
+    "datetime@MetaData": "datetime",
+    "variable_names@VarMetaData": "variable_name"})
+df['datetime'] = df['datetime'].str.decode("utf-8")
+df['variable_name'] = df['variable_name'].str.decode("utf-8")
+df.to_csv('../../test/testinput/owp_snow_obs_prelim.csv', index=False)
---
+++ test/testoutput/owp_snow_obs_csv.nc
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c9fa4ae77b19638d633d2630bbceffda2dcf9bd72cee833d1ffc7685065242d5
+size 16298
