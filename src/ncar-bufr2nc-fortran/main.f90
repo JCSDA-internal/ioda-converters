d55b32d6775d5082b515fe5ae09d153482492c84
Jamie Bresch <hclin@ucar.edu>
2020-11-24 10:04:00 -0700

    add flexibility to convert a subset of files through command arguments
    modified:   src/ncar-bufr2nc-fortran/README
    modified:   src/ncar-bufr2nc-fortran/main.f90

--- src/ncar-bufr2nc-fortran/main.f90
+++ src/ncar-bufr2nc-fortran/main.f90
@@ -9,10 +9,20 @@ use gnssro_bufr2ioda, only: read_write_gnssro
 
 implicit none
 
-character (len=10) :: filename
-character (len=10) :: filedate
-logical            :: fexist
-logical            :: do_radiance
+integer, parameter      :: StrLen    = 10
+integer, parameter      :: DateLen   = 10
+integer, parameter      :: nfile_all = 4
+character(len=StrLen)   :: flist_all(nfile_all) = (/ "gnssrobufr", &
+                                                     "prepbufr  ", &
+                                                     "amsuabufr ", &
+                                                     "airsbufr  " /)
+character (len=StrLen), allocatable :: flist(:)
+character (len=StrLen)  :: filename
+character (len=DateLen) :: filedate
+logical                 :: fexist
+logical                 :: do_radiance
+integer                 :: nfile
+integer                 :: ifile
 
 ! initialize MPI ( from mpisetup)
 ! sets nproc, numproc (from mpisetup),
@@ -21,51 +31,75 @@ call mpi_initialize
 
 do_radiance = .false. ! initialize
 
-filename = 'gnssrobufr'
-inquire(file=filename, exist=fexist)
-if ( .not. fexist ) then
-   write(*,*) 'Warning: ', trim(filename), ' not found for decoding...'
+nfile = command_argument_count()
+if ( nfile > 0 ) then
+   allocate(flist(nfile))
+   do ifile = 1, nfile
+      call get_command_argument(number=ifile, value=flist(ifile))
+   end do
 else
-   write(*,*) '--- processing gnssrobufr ---'
-   call read_write_gnssro(filename)
+   nfile = nfile_all
+   allocate(flist(nfile_all))
+   flist(:) = flist_all(:)
 end if
 
-filename = 'prepbufr'
-inquire(file=filename, exist=fexist)
-if ( .not. fexist ) then
-   write(*,*) 'Warning: ', trim(filename), ' not found for decoding...'
-else
-   ! read prepbufr file and store data in sequential linked list for conv obs
-   call read_prepbufr(filename, filedate)
+do ifile = 1, nfile
 
-   call filter_obs_conv
+   filename = 'gnssrobufr'
+   if ( trim(flist(ifile)) == trim(filename) ) then
+      inquire(file=filename, exist=fexist)
+      if ( .not. fexist ) then
+         write(*,*) 'Warning: ', trim(filename), ' not found for decoding...'
+      else
+         write(*,*) '--- processing gnssrobufr ---'
+         call read_write_gnssro(filename)
+      end if
+   end if
 
-   ! transfer info from limked list to arrays grouped by obs/variable types
-   call sort_obs_conv
+   filename = 'prepbufr'
+   if ( trim(flist(ifile)) == trim(filename) ) then
+      inquire(file=filename, exist=fexist)
+      if ( .not. fexist ) then
+         write(*,*) 'Warning: ', trim(filename), ' not found for decoding...'
+      else
+         ! read prepbufr file and store data in sequential linked list for conv obs
+         call read_prepbufr(filename, filedate)
 
-   ! write out netcdf files
-   call write_obs(filedate, write_nc_conv)
-end if
+         call filter_obs_conv
 
-filename = 'amsuabufr'
-inquire(file=filename, exist=fexist)
-if ( .not. fexist ) then
-   write(*,*) 'Warning: ', trim(filename), ' not found for decoding...'
-else
-   do_radiance = .true.
-   ! read bufr file and store data in sequential linked list for radiances
-   call read_amsua_amsub_mhs(filename, filedate)
-end if
+         ! transfer info from limked list to arrays grouped by obs/variable types
+         call sort_obs_conv
 
-filename = 'airsbufr'
-inquire(file=filename, exist=fexist)
-if ( .not. fexist ) then
-   write(*,*) 'Warning: ', trim(filename), ' not found for decoding...'
-else
-   do_radiance = .true.
-   ! read bufr file and store data in sequential linked list for radiances
-   call read_airs_colocate_amsua(filename, filedate)
-end if
+         ! write out netcdf files
+         call write_obs(filedate, write_nc_conv)
+      end if
+   end if
+
+   filename = 'amsuabufr'
+   if ( trim(flist(ifile)) == trim(filename) ) then
+      inquire(file=filename, exist=fexist)
+      if ( .not. fexist ) then
+         write(*,*) 'Warning: ', trim(filename), ' not found for decoding...'
+      else
+         do_radiance = .true.
+         ! read bufr file and store data in sequential linked list for radiances
+         call read_amsua_amsub_mhs(filename, filedate)
+      end if
+   end if
+
+   filename = 'airsbufr'
+   if ( trim(flist(ifile)) == trim(filename) ) then
+      inquire(file=filename, exist=fexist)
+      if ( .not. fexist ) then
+         write(*,*) 'Warning: ', trim(filename), ' not found for decoding...'
+      else
+         do_radiance = .true.
+         ! read bufr file and store data in sequential linked list for radiances
+         call read_airs_colocate_amsua(filename, filedate)
+      end if
+   end if
+
+end do ! nfile list
 
 if ( do_radiance ) then
    ! transfer info linked list to arrays grouped by satellite instrument types
@@ -74,6 +108,8 @@ if ( do_radiance ) then
    ! write out netcdf files
    call write_obs(filedate, write_nc_radiance)
 end if
+
+if ( allocated(flist) ) deallocate(flist)
 
 ! finalize MPI
 call mpi_cleanup ! from mpisetup
