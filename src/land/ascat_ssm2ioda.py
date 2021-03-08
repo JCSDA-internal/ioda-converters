9ad14b9c902e6d86e9074e7e0e2de950c884d814
YoulongXia-NOAA <Youlong.Xia@noaa.gov>
2021-03-08 14:32:15 -0500

    Modify ascat_ssm2ioda.py based on the reviewer's comments

--- src/land/ascat_ssm2ioda.py
+++ src/land/ascat_ssm2ioda.py
@@ -62,7 +62,7 @@ class ascat(object):
         # open input file name
         ncd = nc.Dataset(self.filename, 'r')
         # set and get global attributes
-        AttrData["satellite"] = "METOp-A"
+        AttrData["satellite"] = "METOp-A/METOp-B"
         AttrData['sensor'] = ncd.getncattr('instrument_name')
         AttrData['platform'] = ncd.getncattr('platform_long_name')
 
