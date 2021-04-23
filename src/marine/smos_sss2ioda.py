e36c341900d536b768ab867f62864329c9ae156f
Shastri Paturi <shastri.paturi@noaa.gov>
2021-04-23 08:57:31 -0500

    corrected error in smos_sss2ioda.py

--- src/marine/smos_sss2ioda.py
+++ src/marine/smos_sss2ioda.py
@@ -14,7 +14,7 @@ from datetime import datetime, timedelta
 import netCDF4 as nc
 from pathlib import Path
 
-IODA_CONV_PATH = Path(__file__).parent/"@SCRIPT_LIB_PATH@
+IODA_CONV_PATH = Path(__file__).parent/"@SCRIPT_LIB_PATH@"
 if not IODA_CONV_PATH.is_dir():
     IODA_CONV_PATH = Path(__file__).parent/'..'/'lib-python'
 sys.path.append(str(IODA_CONV_PATH.resolve()))
