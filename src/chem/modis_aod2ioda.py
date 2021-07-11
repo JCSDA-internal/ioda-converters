a67ff8804e6c6baf5b685d6531ae4b82fed7fa23
Andrew.Tangborn <Andrew.Tangborn@noaa.gov>
2021-07-11 23:47:17 +0000

    This is a revision of the previous pull request, with a number of corrections. These are mainly related to changing dtype from float64 to float32, and some unneeded lines of code.

--- src/chem/modis_aod2ioda.py
+++ src/chem/modis_aod2ioda.py
@@ -58,9 +58,6 @@ class AOD(object):
             self.varDict[iodavar]['errKey'] = iodavar, self.writer.OerrName()
             self.varDict[iodavar]['qcKey'] = iodavar, self.writer.OqcName()
 
-        # Set up modis time
-
-        modis_time = {}
 
         # loop through input filenames
         first = True
@@ -79,20 +76,20 @@ class AOD(object):
 
             # convert time to date_string
             lats = ncd.variables['Latitude'][:].ravel()
+            lats = lats.astype('float32')
             lons = ncd.variables['Longitude'][:].ravel()
+            lons = lons.astype('float32')
             aod = ncd.variables['AOD_550_Dark_Target_Deep_Blue_Combined'][:].ravel()
             land_sea_flag = ncd.variables['Land_sea_Flag'][:].ravel()
             QC_flag = ncd.variables['Land_Ocean_Quality_Flag'][:].ravel()
+            QC_flag = QC_flag.astype('int8')
             sol_zen = ncd.variables['Solar_Zenith'][:].ravel()
-            sol_zen = sol_zen
             sen_zen = ncd.variables['Sensor_Zenith'][:].ravel()
-            sen_zen = sen_zen
             unc_land = ncd.variables['Deep_Blue_Aerosol_Optical_Depth_550_Land_Estimated_Uncertainty'][:].ravel()
-            unc_land = unc_land
 
-            time_1 = timedelta(seconds=1)
-            obs_time = np.empty_like(QC_flag, dtype=object)
-            obs_time = datetime.fromisoformat('1993-01-01') + modis_time*time_1
+            
+            modis_time_nan=np.ma.filled(modis_time.astype(float), 0.0)
+            obs_time = [datetime.fromisoformat('1993-01-01') + timedelta(seconds = x) for x in modis_time_nan]
 
             # uncertainty estimates:
             # From MODIS file (over ocean) and Levy, 2010 (over land)
@@ -101,36 +98,23 @@ class AOD(object):
             over_ocean = np.logical_not(land_sea_flag > 0)
             over_land = np.logical_not(land_sea_flag == 0)
             UNC = np.where(over_land, unc_land, np.add(0.05, np.multiply(0.15, aod)))
-            UNC_2 = UNC[0]
 
             if first:
                 self.loc_mdata['latitude'] = lats
                 self.loc_mdata['longitude'] = lons
                 self.loc_mdata['datetime'] = self.writer.FillNcVector(obs_time, "datetime")
-                self.loc_mdata['latitude'] = np.concatenate((self.loc_mdata['latitude'], lats[0:1]))
-                self.loc_mdata['longitude'] = np.concatenate((self.loc_mdata['longitude'], lons[0:1]))
-                self.loc_mdata['datetime'] = np.concatenate((self.loc_mdata['datetime'],
-                                                            self.writer.FillNcVector(obs_time[0:1], "datetime")))
             else:
                 self.loc_mdata['latitude'] = np.concatenate((self.loc_mdata['latitude'], lats))
                 self.loc_mdata['longitude'] = np.concatenate((self.loc_mdata['longitude'], lons))
-                self.loc_mdata['datetime'] = np.concatenate((self.loc_mdata['datetime'],
-                                                            self.writer.FillNcVector(obs_time, "datetime")))
+                self.loc_mdata['datetime'] = np.concatenate((self.loc_mdata['datetime'], self.writer.FillNcVector(obs_time, "datetime")))
 
             for ncvar, iodavar in obsvars.items():
                 data = aod.astype('float32')
                 err = UNC
-                err_2 = err[0]
                 if first:
-                    self.outdata[self.varDict[iodavar]['valKey']] = aod
+                    self.outdata[self.varDict[iodavar]['valKey']] = data 
                     self.outdata[self.varDict[iodavar]['errKey']] = err
                     self.outdata[self.varDict[iodavar]['qcKey']] = QC_flag
-                    self.outdata[self.varDict[iodavar]['valKey']] = np.concatenate(
-                        (self.outdata[self.varDict[iodavar]['valKey']], data[0:1]))
-                    self.outdata[self.varDict[iodavar]['errKey']] = np.concatenate(
-                        (self.outdata[self.varDict[iodavar]['errKey']], err[0:1]))
-                    self.outdata[self.varDict[iodavar]['qcKey']] = np.concatenate(
-                        (self.outdata[self.varDict[iodavar]['qcKey']], QC_flag[0:1]))
 
                 else:
                     self.outdata[self.varDict[iodavar]['valKey']] = np.concatenate(
@@ -149,6 +133,7 @@ def main():
 
     # get command line arguments
     # Usage: python blah.py -i /path/to/obs/2021060801.nc /path/to/obs/2021060802.nc ... /path/to/obs/2021060823.nc
+    # -o /path/to/ioda/20210608.nc 
     # where the input obs could be for any desired interval to concatenated together.
     parser = argparse.ArgumentParser(
         description=(
