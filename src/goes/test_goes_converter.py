b23421caf0f77daa20cde53f959a8f4f841e03de
eric2 <eric2@ucar.edu>
2021-07-19 16:52:12 -0600

    Removed hard-coded paths from the test driver script.

--- src/goes/test_goes_converter.py
+++ src/goes/test_goes_converter.py
@@ -1,27 +1,22 @@
 #
 # test_goes_converter.py
 #
-# This script is a driver for the GoesConverter class.
+# This script is a test driver for the GoesConverter class.
 #
 #
-import os
 import time
-
-from solo.basic_files import tree
 from goes_converter import GoesConverter
 
 
-def test_goes_converter():
-    input_files_path = "/Users/eric2/PROJECTS/GOES16_TEST_FILES/input/12_15_2020/00"
-    latlon_file_path = "/Users/eric2/PROJECTS/GOES16_TEST_FILES/latlon/goes16_latlon_2km.nc"
-    output_file_path_rf = "/Users/eric2/PROJECTS/GOES16_TEST_FILES/output/12_15_2020/00/goes16_rf.nc"
-    output_file_path_bt = "/Users/eric2/PROJECTS/GOES16_TEST_FILES/output/12_15_2020/00/goes16_bt.nc"
-
-    input_file_paths = []
-    for path, root, filename in tree(input_files_path):
-        if not filename.startswith("."):
-            filepath = os.path.join(path, root, filename)
-            input_file_paths.append(filepath)
+def test_goes_converter(input_file_paths, latlon_file_path, output_file_path_rf, output_file_path_bt):
+    """
+    Test driver for the GoesConverter class.
+    input_file_paths - a list containing 16 absolute paths corresponding to the output files for each channel of a
+        single GOERS_16 or GOES-17 scan
+    latlon_file_path - The path to an existing Goes LatLon file or if it does not exist the path to write the file
+    output_file_path_rf - The path to write the IODAv2 reflectance factor data file
+    output_file_path_bt - The path to write the IODAv2 brightness temperature data file
+    """
     goes_converter = GoesConverter(input_file_paths, latlon_file_path, output_file_path_rf, output_file_path_bt)
     goes_converter.convert()
 
