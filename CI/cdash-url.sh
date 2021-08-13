c9981272082bb575ea90cf320fc81fd56eefb45b
Maryam Abdi-Oskouei <maryamao@ucar.edu>
2021-08-13 10:36:12 -0600

    upload build report to cdash

--- CI/cdash-url.sh
+++ CI/cdash-url.sh
@@ -4,5 +4,5 @@ dir=$1
 tag=$(head -1 $dir/TAG)
 Done=$(cat $dir/$tag/Done.xml)
 buildID=$(echo $Done | grep -o -P '(?<=buildId>).*(?=</build)')
-URL=https://cdash.jcsda.org/viewTest.php?buildid=$buildID
+URL=https://cdash.jcsda.org/buildSummary.php?buildid=$buildID
 echo $URL
