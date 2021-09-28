a165b8f5d9d71b459fab0154b1404607812f043d
bhuang95 <huangbo9955@gmail.com>
2021-09-28 23:34:51 +0000

    Add src/chem/aeronet_aaod2ioda.py

---
+++ src/chem/aeronet_aaod2ioda.py
@@ -0,0 +1,219 @@
+#!/usr/bin/env python3
+
+# Description:
+#        This code reads ASCII files of AERONET inversion products
+#        downloaded from from NASA websit
+#        (https://aeronet.gsfc.nasa.gov/print_web_data_help_v3_inv_new.html)
+#        and converts to IODA format.
+#        Inversion products include conicident AOT data with almucantar
+#        retrieval (CAD), single scattering albedo (SSA),
+#        AOD aborption (TAB) for inversion types of ALM15 or ALM20 and
+#        at wavelengths of 440/675/870/1020nm)
+#
+# Usage:
+#        python aeronet_aaod2ioda.py -i testinput -o aeronet_aaod.nc
+#        -i: input directory to AERONET inversion files
+#            (aeronet_cad.dat, aeronet_ssa.dat, aeronet_tab.dat)
+#        -o: output IODA file
+#
+# Contact:
+#        Bo Huang (bo.huang@noaa.gov) from CU/CIRES and NOAA/ESRL/GSL
+#        (September 28, 2021)
+#
+# Acknowledgement:
+#        Barry Baker from ARL for his initial preparation for this code.
+
+
+import netCDF4 as nc
+import numpy as np
+import inspect, sys, os, argparse
+import pandas as pd
+from datetime import datetime, timedelta
+from builtins import object, str
+from numpy import NaN
+from pathlib import Path
+
+
+IODA_CONV_PATH = Path(__file__).parent/"@SCRIPT_LIB_PATH@"
+if not IODA_CONV_PATH.is_dir():
+    IODA_CONV_PATH = Path(__file__).parent/'..'/'lib-python'
+sys.path.append(str(IODA_CONV_PATH.resolve()))
+import meteo_utils
+import ioda_conv_ncio as iconv
+from collections import defaultdict, OrderedDict
+from orddicts import DefaultOrderedDict
+
+
+def dateparse(x):
+    return datetime.strptime(x, '%d:%m:%Y %H:%M:%S')
+
+
+def add_data(infile):
+    df = pd.read_csv(infile,
+                     engine='python',
+                     header=None,
+                     skiprows=7,
+                     parse_dates={'time': [1, 2]},
+                     date_parser=dateparse,
+                     na_values=-999)
+    header = pd.read_csv(infile, skiprows=6, header=None,
+                         nrows=1).values.flatten()
+
+    cols = ['time']
+    for i in header:
+        if "Date(" in i or 'Time(' in i:
+            if "Last_Processing_Date(" in i or "Last_Processing_Time(" in i:
+                cols.append(i.lower())
+            else:
+                pass
+        else:
+            cols.append(i.lower())
+    df.columns = cols
+    df.index = df.time
+    df.rename(columns={
+        'latitude(degrees)': 'latitude',
+        'longitude(degrees)': 'longitude',
+        'elevation(m)': 'elevation',
+        'aeronet_site': 'siteid'
+    },
+        inplace=True)
+    return df
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description=(
+                     'Reads AERONET inversion files downloaded from NASA website '
+                     ' and converts into IODA format')
+    )
+
+    required = parser.add_argument_group(title='required arguments')
+    required.add_argument(
+                    '-i', '--input',
+                    help="path of AERONET inversion ASCII files that include "
+                         " aeronet_cad.dat, aeronet_ssa.dat and aeronet_tab.dat",
+                    type=str, required=True) 
+    required.add_argument(
+                    '-o', '--output',
+                    help="path and filename of AERONET inversion IODA file",
+                    type=str, required=True)
+
+    args = parser.parse_args()
+    indir = args.input
+    outfile = args.output
+
+    # Read and extract online AERONET inversion data
+    print('Read and extract AERONET inversion data: CAD, SSA, TAB')
+    f3_cad_all = add_data('%s/aeronet_cad.dat' % (indir))
+    f3_ssa_all = add_data('%s/aeronet_ssa.dat' % (indir))
+    f3_tab_all = add_data('%s/aeronet_tab.dat' % (indir))
+    f3_cad = f3_cad_all[['time', 'siteid', 'longitude', 'latitude', 'elevation',
+                         'if_retrieval_is_l2(without_l2_0.4_aod_440_threshold)',
+                         'if_aod_is_l2', 'inversion_data_quality_level',
+                         'aod_coincident_input[440nm]', 'aod_coincident_input[675nm]',
+                         'aod_coincident_input[870nm]', 'aod_coincident_input[1020nm]']]
+    f3_ssa = f3_ssa_all[['single_scattering_albedo[440nm]', 'single_scattering_albedo[675nm]',
+                         'single_scattering_albedo[870nm]', 'single_scattering_albedo[1020nm]']]
+    f3_tab = f3_tab_all[['absorption_aod[440nm]', 'absorption_aod[675nm]',
+                         'absorption_aod[870nm]', 'absorption_aod[1020nm]']]
+    f3 = pd.concat([f3_cad, f3_ssa, f3_tab], axis=1, join='inner')
+
+    # Define wavelengths, channels and frequencies of AERONET inversion data
+    aeronetinv_wav = np.array([440., 675, 870., 1020.], dtype=np.float32)
+    aeronetinv_chan = np.array([3, 5, 6, 7], dtype=np.intc)
+    speed_light = 2.99792458E8
+    frequency = speed_light*1.0E9/aeronetinv_wav
+    print('Output AERONET inverion data at wavelengths/channels/frequencies: ')
+    print(aeronetinv_wav)
+    print(aeronetinv_chan)
+    print(frequency)
+
+    nlocs, columns = f3.shape
+    if nlocs == 0:
+        print('No AERONET inversion data available in input files')
+        exit(0)
+
+    locationKeyList = [("latitude", "float"), ("longitude", "float"), ("datetime", "string"),]
+    writer = iconv.NcWriter(outfile, locationKeyList)
+    varDict = defaultdict(lambda: defaultdict(dict))
+    outdata = defaultdict(lambda: DefaultOrderedDict(OrderedDict))
+    loc_mdata = defaultdict(lambda: DefaultOrderedDict(OrderedDict))
+    var_mdata = defaultdict(lambda: DefaultOrderedDict(OrderedDict))
+    units = {}
+    units['latitude'] = 'degree'
+    units['longitude'] = 'degree'
+    units['station_elevation'] = 'm'
+
+    # Define AERONET inversion varname that match with those in f3
+    # (e.g., match aaeronetinv_wav and aeronetinv_chan)
+    obsvars = {'aerosol_optical_depth_3': 'aod_coincident_input[440nm]',
+               'aerosol_optical_depth_5': 'aod_coincident_input[675nm]',
+               'aerosol_optical_depth_6': 'aod_coincident_input[870nm]',
+               'aerosol_optical_depth_7': 'aod_coincident_input[1020nm]',
+               'single_scattering_albedo_3': 'single_scattering_albedo[440nm]',
+               'single_scattering_albedo_5': 'single_scattering_albedo[675nm]',
+               'single_scattering_albedo_6': 'single_scattering_albedo[870nm]',
+               'single_scattering_albedo_7': 'single_scattering_albedo[1020nm]',
+               'absorption_aerosol_optical_depth_3': 'absorption_aod[440nm]',
+               'absorption_aerosol_optical_depth_5': 'absorption_aod[675nm]',
+               'absorption_aerosol_optical_depth_6': 'absorption_aod[870nm]',
+               'absorption_aerosol_optical_depth_7': 'absorption_aod[1020nm]'}
+
+    # Define varDict variables
+    print('Define varDict variables')
+    for key, value in obsvars.items():
+        print(key, value)
+        varDict[key]['valKey'] = key, writer.OvalName()
+        varDict[key]['errKey'] = key, writer.OerrName()
+        varDict[key]['qcKey'] = key, writer.OqcName()
+
+    # Define loc_mdata
+    print('Define loc_mdata')
+    loc_mdata['latitude'] = np.array(f3['latitude'])
+    loc_mdata['longitude'] = np.array(f3['longitude'])
+    loc_mdata['station_elevation'] = np.array(f3['elevation'])
+    loc_mdata['surface_type'] = np.full((nlocs), 1)
+    # Whether inversion data pass Level 2.0 QC without the threshold of aod_440>=0.4 (0:yes, 1: no)
+    loc_mdata['l20_without_aod440_0.4_threshold_qc'] = np.where(f3['if_retrieval_is_l2(without_l2_0.4_aod_440_threshold)'] == 1,
+                                                                0, 1)
+    # Whether inversion data pass Level 2.0 QC with the threshold of aod_440>=0.4 (0:yes, 1: no)
+    loc_mdata['l20_qc'] = np.where(f3['if_aod_is_l2'] == 1, 0, 1)
+    # AERONET inversion data QC level (0 for Level 2.0 and 1 for Level 1.5)
+    loc_mdata['inversion_l20_qc'] = np.where(f3['inversion_data_quality_level'] == 'lev20', 0, 1)
+
+    c = np.empty([nlocs], dtype='S50')
+    c[:] = np.array(f3.siteid)
+    loc_mdata['station_id'] = writer.FillNcVector(c, 'string')
+
+    # Define datetime
+    d = np.empty([nlocs], 'S20')
+    for i in range(nlocs):
+        d[i] = f3.time[i].strftime('%Y-%m-%dT%H:%M:%SZ')
+    loc_mdata['datetime'] = writer.FillNcVector(d, 'datetime')
+
+    # Define var_mdata
+    print('Define var_mdata')
+    var_mdata['frequency'] = writer.FillNcVector(frequency, 'float')
+    var_mdata['sensor_channel'] = writer.FillNcVector(aeronetinv_chan, 'integer')
+
+    for key, value in obsvars.items():
+        outdata[varDict[key]['valKey']] = np.array(f3[value].fillna(nc.default_fillvals['f4']))
+        outdata[varDict[key]['qcKey']] = np.where(outdata[varDict[key]['valKey']] == nc.default_fillvals['f4'],
+                                                  1, 0)
+        if key in ["aerosol_optical_depth_3", "aerosol_optical_depth_5", "aerosol_optical_depth_6", "aerosol_optical_depth_7"]:
+            outdata[varDict[key]['errKey']] = np.where(outdata[varDict[key]['valKey']] == nc.default_fillvals['f4'],
+                                                       nc.default_fillvals['f4'], 0.02)
+        else:
+            outdata[varDict[key]['errKey']] = np.array(nc.default_fillvals['f4'], dtype=np.float32)
+
+    # Define global atrributes
+    print('Define global atrributes')
+    AttrData = {'observation_type': 'AERONET inversion data',
+		'sensor': "aeronet",
+		'surface_type': 'ocean=0, land=1, costal=2'}
+
+    # Write out IODA V1 NC files
+    print('Write into IODA format file: ' + outfile)
+    writer._nvars = len(aeronetinv_wav)
+    writer._nlocs = nlocs
+    writer.BuildNetcdf(outdata, loc_mdata, var_mdata, AttrData, units)
---
+++ test/testoutput/aeronet_aaod.nc
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:44840476ac30e9586099593b30d4dfc52d7528facd3c6bbfdaec0d9dee2c98d0
+size 49134
