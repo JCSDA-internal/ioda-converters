df87cb41f8094d0bad0198c6228ab3ad6c981b89
James <jamesmcc@ucar.edu>
2021-10-18 18:26:59 +0000

    owp class

--- src/owp_snow_obs/owp_snow_obs_csv_class.py
+++ src/owp_snow_obs/owp_snow_obs_csv_class.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python3
 
+# (C) Copyright 2019 UCAR
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+
+# Purpose: Convert OWP snow observations from CSV format to IODA netcdf format.
+# Author:
+# 2020-11-17, 2021-10-18: James McCreight
+
+# Conceptual Figure:
+# TODO: this is probably out of date... a
+# https://jointcenterforsatellitedataassimilation-jedi-docs.readthedocs-hosted.com/en/latest/_images/IODA_InMemorySchematic.png
+
+# Example testing usage:
+# ipython --pdb -c"%run
+#     owp_snow_obs_csv_class.py
+#     -i $test_input/owp_snow_obs.csv -o $here/owp_snow_obs_csv.nc --thin_swe 1"
+
 from datetime import datetime
-# from multiprocessing import Pool
 import numpy as np
 import os
 import pandas as pd
 import pathlib
 import sys
 from pathlib import Path
-
 
 IODA_CONV_PATH = Path(__file__).parent/"@SCRIPT_LIB_PATH@"
 if not IODA_CONV_PATH.is_dir():
@@ -18,25 +33,6 @@ sys.path.append(str(IODA_CONV_PATH.resolve()))
 import ioda_conv_engines as iconv
 from collections import defaultdict, OrderedDict
 from orddicts import DefaultOrderedDict
-
-# (C) Copyright 2019 UCAR
-# This software is licensed under the terms of the Apache Licence Version 2.0
-# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
-
-# Purpose: Convert OWP snow observations from CSV format to IODA netcdf format.
-# Author:
-# 2020-11-17: James McCreight
-
-# Conceptual Figure:
-# TODO: this is probably out of date... a
-# https://jointcenterforsatellitedataassimilation-jedi-docs.readthedocs-hosted.com/en/latest/_images/IODA_InMemorySchematic.png
-
-# Example testing usage:
-# ipython --pdb -c %run owp_snow_obs_csv_2_ioda.py \
-#     -i ../../test/# testinput/owp_snow_obs.csv  \
-#     -o_depth owp_snow_obs_depth_csv.nc \
-#     -o_swe owp_snow_obs_swe_csv.nc \
-#     -d 2018100100 -t .3
 
 arg_parse_description = (
     """Reads snow OWP observations in CSV files and converts
@@ -65,10 +61,11 @@ attr_data = {
 
 
 class OwpSnowObs(object):
-    def __init__(self, file_in, file_out):  #, thin):
+    def __init__(self, file_in, file_out, thin_swe, thin_depth):
         self.file_in = file_in
         self.file_out = file_out
-        # self.thin = thin
+        self.thin_swe = thin_swe
+        self.thin_depth = thin_depth
 
         self.var_dict = defaultdict(lambda: defaultdict(dict))
         self.meta_dict = defaultdict(lambda: defaultdict(dict))
@@ -82,7 +79,9 @@ class OwpSnowObs(object):
     def _read(self):
         print(f"Reading: {self.file_in}")
         self.attr_data['obs_file'] = str(self.file_in)
+        # use pandas to get the data lined up
         obs_df = pd.read_csv(self.file_in, header=0, index_col=False)
+        # TODO: drop rows where PreQC != 0 ?
         data_cols = set(['ObsValue', 'ObsError', 'PreQC'])
         index_cols = list(
             set(obs_df.columns.values.tolist()).difference(data_cols))
@@ -90,28 +89,38 @@ class OwpSnowObs(object):
         obs_df = obs_df.unstack('variable_name').reset_index()
         obs_df.columns = [' '.join(col).strip() for col in obs_df.columns.values]
 
-        # Do we want to think both variable the same amount?
-        # if self.thin > 0:
-        #     # Set a seed for each day - reproducibly random
-        #     time_diff = (
-        #         pd.Timestamp(self.attr_data['ref_date_time']) -
-        #         pd.Timestamp('2014-01-23 00:00:00', tz='UTC'))
-        #     np.random.seed(int(time_diff.total_seconds()))
-        #     randoms = np.random.uniform(size=len(obs_df))
-        #     thin_quantile = np.quantile(randoms, self.thin)
-        #     thin_inds = np.where(randoms <= thin_quantile)[0]  # 1-D
-        #     obs_df = obs_df.drop(thin_inds)
-
-        # TODO: drop rows where PreQC != 0 ?
-
         self.attr_data['ref_date_time'] = (
             pd.to_datetime(obs_df.datetime[0])
             .round('D')
             .strftime('%Y-%m-%dT%H:%M:%SZ'))
 
-        self.data[('datetime', 'MetaData')] = obs_df.datetime
-        self.data[('latitude', 'MetaData')] = obs_df.latitude
-        self.data[('longitude', 'MetaData')] = obs_df.longitude
+        if self.thin_swe == 1.0 and self.thin_depth == 1.0:
+            print('No output requested, but variables thined by 100%')
+            return(None)
+        self.attr_data['thin_swe'] = self.thin_swe
+        self.attr_data['thin_depth'] = self.thin_depth
+        thin_dict = {
+            'snow_water_equivalent_mm': self.thin_swe,
+            'snow_depth_mm': self.thin_depth}
+        # Set a seed for each day - reproducibly random
+        time_diff = (
+            pd.Timestamp(self.attr_data['ref_date_time']) -
+            pd.Timestamp('1979-01-01 00:00:00', tz='UTC'))
+        np.random.seed(int(time_diff.total_seconds()))
+        for var, thin in thin_dict.items():
+            if thin > 0:
+                wh_var = np.where(~np.isnan(obs_df[f'ObsValue {var}']))[0] # 1-D
+                randoms = np.random.uniform(size=len(wh_var))
+                thin_quantile = np.quantile(randoms, thin)
+                thin_inds = np.where(randoms <= thin_quantile)[0]
+                obs_df = obs_df.drop(wh_var[thin_inds])
+                if all(np.isnan(obs_df['ObsValue snow_water_equivalent_mm'])):
+                    # If there's no data, drop the variable from the output
+                    del output_var_dict[var]
+
+        self.data[('datetime', 'MetaData')] = obs_df.datetime.values
+        self.data[('latitude', 'MetaData')] = obs_df.latitude.values
+        self.data[('longitude', 'MetaData')] = obs_df.longitude.values
 
         for obsvar, iodavar in output_var_dict.items():
             # define the ioda variable
@@ -122,9 +131,9 @@ class OwpSnowObs(object):
             self.units[iodavar] = output_var_unit_dict[iodavar]
             self.var_metadata[iodavar]['coordinates'] = 'longitude latitude'
             # the data
-            self.data[self.var_dict[iodavar]['valKey']] = obs_df[f'ObsValue {obsvar}']
-            self.data[self.var_dict[iodavar]['errKey']] = obs_df[f'ObsError {obsvar}']
-            self.data[self.var_dict[iodavar]['qcKey']] = obs_df[f'PreQC {obsvar}']
+            self.data[self.var_dict[iodavar]['valKey']] = obs_df[f'ObsValue {obsvar}'].values
+            self.data[self.var_dict[iodavar]['errKey']] = obs_df[f'ObsError {obsvar}'].values
+            self.data[self.var_dict[iodavar]['qcKey']] = obs_df[f'PreQC {obsvar}'].values
 
         nlocs = len(self.data[('datetime', 'MetaData')])
         dim_dict['nlocs'] = nlocs
@@ -153,20 +162,17 @@ def parse_arguments():
         '-o', '--output',
         help="path of IODA output file",
         type=str, required=False)
-    # optional = parser.add_argument_group(title='optional arguments')
-    # optional.add_argument(
-    #     '-t', '--thin',
-    #     help="percentage of random thinning, from 0.0 to 1.0. Zero indicates"
-    #          " no thinning is performed. (default: %(default)s)",
-    #     type=float, default=0.0)
-    # optional.add_argument(
-    #     '--processes',
-    #     # TODO JLM: multiprocessing.Pool provides process based parallelism.
-    #     # TODO JLM: multiprocessing.pool.ThreadPool provides unsupported
-    #     #           thread-based pool parallelism.
-    #     help='multiprocessing.Pool can load input files in parallel.'
-    #          ' (default: %(default)s)',
-    #     type=int, default=1)
+    optional = parser.add_argument_group(title='optional arguments')
+    optional.add_argument(
+        '--thin_swe',
+        help="percentage of random thinning for SWE, from 0.0 to 1.0. Zero indicates"
+             " no thinning is performed. (default: %(default)s)",
+        type=float, default=0.0)
+    optional.add_argument(
+        '--thin_depth',
+        help="percentage of random thinning for snow depth, from 0.0 to 1.0. Zero indicates"
+             " no thinning is performed. (default: %(default)s)",
+        type=float, default=0.0)
 
     args = parser.parse_args()
     return args
@@ -174,6 +180,7 @@ def parse_arguments():
 
 if __name__ == '__main__':
     args = parse_arguments()
-    owp_snow_obs = OwpSnowObs(args.input, args.output)  # , args.thin)
+    owp_snow_obs = OwpSnowObs(
+        args.input, args.output , args.thin_swe, args.thin_depth)
     result = owp_snow_obs.write()
     sys.exit(result)
