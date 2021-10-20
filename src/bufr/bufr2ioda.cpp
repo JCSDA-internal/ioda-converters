129fb532e5fd1144e6464a14cc44699ba34ae566
Ronald McLaren <ronald.mclaren@noaa.gov>
2021-10-20 14:15:59 -0400

    Added arguments to bufr2ioda so you can limit the amount of messages that are parsed.

--- src/bufr/bufr2ioda.cpp
+++ src/bufr/bufr2ioda.cpp
@@ -25,7 +25,7 @@
 
 namespace Ingester
 {
-    void parse(std::string yamlPath)
+    void parse(const std::string& yamlPath, std::size_t numMsgs = 0)
     {
         std::unique_ptr<eckit::YAMLConfiguration>
             yaml(new eckit::YAMLConfiguration(eckit::PathName(yamlPath)));
@@ -42,7 +42,7 @@ namespace Ingester
                 }
 
                 auto parser = ParserFactory::create(obsConf.getSubConfiguration("obs space"));
-                auto data = parser->parse();
+                auto data = parser->parse(numMsgs);
 
                 auto encoder = IodaEncoder(obsConf.getSubConfiguration("ioda"));
                 encoder.encode(data);
@@ -61,15 +61,58 @@ namespace Ingester
 }  // namespace Ingester
 
 
+static void showHelp()
+{
+    std::cerr << "Usage: [-n NUM_MESSAGES] YAML_PATH"
+              << "Options:\n"
+              << "  -h,  Show this help message\n"
+              << "  -n NUM_MESSAGES,  Number of BUFR messages to parse."
+              << std::endl;
+}
+
+
 int main(int argc, char **argv)
 {
     if (argc < 2)
     {
-        eckit::BadParameter("Missing argument. Must include YAML file path.");
+        showHelp();
+        return 0;
+    }
+
+    std::string yamlPath;
+    std::size_t numMsgs = 0;
+
+    std::size_t argIdx = 1;
+    while (argIdx < static_cast<std::size_t> (argc))
+    {
+        if (strcmp(argv[argIdx], "-n") == 0)
+        {
+            if (static_cast<std::size_t> (argc) > argIdx + 1)
+            {
+                numMsgs = atoi(argv[argIdx + 1]);
+            }
+            else
+            {
+                showHelp();
+                return 0;
+            }
+
+            argIdx += 2;
+        }
+        else if (strcmp(argv[argIdx], "-h") == 0)
+        {
+            showHelp();
+            return 0;
+        }
+        else
+        {
+            yamlPath = std::string(argv[argIdx]);
+            argIdx++;
+        }
     }
 
     Ingester::registerParsers();
-    Ingester::parse(std::string(argv[1]));
+    Ingester::parse(yamlPath, numMsgs);
 
     return 0;
 }
