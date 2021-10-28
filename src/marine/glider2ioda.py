2a9a2fd06bb72f6b962e727afd66efff8d2bd560
lliu1220 <lingcasper@gmail.com>
2021-10-28 02:57:38 +0000

    roll back test

--- src/marine/glider2ioda.py
+++ src/marine/glider2ioda.py
@@ -8,13 +8,11 @@
 
 from __future__ import print_function
 import sys
+import argparse
+import netCDF4 as nc
+from datetime import datetime, timedelta
 import numpy as np
 import numpy.matlib
-from datetime import datetime, timedelta
-from scipy.io import FortranFile
-import argparse
-from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
-import netCDF4 as nc
 from pathlib import Path
 
 IODA_CONV_PATH = Path(__file__).parent/"@SCRIPT_LIB_PATH@"
@@ -22,8 +20,7 @@ if not IODA_CONV_PATH.is_dir():
     IODA_CONV_PATH = Path(__file__).parent/'..'/'lib-python'
 sys.path.append(str(IODA_CONV_PATH.resolve()))
 
-import ioda_conv_engines as iconv
-#from collections import defaultdict, OrderedDict
+import ioda_conv_ncio as iconv
 from orddicts import DefaultOrderedDict
 
 vName = [
@@ -38,18 +35,20 @@ locationKeyList = [
     ("datetime", "string")
 ]
 
-#self.GlobalAttrs = {
-#    'odb_version': 1}
+AttrData = {
+    'odb_version': 1,
+}
+
 
 class Profile(object):
-    def __init__(self, filename, date):
+
+    def __init__(self, filename, date, writer):
         self.filename = filename
         self.date = date
         self.data = DefaultOrderedDict(lambda: DefaultOrderedDict(dict))
-        self.varAttrs = DefaultOrderedDict(lambda: DefaultOrderedDict(dict))
+        self.writer = writer
         self._read()
-        #self._rd_glider()
-        return
+
     def _read(self):
         ncd = nc.Dataset(self.filename)
         dpth = (ncd.variables['ctd_depth'][:])
@@ -68,55 +67,53 @@ class Profile(object):
         base_date = datetime(1970, 1, 1)
         for i in range(len(time)-1):
             for j in [0, 1]:
-                valKey = vName[j], iconv.OvalName()
-                errKey = vName[j], iconv.OerrName()
-                qcKey = vName[j], iconv.OqcName()
+                valKey = vName[j], self.writer.OvalName()
+                errKey = vName[j], self.writer.OerrName()
+                qcKey = vName[j], self.writer.OqcName()
                 dt = base_date + timedelta(seconds=int(time[i]))
                 locKey = lats[i], lons[i], dpth[i], dt.strftime(
                     "%Y-%m-%dT%H:%M:%SZ")
                 if j == 0:
-                    self.data[locKey][valKey] = temperature[i]
-                    self.data[locKey][errKey] = errs[i]
-                    self.data[locKey][qcKey] = Tqcs[i]
+                    self.data[0][locKey][valKey] = temperature[i]
+                    self.data[0][locKey][errKey] = errs[i]
+                    self.data[0][locKey][qcKey] = Tqcs[i]
                 else:
-                    self.data[locKey][valKey] = salinity[i]
-                    self.data[locKey][errKey] = errs[i]
-                    self.data[locKey][qcKey] = Sqcs[i]
+                    self.data[0][locKey][valKey] = salinity[i]
+                    self.data[0][locKey][errKey] = errs[i]
+                    self.data[0][locKey][qcKey] = Sqcs[i]
 
 
 def main():
-    desc = 'Convert AOML glider data to IODA netCDF4 format'
-    parser = ArgumentParser(
-        description=desc,
-        formatter_class=ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument(
+    parser = argparse.ArgumentParser(
+        description=(
+            'Read NOAA AOML Hurricane Glider Temperature and Salinity profile observation file(s) '
+        )
+    )
+
+    required = parser.add_argument_group(title='required arguments')
+    required.add_argument(
         '-i', '--input',
         help="name of Glider observation input file(s)",
         type=str, required=True)
-    parser.add_argument(
+    required.add_argument(
         '-o', '--output',
-        help="path of netCDF4 output file",
+        help="path of ioda output file",
         type=str, required=True)
-    parser.add_argument(
+    required.add_argument(
         '-d', '--date',
         help="base date for the center of the window",
         metavar="YYYYMMDDHH", type=str, required=True)
     args = parser.parse_args()
     fdate = datetime.strptime(args.date, '%Y%m%d%H')
 
-    #writer = iconv.NcWriter(args.output, locationKeyList)
+    writer = iconv.NcWriter(args.output, locationKeyList)
 
-    prof = Profile(args.input, fdate)
-    #AttrData['date_time_string'] = fdate.strftime("%Y-%m-%dT%H:%M:%SZ")
-    #(ObsVars, LocMdata, VarMdata) = writer.ExtractObsData(prof.data)
-    ObsVars, nlocs = iconv.ExtractObsData(self.data, self.locKeyList,DimDict)
-    DimDict = {'nlocs': nlocs}
-    #writer.BuildNetcdf(ObsVars, LocMdata, VarMdata, AttrData)
-    self.writer = iconv.IodaWriter(self.filename, self.locKeyList,DimDict)
-    self.writer.BuildIoda(ObsVars, varDims, self.varAttrs,self.GlobalAttrs)
-    
-        
+    prof = Profile(args.input, fdate, writer)
+    AttrData['date_time_string'] = fdate.strftime("%Y-%m-%dT%H:%M:%SZ")
+    (ObsVars, LocMdata, VarMdata) = writer.ExtractObsData(prof.data)
+    writer.BuildNetcdf(ObsVars, LocMdata, VarMdata, AttrData)
+
 
 if __name__ == '__main__':
     main()
