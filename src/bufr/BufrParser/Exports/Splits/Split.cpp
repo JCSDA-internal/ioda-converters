75109ea7d0edbd264f269308738d934fb8f79278
emilyhcliu <36091766+emilyhcliu@users.noreply.github.com>
2021-11-30 23:36:05 +0800

    Update Split.cpp

--- src/bufr/BufrParser/Exports/Splits/Split.cpp
+++ src/bufr/BufrParser/Exports/Splits/Split.cpp
@@ -1,5 +1,5 @@
 /*
- * (C) Copyright 2020 NOAA/NWS/NCEP/EMC
+ * (C) Copyright 2021 NOAA/NWS/NCEP/EMC
  *
  * This software is licensed under the terms of the Apache Licence Version 2.0
  * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
