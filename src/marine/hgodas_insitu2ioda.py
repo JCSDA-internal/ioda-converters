defb840f1760cf1c9fa71e357e8f285df13ef328
Ryan Honeyager <honeyage@ucar.edu>
2021-12-16 14:14:54 -0500

    Feature/v2profile (#642)
    * Start to update godae profile to use ioda python api
    
    * Fixes to handle lat,lon,time vars
    
    * Update to remove recKey
    
    * Upgraded hgodas_insitu converter to ioda v2 library.
    
    * Some changes to handle units and fill values
    
    * Some notes to finish converter attrdata
    
    * updates to put in a fill value
    
    * Fix get_default_fill_val
    
    * Example of units
    
    * Sync'd up test references with ioda-v2 format and currently what converters are producing. I was careful
    to make sure that all variable values did not change in the test reference files. Still need to add in
    missing attributes, etc.
    
    * Added units and _FillValue attributes to all variables (except the MetaData variables that have defaluts for these).
    
    * replace godae_prof.nc output
    
    * replace hgodas_insitu.nc for testoutput
    
    * Removed deprecated date_time_string global attribute.
    
    * Updated units values on profile and insitu conveters.
    
    * Filled in missing units values, which turned out to be meters.
    
    Co-authored-by: CoryMartin-NOAA <cory.r.martin@noaa.gov>
    Co-authored-by: Stephen Herbener <stephen.herbener@gmail.com>
    Co-authored-by: Stephen Herbener <32968781+srherbener@users.noreply.github.com>
    Co-authored-by: lliu1220 <lingcasper@gmail.com>

--- src/marine/hgodas_insitu2ioda.py
+++ src/marine/hgodas_insitu2ioda.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# (C) Copyright 2019 UCAR
+# (C) Copyright 2019-2021 UCAR
 #
 # This software is licensed under the terms of the Apache Licence Version 2.0
 # which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
@@ -19,13 +19,14 @@ if not IODA_CONV_PATH.is_dir():
     IODA_CONV_PATH = Path(__file__).parent/'..'/'lib-python'
 sys.path.append(str(IODA_CONV_PATH.resolve()))
 
-import ioda_conv_ncio as iconv
+import ioda_conv_engines as iconv
 from orddicts import DefaultOrderedDict
 
 
 vName = {
-    2210: "sea_water_temperature",
-    2220: "sea_water_salinity",
+    #          var name,            units
+    2210: ["sea_water_temperature", "C"],
+    2220: ["sea_water_salinity", "PSU"]
 }
 
 locationKeyList = [
@@ -35,18 +36,20 @@ locationKeyList = [
     ("datetime", "string")
 ]
 
-AttrData = {
+GlobalAttrs = {
     'odb_version': 1,
 }
 
 
 class Profile(object):
 
-    def __init__(self, filename, date, writer):
+    def __init__(self, filename, date):
         self.filename = filename
         self.date = date
         self.data = DefaultOrderedDict(lambda: DefaultOrderedDict(dict))
-        self.writer = writer
+        self.VarAttrs = DefaultOrderedDict(lambda: DefaultOrderedDict(dict))
+        self.floatDefFillVal = iconv.get_default_fill_val('float32')
+        self.intDefFillVal = iconv.get_default_fill_val('int32')
         self._read()
 
     def _read(self):
@@ -64,21 +67,31 @@ class Profile(object):
 
         base_date = datetime(1970, 1, 1) + timedelta(seconds=int(time[0]))
 
+        self.VarAttrs['depth', 'MetaData']['units'] = 'm'
         for i in range(len(hrs)):
             # there shouldn't be any bad obs, but just in case remove them all
             if qcs[i] != 0:
                 continue
 
-            valKey = vName[obid[i]], self.writer.OvalName()
-            errKey = vName[obid[i]], self.writer.OerrName()
-            qcKey = vName[obid[i]], self.writer.OqcName()
+            varName = vName[obid[i]][0]
+            varUnits = vName[obid[i]][1]
+            self.VarAttrs[varName, iconv.OvalName()]['_FillValue'] = self.floatDefFillVal
+            self.VarAttrs[varName, iconv.OerrName()]['_FillValue'] = self.floatDefFillVal
+            self.VarAttrs[varName, iconv.OqcName()]['_FillValue'] = self.intDefFillVal
+            self.VarAttrs[varName, iconv.OvalName()]['units'] = varUnits
+            self.VarAttrs[varName, iconv.OerrName()]['units'] = varUnits
+            self.VarAttrs[varName, iconv.OqcName()]['units'] = 'unitless'
+
+            valKey = varName, iconv.OvalName()
+            errKey = varName, iconv.OerrName()
+            qcKey = varName, iconv.OqcName()
 
             dt = base_date + timedelta(hours=float(hrs[i]))
             locKey = lats[i], lons[i], dpth[i], dt.strftime(
                 "%Y-%m-%dT%H:%M:%SZ")
-            self.data[0][locKey][valKey] = vals[i]
-            self.data[0][locKey][errKey] = errs[i]
-            self.data[0][locKey][qcKey] = qcs[i]
+            self.data[locKey][valKey] = vals[i]
+            self.data[locKey][errKey] = errs[i]
+            self.data[locKey][qcKey] = qcs[i]
 
 
 def main():
@@ -105,15 +118,21 @@ def main():
     args = parser.parse_args()
     fdate = datetime.strptime(args.date, '%Y%m%d%H')
 
-    writer = iconv.NcWriter(args.output, locationKeyList)
+    VarDims = {
+        'sea_water_temperature': ['nlocs'],
+        'sea_water_salinity': ['nlocs'],
+    }
 
     # Read in the profiles
-    prof = Profile(args.input, fdate, writer)
+    prof = Profile(args.input, fdate)
 
     # write them out
-    AttrData['date_time_string'] = fdate.strftime("%Y-%m-%dT%H:%M:%SZ")
-    (ObsVars, LocMdata, VarMdata) = writer.ExtractObsData(prof.data)
-    writer.BuildNetcdf(ObsVars, LocMdata, VarMdata, AttrData)
+    GlobalAttrs['date_time_string'] = fdate.strftime("%Y-%m-%dT%H:%M:%SZ")
+    ObsVars, nlocs = iconv.ExtractObsData(prof.data, locationKeyList)
+
+    DimDict = {'nlocs': nlocs}
+    writer = iconv.IodaWriter(args.output, locationKeyList, DimDict)
+    writer.BuildIoda(ObsVars, VarDims, prof.VarAttrs, GlobalAttrs)
 
 
 if __name__ == '__main__':
