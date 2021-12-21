ef488363a53a6a9fd4f042632d7953046c4dfeb7
BenjaminRuston <benr@ucar.edu>
2021-12-20 21:27:58 -0600

    fix argument parsing     consolodate some functions     put in some prints for uncommon cases     if surface pressure is not initial record index arrays     more investigation is needed for unique cases

--- src/conventional/decode_bufr_LDM_raob.py
+++ src/conventional/decode_bufr_LDM_raob.py
@@ -158,11 +158,10 @@ def get_meta_data(bufr):
     profile_meta_data = {}
     for k, v in meta_data_keys.items():
         profile_meta_data[k] = codes_get(bufr, v)
-        if (k == "stationIdWMOblock"):
-          wmo_block_id = profile_meta_data[k]
-        if (k == "stationIdWMOstation"):
-          wmo_station_id = profile_meta_data[k]
-    profile_meta_data['station_id'] = '{0:02}'.format(wmo_block_id) + '{0:03}'.format(wmo_station_id)
+
+    profile_meta_data = get_station_elevation(bufr, profile_meta_data)
+    profile_meta_data = get_station_id(profile_meta_data)
+
 
     # do the hokey time structure to time structure
     year  = codes_get(bufr, 'year')
@@ -180,6 +179,30 @@ def get_meta_data(bufr):
 
     return profile_meta_data
 
+def get_station_id(profile_meta_data):
+
+    k = 'station_id'
+    wmo_block_id = profile_meta_data["stationIdWMOblock"]
+    wmo_station_id = profile_meta_data["stationIdWMOstation"]
+    profile_meta_data[k] = '{0:02}'.format(wmo_block_id) + '{0:03}'.format(wmo_station_id)
+
+    return profile_meta_data
+
+def get_station_elevation(bufr, profile_meta_data):
+
+    # try to progressively get more detailed height of instrument
+    k = 'station_elevation'
+
+    station_height = codes_get(bufr, 'heightOfStationGroundAboveMeanSeaLevel')
+    if station_height != abs(int_missing_value):
+        profile_meta_data[k] = station_height
+    else:
+        station_height = codes_get(bufr, 'heightOfBarometerAboveMeanSeaLevel')
+        if station_height != abs(int_missing_value):
+            profile_meta_data[k] = station_height
+
+    return profile_meta_data
+
 def def_meta_data():
 
     meta_data_keys = {
@@ -187,7 +210,7 @@ def def_meta_data():
  "stationIdWMOstation"                   : 'stationNumber',
  "stationLongName"                       : 'shipOrMobileLandStationIdentifier',
  "instrumentType"                        : 'radiosondeType',
- "station_elevation"                     : 'heightOfBarometerAboveMeanSeaLevel',
+ "station_elevation"                     : 'height',
  "instrumentSerialNum"                   : 'radiosondeSerialNumber',
  "instrumentSoftwareVersion"             : 'softwareVersionNumber',
  "instrumentHumidityCorrectionInfo"      : 'correctionAlgorithmsForHumidityMeasurements',
@@ -196,6 +219,33 @@ def def_meta_data():
 
     return meta_data_keys
 
+
+def def_significance_table():
+
+    significance_table = {
+     "pressure level originally indicated by height as the veritcal coordinate" : 1,
+     "freezing level" : 2,
+     "level determined by regional decision" : 3,
+     "top of wind sounding" : 4,
+     "end of missing wind data" : 5,
+     "beginning of missing wind data" : 6,
+     "end of missing humidity data" : 7,
+     "beginning of missing humidity data" : 8,
+     "end of missing temperature data" : 9,
+     "beginning of missing temperature data" : 10,
+     "significant wind level" : 11,
+     "significant humidity level" : 12,
+     "significant temperature level" : 13,
+     "maximum wind level" : 14,
+     "tropopause level" : 15,
+     "standard level" : 16,
+     "surface" : 17,
+}
+
+    return significance_table
+
+def get_normalized_bit(value, bit_index):
+    return (value >> bit_index) & 1
 
 def assign_values(data):
     if data.dtype == float:
@@ -211,6 +261,7 @@ def assign_values(data):
 def read_bufr_message( f, count, start_pos ):
 
     obs_data = {}
+    call_fail = False
     if start_pos == f.tell():
         return obs_data, count, None
     start_pos = f.tell()
@@ -238,12 +289,95 @@ def read_bufr_message( f, count, start_pos ):
         temp_dewpoint = codes_get_array(bufr, 'dewpointTemperature', ktype=float)
         pressure = codes_get_array(bufr, 'pressure', ktype=float)
         geop_height = codes_get_array(bufr, 'nonCoordinateGeopotentialHeight', ktype=float)
+        sounding_significance = codes_get_array(bufr, 'extendedVerticalSoundingSignificance', ktype=int)
+
+        significance_table = def_significance_table()
+
+        # set a default if first record labeled surface set surface pressure
+        # could set this to None and skip if never gets set
+        surface_pressure = pressure[0]
+        if np.any(sounding_significance & 2**significance_table["surface"]):
+
+            if np.sum(np.any(sounding_significance & 2**significance_table["surface"])) > 1:
+                print ( " need more error handling too many surface levels" )
+                call_fail = True
+                print ( "starting position: %i" % start_pos )
+                print ( "length pressure: %i" % len(pressure) )
+                print ( "length temperature: %i" % len(temp_air) )
+                print ( "length dewpoint: %i" % len(temp_dewpoint) )
+                print ( "length geop_height: %i" % len(geop_height) )
+                print ( "length sound_sig: %i" % len(sounding_significance) )
+                print ( "length wind_dir: %i" % len(wind_direction) )
+                print ( "length wind_speed: %i" % len(wind_speed) )
+                print ( "length lat_displacement: %i" % len(lat_displacement) )
+                print ( "length lon_displacement: %i" % len(lon_displacement) )
+                print ( "length time_displacement: %i" % len(time_displacement) )
+                print ( "========" )
+            surface_level = np.where(sounding_significance & 2**significance_table["surface"])[0][0]
+            surface_pressure = pressure[ surface_level ]
+            if len(pressure) == len(temp_air):
+                pass
+                # if surface_level > 0 need to throw away data ????
+            elif len(pressure) == len(temp_air)+1:
+                if surface_level != 0:
+                    # surface level not first record
+                    pressure = pressure[surface_level:]
+                    sounding_significance = sounding_significance[surface_level:]
+                elif surface_level == 0:
+                    # surface level first record
+                    pressure = pressure[1:]
+                    sounding_significance = sounding_significance[1:]
+            elif len(pressure) > len(temp_air) + 1:
+                if len(pressure) == len(lat_dispalcement):
+                    # the only case we know
+                    pressure = pressure[surface_level:len(pressure)+surface_level-1]
+                    sounding_significance = sounding_significance[surface_level:len(pressure)+surface_level-1]
+                    lat_displacement = lat_displacement[surface_level:len(pressure)+surface_level-1]
+                    lon_displacement = lon_displacement[surface_level:len(pressure)+surface_level-1]
+
+
+        if len( pressure ) != len( temp_air ):
+            print ( " need more error handling mis-matched pressure to air temperature" )
+            call_fail = True
+            print ( "starting position: %i" % start_pos )
+            print ( "length pressure: %i" % len(pressure) )
+            print ( "length temperature: %i" % len(temp_air) )
+            print ( "length dewpoint: %i" % len(temp_dewpoint) )
+            print ( "length geop_height: %i" % len(geop_height) )
+            print ( "length sound_sig: %i" % len(sounding_significance) )
+            print ( "length wind_dir: %i" % len(wind_direction) )
+            print ( "length wind_speed: %i" % len(wind_speed) )
+            print ( "length lat_displacement: %i" % len(lat_displacement) )
+            print ( "length lon_displacement: %i" % len(lon_displacement) )
+            print ( "length time_displacement: %i" % len(time_displacement) )
+            print ( "========" )
+            #shell()
+            #sys.exit()
+
+        # skip records with bad sounding significance values
+        if np.any( sounding_significance == np.abs( int_missing_value ) ):     #2147483647
+            valid_level = np.where( sounding_significance != np.abs( int_missing_value ) )
+            lat_displacement = lat_displacement[ valid_level ]
+            lon_displacement = lon_displacement[ valid_level ]
+            time_displacement = time_displacement[ valid_level ]
+            wind_direction = wind_direction[ valid_level ]
+            wind_speed = wind_speed[ valid_level ]
+            temp_air = temp_air[ valid_level ]
+            temp_dewpoint = temp_dewpoint[ valid_level ]
+            pressure = pressure[ valid_level ]
+            geop_height = geop_height[ valid_level ]
+            sounding_significance = sounding_significance[ valid_level ]
 
         codes_release( bufr )
 
         num_levels        = len( lat_displacement )
+        if ( len( lat_displacement ) != len( time_displacement ) ):
+            print ( "this should never happen lat and time have different displacements lengths")
+            call_fail = True
+            sys.exit()
 
         # what to do for "ObsError" set to 1? and "PreQC" all zero?
+        obs_data[('surface_pressure', "ObsValue")] = np.full(num_levels, surface_pressure, dtype='float32')
         obs_data[('geopotential_height', "ObsValue")] = assign_values(geop_height)
         obs_data[('wind_direction', "ObsValue")] = assign_values(wind_direction)
         obs_data[('wind_speed', "ObsValue")] = assign_values(wind_speed)
@@ -255,7 +389,7 @@ def read_bufr_message( f, count, start_pos ):
         obs_data[('air_pressure', "MetaData")] = assign_values(pressure)
 
         # fake surface pressure for now from the first entry of air_pressure
-        profile_meta_data['surface_pressure'] = pressure[0]
+#       profile_meta_data['surface_pressure'] = pressure[0]  # should go to obs value
         for k, v in profile_meta_data.items():
             vals = np.repeat(v, krepfac[0])
             obs_data[(k, "MetaData")] = assign_values(vals)
@@ -268,6 +402,8 @@ def read_bufr_message( f, count, start_pos ):
 
     except:
         # print ( "invalid bufr message" )
+        if call_fail:
+            sys.exit()
         count[1] += 1
         # print ( "number of valid mssg: ", count[0] )
         # print ( "number of invalid mssg: ", count[1] )
@@ -278,13 +414,17 @@ if __name__ == "__main__":
 
     from argparse import ArgumentParser
 
-    usage = 'usage: %prog -i input-files -o output-file'
-    parser = ArgumentParser(usage)
-    parser.add_argument('-i', '--input-files', nargs='+', dest='file_names',
-                      action='store', default=None,
+    parser = ArgumentParser(
+        description=(
+            'Read a raob BUFR file and convert into IODA output file')
+)
+
+    required = parser.add_argument_group(title='required arguments')
+    required.add_argument('-i', '--input-files', nargs='+', dest='file_names',
+                      action='store', default=None, required=True,
                       help='input files')
-    parser.add_argument('-o', '--output-file', dest='output_file',
-                      action='store', default=None,
+    required.add_argument('-o', '--output-file', dest='output_file',
+                      action='store', default=None, required=True,
                       help='output file')
     args = parser.parse_args()
 
