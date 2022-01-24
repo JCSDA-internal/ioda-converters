c1dabb2a4fe8b942ede456b10d6f34190c264b3e
gthompsnJCSDA <gthompsn@ucar.edu>
2022-01-24 14:58:56 -0600

    support for vector of Int64

---
+++ src/bufr/DataObject/Int64VecDataObject.cpp
@@ -0,0 +1,58 @@
+/*
+ * (C) Copyright 2022 UCAR
+ *
+ * This software is licensed under the terms of the Apache Licence Version 2.0
+ * which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+#include <iostream>
+
+#include "Int64VecDataObject.h"
+
+
+namespace Ingester
+{
+    Int64VecDataObject::Int64VecDataObject(const std::vector<int64_t>& int64Vector) :
+        int64Vector_(int64Vector)
+    {
+    }
+
+    ioda::Variable Int64VecDataObject::createVariable(ioda::ObsGroup& obsGroup,
+                                                    const std::string& name,
+                                                    const std::vector<ioda::Variable>& dimensions,
+                                                    const std::vector<ioda::Dimensions_t>& chunks,
+                                                    int compressionLevel)
+    {
+        auto params = makeCreationParams(chunks, compressionLevel);
+        auto var = obsGroup.vars.createWithScales<std::string>(name, dimensions, params);
+        var.write(int64Vector_);
+        return var;
+    }
+
+    void Int64VecDataObject::print() const
+    {
+        std::cout << int64Vector_ << std::endl;
+    }
+
+    size_t Int64VecDataObject::nrows() const
+    {
+        return int64Vector_.size();
+    }
+
+    size_t Int64VecDataObject::ncols() const
+    {
+        return 1;
+    }
+
+    ioda::VariableCreationParameters Int64VecDataObject::makeCreationParams(
+                                                    const std::vector<ioda::Dimensions_t>& chunks,
+                                                    int compressionLevel)
+    {
+        ioda::VariableCreationParameters params;
+        params.chunk = true;
+        params.chunks = chunks;
+        params.compressWithGZIP(compressionLevel);
+
+        return params;
+    }
+}  // namespace Ingester
