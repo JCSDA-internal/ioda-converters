0f0ce54bfeb84356dcf24806819160ac47f4ac73
YoulongXia-NOAA <Youlong.Xia@noaa.gov>
2022-03-10 21:34:00 +0000

    update src/land/imsfv3_scf2ioda.py and testoutput

--- src/land/imsfv3_scf2ioda.py
+++ src/land/imsfv3_scf2ioda.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# (C) Copyright 2021 NOAA/NWS/NCEP/EMC
+# (C) Copyright 2020-2022 NOAA/NWS/NCEP/EMC
 #
 # This software is licensed under the terms of the Apache Licence Version 2.0
 # which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
@@ -37,15 +37,14 @@ obsvars = {
 }
 
 AttrData = {
-    'converter': os.path.basename(__file__),
 }
 
 DimDict = {
 }
 
 VarDims = {
-    'snowCoverFraction': ['nlocs'],
-    'totalSnowDepth': ['nlocs'],
+    'snowCoverFraction': ['Location'],
+    'totalSnowDepth': ['Location'],
 }
 
 
@@ -71,22 +70,19 @@ class imsFV3(object):
             if iodavar == 'snowCoverFraction':
                 self.varAttrs[iodavar, iconv.OvalName()]['units'] = '1'
                 self.varAttrs[iodavar, iconv.OerrName()]['units'] = '1'
-                self.varAttrs[iodavar, iconv.OqcName()]['units'] = 'unitless'
                 self.varAttrs[iodavar, iconv.OvalName()]['_FillValue'] = -999.
                 self.varAttrs[iodavar, iconv.OerrName()]['_FillValue'] = -999.
                 self.varAttrs[iodavar, iconv.OqcName()]['_FillValue'] = -999
 
             if iodavar == 'totalSnowDepth':
-                self.varAttrs[iodavar, iconv.OvalName()]['units'] = 'mm'
-                self.varAttrs[iodavar, iconv.OerrName()]['units'] = 'mm'
-                self.varAttrs[iodavar, iconv.OqcName()]['units'] = 'unitless'
+                self.varAttrs[iodavar, iconv.OvalName()]['units'] = 'm'
+                self.varAttrs[iodavar, iconv.OerrName()]['units'] = 'm'
                 self.varAttrs[iodavar, iconv.OvalName()]['_FillValue'] = -999.
                 self.varAttrs[iodavar, iconv.OerrName()]['_FillValue'] = -999.
                 self.varAttrs[iodavar, iconv.OqcName()]['_FillValue'] = -999
 
         # read netcdf file
         ncd = nc.Dataset(self.filename)
-        AttrData["sensor"] = "IMS Multisensor"
         lons = ncd.variables['lon'][:]
         lats = ncd.variables['lat'][:]
         oros = ncd.variables['oro'][:]
@@ -103,7 +99,7 @@ class imsFV3(object):
         qdflg = 0*sndv.astype('int32')
         errsc = 0.0*sncv
         errsd = 0.0*sndv
-        errsd[:] = 80.0
+        errsd[:] = 0.08
         ncd.close()
 
         times = np.empty_like(sncv, dtype=object)
@@ -113,10 +109,10 @@ class imsFV3(object):
         my_date = datetime.strptime(str_date, "%Y%m%d")
         start_datetime = my_date.strftime('%Y-%m-%d')
         base_datetime = start_datetime + 'T18:00:00Z'
-        AttrData['date_time_string'] = base_datetime
 
         for i in range(len(lats)):
             times[i] = base_datetime
+            sndv[i] = 0.001*sndv[i]
 
         # add metadata variables
         self.outdata[('datetime', 'MetaData')] = times
@@ -136,8 +132,7 @@ class imsFV3(object):
                     self.outdata[self.varDict[iodavar]['valKey']] = sndv
                     self.outdata[self.varDict[iodavar]['errKey']] = errsd
                     self.outdata[self.varDict[iodavar]['qcKey']] = qdflg
-        DimDict['nlocs'] = len(self.outdata[('datetime', 'MetaData')])
-        AttrData['nlocs'] = np.int32(DimDict['nlocs'])
+        DimDict['Location'] = len(self.outdata[('datetime', 'MetaData')])
 
 
 def main():
--- test/testoutput/imsfv3_scf.nc
+++ test/testoutput/imsfv3_scf.nc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b0560a9f19ff661314b56a32d3de6bf6724f98178597ed97d1b4e87b5a96fc0
-size 225977
+oid sha256:2a783825e699c15f6ce86e201fb973ef40aea27ff131b32350fa72cca461a45a
+size 225262
