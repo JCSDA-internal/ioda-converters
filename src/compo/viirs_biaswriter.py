a70624740eb1f1ce65f1c77439b7b0a2e52738d5
Andrew.Tangborn <Andrew.Tangborn@noaa.gov>
2022-03-18 17:01:26 +0000

    Fixed python coding conventions.

--- src/compo/viirs_biaswriter.py
+++ src/compo/viirs_biaswriter.py
@@ -3,11 +3,11 @@ import netCDF4
 import numpy as np
 import os
 
-# Writes out netCDF file with bias coefficient for VIIRS AOD observations. Currently this 
-# is just a single global value for the "constant" parameter value. This value is defined in the 
-# python code rather than read in from a file. Future implementations 
+# Writes out netCDF file with bias coefficient for VIIRS AOD observations. Currently this
+# is just a single global value for the "constant" parameter value. This value is defined in the
+# python code rather than read in from a file. Future implementations
 # will include regional, seasonal and AOD magnitude depencies.
-#
+
 # Usage:
 # python viirs_biaswriter.p -o viirs_bias.nc
  
