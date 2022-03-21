f992976a22746a527f36319c91f37d699b6d61b1
Maryam Abdi-Oskouei <maryamao@ucar.edu>
2022-03-21 16:09:55 -0600

    cdash  url from  s3

--- CI/cdash-url.sh
+++ CI/cdash-url.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 dir=$1
+cdash_url=$2
 tag=$(head -1 $dir/TAG)
 Done=$(cat $dir/$tag/Done.xml)
 buildID=$(echo $Done | grep -o -P '(?<=buildId>).*(?=</build)')
-URL=https://cdash.jcsda.org/buildSummary.php?buildid=$buildID
+URL=$cdash_url/viewTest.php?buildid=$buildID
 echo $URL
