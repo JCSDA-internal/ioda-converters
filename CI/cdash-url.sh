425f44a868c5726c156a14e457bfc73a5099fa90
Ryan Honeyager <honeyage@ucar.edu>
2022-03-22 15:12:33 -0400

    Update CI/cdash-url.sh

--- CI/cdash-url.sh
+++ CI/cdash-url.sh
@@ -5,5 +5,5 @@ cdash_url=$2
 tag=$(head -1 $dir/TAG)
 Done=$(cat $dir/$tag/Done.xml)
 buildID=$(echo $Done | grep -o -P '(?<=buildId>).*(?=</build)')
-URL=$cdash_url/viewTest.php?buildid=$buildID
+URL=$cdash_url/buildSummary.php?buildid=$buildID
 echo $URL
