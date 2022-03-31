e16cd8a98559390a557c07b6868111b596dd1171
YoulongXia-NOAA <Youlong.Xia@noaa.gov>
2022-03-31 19:35:23 +0000

    Update hgodas_insitu2ioda.py

--- src/marine/hgodas_insitu2ioda.py
+++ src/marine/hgodas_insitu2ioda.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 #
-# (C) Copyright 2019-2021 UCAR
+# (C) Copyright 2019-2022 UCAR
 #
 # This software is licensed under the terms of the Apache Licence Version 2.0
 # which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
