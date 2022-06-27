d65d92a40171fe41b8118406a94f8c3c9fd73a3c
mjhossen <mjhossen55@gmail.com>
2022-06-27 15:11:35 -0500

    reverted

--- src/marine/copernicus_l4adt2ioda.py
+++ src/marine/copernicus_l4adt2ioda.py
@@ -16,7 +16,7 @@ import os
 from pathlib import Path
 IODA_CONV_PATH = Path(__file__).parent/"@SCRIPT_LIB_PATH@"
 if not IODA_CONV_PATH.is_dir():
-    IODA_CONV_PATH = Path(__file__).parent/"@SCRIPT_LIB_PATH@"
+    IODA_CONV_PATH = Path(__file__).parent/'..'/'lib-python'
 sys.path.append(str(IODA_CONV_PATH.resolve()))
 
 import ioda_conv_engines as iconv
