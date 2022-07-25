65ffefb625cb1b1d0579d5c279005a607b67c41b
Ron McLaren <ronlaren@aol.com>
2022-07-25 16:07:55 -0400

    Added target.h and updated cmakelists.txt

---
+++ src/bufr/BufrParser/Query/Target.h
@@ -0,0 +1,32 @@
+//
+// Created by Ronald McLaren on 7/13/22.
+//
+
+#pragma once
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+#include "DataProvider.h"
+
+namespace Ingester {
+namespace bufr {
+    /// \brief The information or Meta data for a BUFR field whose data we wish to capture when
+    /// we execute a query.
+    /// \note Will be refactored to use the SubsetTable object.
+    struct Target
+    {
+        std::string name;
+        std::string queryStr;
+        std::string unit;
+        std::vector<int> seqPath;
+        std::vector<int> nodeIds;
+        std::vector<std::string> dimPaths;
+        std::vector<int> exportDimIdxs;
+        ElementInfo elementInfo;
+    };
+
+    typedef std::vector<std::shared_ptr<Target>> Targets;
+}  // Ingester
+}  // bufr
