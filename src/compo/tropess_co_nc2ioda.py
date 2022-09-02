cfa50c22b07d3d49c69b8c2a638574a43638566c
Maryam Abdi-Oskouei <maryamao@ucar.edu>
2022-09-02 16:08:04 -0600

    .

--- src/compo/tropess_co_nc2ioda.py
+++ src/compo/tropess_co_nc2ioda.py
@@ -10,6 +10,8 @@
 # TODO:
 #      - Add flags
 #      - Unit conversion
+#      - add obsvar, obserr, etc.
+#      - a better way to add metadata
 
 import sys
 import argparse
@@ -40,11 +42,9 @@ locationKeyList = [
 ]
 
 obsvars = {
-    'x': 'carbonmonoxideProfile'  # Total col is not ready, is Profile the right term?
-    # units:1
-    # standard_name:dry_atmosphere_volume_mixing_ratio_of_carbon_monoxide
-    # long_name:carbon_monoxide_vmr
-    # comment:Volume mixing ratio (VMR) of Carbon Monoxide relative to dry air
+    'x_l_1': 'carbon_monoxide_in_l_1',
+    'x_l_5': 'carbon_monoxide_in_l_5'
+
 }
 
 AttrData = {
@@ -60,9 +60,7 @@ VarDims = {
 }
     
 # constants
-avogadro = 6.02214076E23
-scm2sm = 1E4
-vmr2col = 2.12E13  # What is this value for TROPESS?
+vmr2mol_m2 = 3.405 # 1/(MWAir * grav)[(mole.s2)/(kg.m)]
 hPa2Pa = 1E2
     
 class tropess(object):
@@ -75,16 +73,16 @@ class tropess(object):
 
     def _read(self):
         # set up variable names for IODA
-        for iodavar in obsvars.items():
-            self.varDict[iodavar]['valKey'] = iodavar, iconv.OvalName()
-            self.varDict[iodavar]['errKey'] = iodavar, iconv.OerrName()
-            self.varDict[iodavar]['qcKey'] = iodavar, iconv.OqcName()
-            self.varAttrs[iodavar, iconv.OvalName()]['coordinates'] = 'longitude latitude'
-            self.varAttrs[iodavar, iconv.OerrName()]['coordinates'] = 'longitude latitude'
-            self.varAttrs[iodavar, iconv.OqcName()]['coordinates'] = 'longitude latitude'
-            self.varAttrs[iodavar, iconv.OvalName()]['units'] = '1' # make sure to update this
-            self.varAttrs[iodavar, iconv.OerrName()]['units'] = '1'
-            self.varAttrs[iodavar, iconv.OqcName()]['units'] = 'unitless'
+        for key, item in obsvars.items():
+            self.varDict[item]['valKey'] = item, iconv.OvalName()
+            self.varDict[item]['errKey'] = item, iconv.OerrName()
+            self.varDict[item]['qcKey'] = item, iconv.OqcName()
+            self.varAttrs[item, iconv.OvalName()]['coordinates'] = 'longitude latitude'
+            self.varAttrs[item, iconv.OerrName()]['coordinates'] = 'longitude latitude'
+            self.varAttrs[item, iconv.OqcName()]['coordinates'] = 'longitude latitude'
+            self.varAttrs[item, iconv.OvalName()]['units'] = 'mol m-2'
+            self.varAttrs[item, iconv.OerrName()]['units'] = 'mol m-2'
+            self.varAttrs[item, iconv.OqcName()]['units'] = 'unitless'
 
         # loop through input filenames
         first = True
@@ -92,96 +90,126 @@ class tropess(object):
 
             ds_cris = xr.open_mfdataset(filename, parallel=False)
 
-            # get global attributes
+            # Read global attributes
             AttrData['sensor'] = ds_cris.attrs['Instrument']
             AttrData['platform'] = ds_cris.attrs['Platform']
 
+            # Read lat lon
             lats = ds_cris['latitude']
             lons = ds_cris['longitude']
 
-            # convert to ioda time format
+            # Read time and convert to ioda time format
             # note that datetime_utc variable in the intput is buggy 
             # and has +10sec offset when compared time_tai93.
             times = ds_cris['time'].dt.strftime("%Y-%m-%dT%H:%M:%SZ").values
-                
-            pressure = ds_cris['pressure'] #hPa
+            
+            # Read pressure
+            pressure = ds_cris['pressure'] # hPa (nlocsxnlevs)
             nlocs = pressure.shape[0]
             nlevs = pressure.shape[1]
-            x = ds_cris['x'] # dry_atmosphere_volume_mixing_ratio_of_carbon_monoxide
+            # add p=0 to the top of the atmopshere
+            top_pressure = np.zeros((nlocs,1))
+            full_pressure = np.concatenate((pressure, top_pressure), axis=1)
+            
+            # Read other variables
+            x = ds_cris['x'] # dry_atmosphere_volume_mixing_ratio_of_carbon_monoxide (nlocsxnlevs)
 
             # open observation_ops group
             ds_cris_observation_ops= xr.open_mfdataset(filename, group='observation_ops', parallel=False)
     
-            # A priori state, as volume mixing ratio (VMR) relative to dry air, unit = 1
-            xa = ds_cris_observation_ops['xa']                                                                                                                   
-    
+            # A priori state, as volume mixing ratio (VMR) relative to dry air , unit = 1
+            xa = ds_cris_observation_ops['xa'] # (nlocsxnlevs)
+            
+
             # logarithmic_averaging_kernel
-            averaging_kernel = ds_cris_observation_ops['averaging_kernel']
+            averaging_kernel = ds_cris_observation_ops['averaging_kernel'] # (nlocsxnlevsxnlevs)
      
             # logarithmic_observation_error
-            observation_error = ds_cris_observation_ops['observation_error']
+            log_obs_error = ds_cris_observation_ops['observation_error']  # (nlocsxnlevsxnlevs)
             
-            # The estimated state for target 0 based on x[0], xa[0], and avg_kernel[0], as volume mixing ratio (VMR) relative to dry air
+            # The estimated state for target 0 based on x[0], xa[0], 
+            # and avg_kernel[0], as volume mixing ratio (VMR) relative to dry air
             test_x = ds_cris_observation_ops['x_test']
-    
+
+            # --- Unit conversion from VMR to mole/m2
+            x_mole_m2 = np.zeros((nlocs,nlevs))
+            xa_mole_m2 = np.zeros((nlocs,nlevs))
+            obs_error_mole_m2 = np.zeros((nlocs,nlevs,nlevs))
+            qa = np.zeros((nlocs,nlevs))
+            ap_mole_m2 = np.zeros((nlocs,nlevs))
+            
+
+            
+            # --- Calculations
             # Calculate apriori term
-            user_levels = [1,5] ## read this from YAML?
+            user_levels = [1,5]
             lev=0
             ap = np.zeros((nlocs,len(user_levels)))
             for user_level in user_levels:
                 ap [:,lev] = 0
-                for j in range(nlevs):
-                    ap[:,lev] = ln(xa)[:,user_level] - averaging_kernel[:,user_level,j]*ln(xa)[:,j] + ap[:,lev]
+                for j in range(nlevs):                    
+                    ap[:,lev] = np.exp(ln(xa[:,lev]) - averaging_kernel[:,user_level,j]*ln(xa[:,j])) + ap[:,lev]
                 lev=lev+1
-    
-            ## TOTAL COLUMN CALC:
-    
-            # H(xt) = ln(xa) + A (ln(xt) - ln(xa))
-            # convert VMR to total column (ct) following MOPPIT Version 4 Product User's Guide
-            # C = K x sum(delP x VMR); 
-            # where K = 2.2 x 10^13 (mol/cm2)/(hPa ppb)
-            # for tropess need to remove ppb => K = 2.2 x 10^(13+9) = 2.2 x 10^22 (mol/cm2)/(hPa)
-            #vmr2col = 2.2 x 10^22    
-            #ap_tc = np.zeros(nlocs)
-            #for j in range(nlevs):
-            #    for i in range(nlevs-1):
-            #        delP = pressure[:,i] - pressure[:,i+1]
-            #        sumterm = delP*x[:,i]*averaging_kernel[:,i,j] + sumterm
-            #ap_tc[:,j] = (vmr2col/math.log10(math.e)) * sumterm
 
-            # if first: # add this later
-    
+            for i in range(nlevs):
+                delP_pa = hPa2Pa*(full_pressure[:,i] - full_pressure[:,i+1])
+                x_mole_m2[:,i] = x[:,i]*delP_pa*vmr2mol_m2
+                xa_mole_m2[:,i] = xa[:,i]*delP_pa*vmr2mol_m2
+                obs_error_mole_m2[:,i,i] = np.exp(log_obs_error[:,i,i])*delP_pa*vmr2mol_m2 ##?? this is 3D
+                
+            lev=0
+            for user_level in user_levels:
+                delP_pa = hPa2Pa*(full_pressure[:,user_level] - full_pressure[:,user_level+1])
+                print("delP is = ")
+                print(delP_pa)
+                print(ap[:,lev])
+                ap_mole_m2[:,lev] = ap[:,lev]*delP_pa*vmr2mol_m2
+                lev=lev+1
+
+                
+            # ---- Write Metadata and data 
+            
+            #if first:
             # add metadata variables
             self.outdata[('datetime', 'MetaData')] = times
             self.outdata[('latitude', 'MetaData')] = lats
             self.outdata[('longitude', 'MetaData')] = lons
-            #self.outdata[('apriori_term', 'RtrvlAncData')] = ap
-    
+
+            lev=0
+            for user_level in user_levels: ## over user_levels or retrieval levels
+                for j in range(nlevs):
+                    varname_ak = ('averaging_kernel_l'+str(user_level)+'_level_'+str(i), 'RtrvlAncData')
+                    self.outdata[varname_ak] = averaging_kernel[:,user_level,j]
+                    self.varAttrs[varname_ak]['coordinates'] = 'longitude latitude'
+                    self.varAttrs[varname_ak]['units'] = ''
+                
+                    varname_pr = ('pressure_l'+str(user_level)+'level_'+str(i), 'RtrvlAncData')
+                    self.outdata[varname_pr] = hPa2Pa * pressure[:, j]
+                    self.varAttrs[varname_ak]['coordinates'] = 'longitude latitude'
+                    self.varAttrs[varname_ak]['units'] = 'Pa'
+              
+                varname_ap = ('apriori_term_'+str(user_level), 'RtrvlAncData')
+                self.outdata[varname_ap] = ap_mole_m2[:,lev]
+                self.varAttrs[varname_ap]['coordinates'] = 'longitude latitude'
+                self.varAttrs[varname_ap]['units'] = 'mol m-2'
+                
+                varname_xa = ('xa_l_'+str(user_level), 'RtrvlAncData')
+                self.outdata[varname_xa] = xa_mole_m2[:,lev]
+                self.varAttrs[varname_xa]['coordinates'] = 'longitude latitude'
+                self.varAttrs[varname_xa]['units'] = 'mol m-2'
+
+                lev = lev + 1
+                          
             counter = 0
 
-            for k in user_levels: ## over user_levels or retrieval levels
-                varname_ak = ('averaging_kernel_level_'+str(k), 'RtrvlAncData')
-                self.outdata[varname_ak] = averaging_kernel.sum(axis=1) # 2D or 1 D? Sum over rows? 
-                
-                varname_pr = ('pressure_level_'+str(k), 'RtrvlAncData')
-                self.outdata[varname_pr] = hPa2Pa * pressure[:, k]
-            
-                varname_ap = ('apriori_term_'+str(k), 'RtrvlAncData')
-                self.outdata[varname_ap] = ap[:,counter]
-                counter = counter + 1
-               
-            varname_xa_ln = ('apriori_state_ln', 'RtrvlAncData')
-            self.outdata[varname_xa_ln] = ln(xa)    
-    
+            for i in obsvars.keys():                
+                self.outdata[self.varDict[obsvars[i]]['valKey']] = x_mole_m2[:,user_levels[counter]]
+                self.outdata[self.varDict[obsvars[i]]['errKey']] = obs_error_mole_m2[:,user_levels[counter],user_levels[counter]]
+                self.outdata[self.varDict[obsvars[i]]['qcKey']] = qa[:,user_levels[counter]]
+                counter = counter+1                                
 
         DimDict['nlocs'] = len(self.outdata[('datetime', 'MetaData')])
         AttrData['nlocs'] = np.int32(DimDict['nlocs'])
-        
-        for k in user_levels:
-            varname = 'averaging_kernel_level_'+str(k)
-            vkey = (varname, 'RtrvlAncData')
-            self.varAttrs[vkey]['coordinates'] = 'longitude latitude'
-            self.varAttrs[vkey]['units'] = ''
 
 def main():
 
@@ -205,7 +233,7 @@ def main():
 
     args = parser.parse_args()
 
-    # Read in the CO2 data
+    # Read in the CO data
     co = tropess(args.input)
 
     # setup the IODA writer
---
+++ test/testinput/tropess_cris_co.nc
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:2934841e286d49941c59acaede5d75b2cfeb50d8426daf2a2c8c7dfb00cc790d
+size 53554368
