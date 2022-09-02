3c6d6e731f8103ac4c79052a8d26fede9b8afea8
gmao-jjin3 <jianjun.jin@nasa.gov>
2022-09-02 09:38:32 -0400

    Only changed land_type_index to land_type_index_NPOESS.

--- src/gsi-ncdiag/gsi_ncdiag.py
+++ src/gsi-ncdiag/gsi_ncdiag.py
@@ -84,16 +84,6 @@ conv_bufrtypes = {
 all_LocKeyList = {
     'Station_ID': ('station_id', 'string'),
     'Time': ('datetime', 'string'),
-    'time': ('time', 'string'),
-    'ascending_flag': ('ascending_flag', 'string'),
-    'earth_radius_of_curvature': ('earth_radius_of_curvature', 'string'),
-    'reference_sat_id': ('reference_sat_id', 'string'),
-    'occulting_sat_id': ('occulting_sat_id', 'string'),
-    'record_number': ('record_number', 'string'),
-    'geoid_height_above_reference_ellipsoid': ('geoid_height_above_reference_ellipsoid', 'string'),
-    'gnss_sat_class': ('gnss_sat_class', 'string'),
-    'impact_height': ('impact_height', 'string'),
-    'impact_parameter': ('impact_parameter', 'string'),
     'Latitude': ('latitude', 'float'),
     'Longitude': ('longitude', 'float'),
     'Station_Elevation': ('station_elevation', 'float'),
@@ -125,7 +115,6 @@ all_LocKeyList = {
     'SCCF_chan_wavelen': ('channel_wavelength', 'float'),
     'QI_with_FC': ('satwind_quality_ind_with_fc', 'float'),
     'QI_without_FC': ('satwind_quality_ind_no_fc', 'float'),
-    'Data_Vertical_Velocity': ('data_vertical_velocity', 'float'),
     'LaunchTime': ('LaunchTime', 'float'),
 }
 
@@ -315,7 +304,6 @@ geovals_vars = {
     'specific_humidity': 'specific_humidity',
     'northward_wind': 'northward_wind',
     'eastward_wind': 'eastward_wind',
-    'geopotential_height_levels': 'geopotential_height_levels',
     'geopotential_height': 'geopotential_height',
     'geometric_height': 'geometric_height',
     'height': 'height_above_mean_sea_level',
@@ -327,7 +315,6 @@ geovals_vars = {
     'surface_roughness': 'surface_roughness_length',
     'surface_height': 'surface_geopotential_height',
     'surface_geopotential_height': 'surface_geopotential_height',
-    'surface_altitude': 'surface_altitude',
     'surface_geometric_height': 'surface_geometric_height',
     'landmask': 'land_area_fraction',
     'air_temperature': 'air_temperature',
@@ -424,18 +411,12 @@ units_values = {
     'northward_wind': 'm s-1',
     'eastward_wind': 'm s-1',
     'geopotential_height': 'm',
-    'geopotential_height_levels': 'm',
     'height_above_mean_sea_level': 'm',
     'surface_pressure': 'Pa',
     'sea_surface_temperature': 'K',
     'surface_temperature': 'K',
     'surface_roughness_length': 'm',
     'surface_geopotential_height': 'm',
-    'surface_altitude': 'm',
-    'geoid_height_above_reference_ellipsoid': 'Meters',
-    'earth_radius_of_curvature': 'Meters',
-    'impact_height': 'Meters',
-    'impact_parameter': 'Meters',
     'land_area_fraction': '1',
     'air_temperature': 'K',
     'air_pressure': 'Pa',
@@ -513,7 +494,6 @@ units_values = {
     'clw_retrieved_from_background': 'kg/m/m',
     'scat_retrieved_from_observation': '1',
     'LaunchTime': 'hours',
-    'bending_angle': 'radians',
 }
 
 # @TestReference
@@ -701,8 +681,7 @@ class Conv(BaseGSI):
                             if (len(var.dimensions) == 1):
                                 dims = ("nlocs",)
                             else:
-                                if (vname == "atmosphere_pressure_coordinate_interface" \
-                                    or vname == "geopotential_height_levels"):
+                                if vname == "atmosphere_pressure_coordinate_interface":
                                     dims = ("nlocs", "ninterfaces")
                                 else:
                                     dims = ("nlocs", "nlevs")
@@ -868,7 +847,7 @@ class Conv(BaseGSI):
                         tmp = self.var(lvar)[idx]
                         StationIDs = [bytes((b''.join(tmp[a])).decode('iso-8859-1').encode('utf8')) for a in range(len(tmp))]
                         outdata[(loc_mdata_name, 'MetaData')] = np.array(StationIDs, dtype=object)
-                    elif lvar == 'Time' or lvar == 'time':  # need to process into time stamp strings #"%Y-%m-%dT%H:%M:%SZ"
+                    elif lvar == 'Time':  # need to process into time stamp strings #"%Y-%m-%dT%H:%M:%SZ"
                         tmp = self.var(lvar)[idx]
                         obstimes = [self.validtime + dt.timedelta(hours=float(tmp[a])) for a in range(len(tmp))]
                         obstimes = [a.strftime("%Y-%m-%dT%H:%M:%SZ") for a in obstimes]
@@ -1383,8 +1362,7 @@ class Radiances(BaseGSI):
         gsiqcname = value, 'GsiEffectiveQC'
         errname = value, 'GsiFinalObsError'
         gsiqc = np.zeros_like(outdata[varDict[value]['valKey']])
-        # keep original QC.
-        gsiqc = np.reshape(self.var('QC_Flag'), (nlocs, nchans))
+        gsiqc[outdata[errname] > 1e8] = 1
         gsiqc[np.reshape(self.var('QC_Flag'), (nlocs, nchans)) < 0] = 1
         outdata[gsiqcname] = gsiqc.astype(np.int32)
         varAttrs[gsiqcname]['units'] = 'unitless'
