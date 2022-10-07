e5f1b4616eef2d28c068492cd4eef420c7c9e243
Nicholas.Esposito <Nicholas.Esposito@noaa.gov>
2022-10-07 20:02:32 +0000

    most recent updates AA

--- src/bufr/BufrParser/Exports/Variables/AircraftAltitudeVariable.cpp
+++ src/bufr/BufrParser/Exports/Variables/AircraftAltitudeVariable.cpp
@@ -93,99 +93,101 @@ namespace Ingester
     std::shared_ptr<DataObjectBase> AircraftAltitudeVariable::exportData(const BufrDataMap& map)
     {
         checkKeys(map);
-        static const float missing_int = 1.e+11;
+//        static const float missing_int = 1.e+11;
+//        static const float missing_int = DataObject<int>::missingValue();
 
-        std::vector<float> AircraftAltitudeArray;
-        AircraftAltitudeArray.reserve(map.at(getExportKey(ConfKeys::Latitude))->size());
-        for (unsigned int idx = 0; idx < map.at(getExportKey(ConfKeys::Latitude))->size(); idx++)
+        std::vector<float> aircraftaltitudearray;
+        aircraftaltitudearray.reserve(map.at(getExportKey(ConfKeys::Latitude))->size());
+        unsigned int datalength = map.at(getExportKey(ConfKeys::Latitude))->size();
+//        auto prestype =  map.at(getExportKey(ConfKeys::Latitude))->typeInfo();
+//        auto prestype =  map.at(getExportKey(ConfKeys::Latitude))->typeInfo;
+//        std::cout << "prestype is " << prestype << std::endl;
+        for (unsigned int idx = 0; idx < datalength; idx++)
         {
-            float latitude = static_cast<float>(map.at(getExportKey(ConfKeys::Latitude))->getAsFloat(idx));
+//            float latitude = map.at(getExportKey(ConfKeys::Latitude))->getAsFloat(idx);
             float acftalt = FLT_MAX; 
-            if (latitude != missing_int)
-            {
-                float pressures = FLT_MAX;
+//            if(latitude != DataObject<float>::missingValue())
+//            {
+/*                float pressures = FLT_MAX;
                 float aircraftIndicatedAltitudes = FLT_MAX;
                 float pressureAltitudeRelativeToMeanSeaLevels = FLT_MAX;
                 float flightLevels = FLT_MAX;
                 float heights = FLT_MAX;
                 float heightOrAltitudes = FLT_MAX;
-                float flightLevelSTs = FLT_MAX;
+                float flightLevelSTs = FLT_MAX; */
                 if (!pressureQuery_.empty()) 
                 {
-                    pressures = static_cast<float>(map.at(getExportKey(ConfKeys::Pressure))->getAsFloat(idx));
-                    if (pressures != INT_MAX)
+                      auto prestype = std::shared_pt<map.at(getExportKey(ConfKeys::Pressure))> objectByTypeInfo(TypeInfo& info) const;
+//                    auto prestype = map.at(getExportKey(ConfKeys::Pressure)).target->typeInfo;
+//                    auto prestype = map.at(getExportKey(ConfKeys::Pressure)).target->typeInfo;
+                    float pressures = map.at(getExportKey(ConfKeys::Pressure))->getAsFloat(idx);
+                     
+                    if (pressures != INT_MAX) 
+//                    if (pressures != ResultSet<pressureQuery_.typeInfo>::missingValue())
+//                    if (pressures != ResultSet<pressureQuery_.typeInfo>::missingValue())
                     {
-                        float ht_from_p = FLT_MAX;
 //                      If pressure exists, derive height from US standard atmosphere
                         if (pressures < 22630)
                         {
-                            ht_from_p = 11000 - (std::log1p(pressures/22630)/0.0001576106);
+                            acftalt = 11000 - (std::log1p(pressures/22630)/0.0001576106);
                         } else {
-                            ht_from_p = (1.0-pow((pressures/101325),(1.0/5.256)))*(288.15/0.0065);
+                            acftalt = (1.0-pow((pressures/101325),(1.0/5.256)))*(288.15/0.0065);
                         }
-                        acftalt = ht_from_p;
                     } else if (!aircraftIndicatedAltitudeQuery_.empty())
                     {
-                        aircraftIndicatedAltitudes = static_cast<float>(map.at(getExportKey(ConfKeys::AircraftIndicatedAltitude))->getAsFloat(idx));
-                        if (aircraftIndicatedAltitudes != UINT_MAX)
-                        // If aircraftIndicatedAltitudes exists, set as the aircraftAltitude
+                        float aircraftIndicatedAltitudes = map.at(getExportKey(ConfKeys::AircraftIndicatedAltitude))->getAsFloat(idx);
+                        if (aircraftIndicatedAltitudes != DataObject<unsigned int>::missingValue())
+                        // If aircraftIndicatedAltitudes exists (it should if PRLC doesn't), set as the aircraftAltitude
                         {
                             acftalt = aircraftIndicatedAltitudes;
                         }
-                    } else {
-                        acftalt = FLT_MAX;
-                    }
+                    } //else {
+                    //    acftalt = FLT_MAX;
+                    //}
                 }
                 if (!pressureAltitudeRelativeToMeanSeaLevelQuery_.empty())
                 {
-                    pressureAltitudeRelativeToMeanSeaLevels = static_cast<float>(map.at(getExportKey(ConfKeys::PressureAltitudeRelativeToMeanSeaLevel))->getAsFloat(idx));
-                    if (pressureAltitudeRelativeToMeanSeaLevels != FLT_MAX)
+                    float pressureAltitudeRelativeToMeanSeaLevels = map.at(getExportKey(ConfKeys::PressureAltitudeRelativeToMeanSeaLevel))->getAsFloat(idx);
+                    if (pressureAltitudeRelativeToMeanSeaLevels != DataObject<float>::missingValue())
                     {
                         acftalt = pressureAltitudeRelativeToMeanSeaLevels;
                     }
-                }
-                if (!flightLevelQuery_.empty())
+                } else if (!flightLevelQuery_.empty())
                 {
-                    flightLevels = static_cast<float>(map.at(getExportKey(ConfKeys::FlightLevel))->getAsFloat(idx));
-                    if (flightLevels != FLT_MAX)
+                    float flightLevels = map.at(getExportKey(ConfKeys::FlightLevel))->getAsFloat(idx);
+                    if (flightLevels != DataObject<float>::missingValue())
                     {
                         acftalt = flightLevels;
                     }
-                }
-
-                if (!heightQuery_.empty())
+                } else if (!heightQuery_.empty())
                 {
-                    heights = static_cast<float>(map.at(getExportKey(ConfKeys::Height))->getAsFloat(idx));
-                    if (heights != static_cast<float>(INT_MAX))
+                    float heights = map.at(getExportKey(ConfKeys::Height))->getAsFloat(idx);
+                    if (heights != DataObject<float>::missingValue())
                     {
                         acftalt = heights;
                     }
-                }
-
-                if (!heightOrAltitudeQuery_.empty())
+                } else if (!heightOrAltitudeQuery_.empty())
                 {
-                    heightOrAltitudes = static_cast<float>(map.at(getExportKey(ConfKeys::HeightOrAltitude))->getAsFloat(idx));
-                    if (heightOrAltitudes != static_cast<float>(INT_MAX))
+                    float heightOrAltitudes = map.at(getExportKey(ConfKeys::HeightOrAltitude))->getAsFloat(idx);
+                    if (heightOrAltitudes != DataObject<float>::missingValue())
                     {
                         acftalt = heightOrAltitudes;
                     }
-                }
-
-                if (!flightLevelSTQuery_.empty())
+                } else if (!flightLevelSTQuery_.empty())
                 {
-                    flightLevelSTs = static_cast<float>(map.at(getExportKey(ConfKeys::FlightLevelST))->getAsFloat(idx));
-                    if (flightLevelSTs != static_cast<float>(INT_MAX))
+                    float flightLevelSTs = map.at(getExportKey(ConfKeys::FlightLevelST))->getAsFloat(idx);
+                    if (flightLevelSTs != DataObject<float>::missingValue())
                     {
                         acftalt = flightLevelSTs;
                     }
 
                 }
-            AircraftAltitudeArray.push_back(acftalt);
-            }
+            aircraftaltitudearray.push_back(acftalt);
+//            }
         } 
-        Dimensions dims = {static_cast<int>(AircraftAltitudeArray.size())};
+        Dimensions dims = {static_cast<int>(aircraftaltitudearray.size())};
         return std::make_shared<DataObject<float>>(
-                AircraftAltitudeArray, 
+                aircraftaltitudearray, 
                 getExportName(),
                 groupByField_,
                 dims,
@@ -265,6 +267,7 @@ namespace Ingester
             info.name = getExportKey(ConfKeys::Latitude);
             info.query = latitudeQuery_;
             info.groupByField = groupByField_;
+//            info.type = getExportKey(ConfKeys::Latitude).typeInfo; 
             queries.push_back(info);
         }
 
@@ -274,6 +277,7 @@ namespace Ingester
             info.name = getExportKey(ConfKeys::Pressure);
             info.query = pressureQuery_;
             info.groupByField = groupByField_;
+//            info.type = getExportKey(ConfKeys::Pressure).typeInfo;
             queries.push_back(info);
         }
 
