2ae8bba6c7c326c8761f19dc1d693b8a4ab961d1
ShastriPaturi <shastri.paturi@noaa.gov>
2022-10-13 15:32:41 -0500

    coding norms

--- src/marine/amsr2_icec2ioda.py
+++ src/marine/amsr2_icec2ioda.py
@@ -75,7 +75,7 @@ class iceconc(object):
                 dateend = ncd.getncattr('time_coverage_end')
                 date1 = datetime.strptime(datestart, "%Y-%m-%dT%H:%M:%S.%fZ")
                 date2 = datetime.strptime(dateend, "%Y-%m-%dT%H:%M:%S.%fZ")
-                avg = date1 + (date2 - date1)*0.5 
+                avg = date1 + (date2 - date1)*0.5
                 locKey = lat[i], lon[i], avg.strftime("%Y-%m-%dT%H:%M:%SZ")
                 self.data[locKey][valKey] = icec[i]*1e-02
                 self.data[locKey][errKey] = 0.1
