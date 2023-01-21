df646ecf909e275fdc4c1452776102f0c4b15351
BenjaminRuston <benr@ucar.edu>
2023-01-21 00:54:19 +0000

    implement the dateTime as epoch seconds

--- src/hdf5/atms_netcdf_hdf5_2ioda.py
+++ src/hdf5/atms_netcdf_hdf5_2ioda.py
@@ -5,7 +5,7 @@ Python code to ingest netCDF4 or HDF5 ATMS data
 """
 
 import argparse
-from datetime import datetime
+from datetime import datetime, timezone
 import glob
 # from concurrent.futures import ProcessPoolExecutor
 from pathlib import Path
@@ -31,6 +31,7 @@ ATMS_WMO_sensor_ID = 621
 
 float_missing_value = iconv.get_default_fill_val(np.float32)
 int_missing_value = iconv.get_default_fill_val(np.int32)
+long_missing_value = iconv.get_default_fill_val(np.int64)
 
 metaDataName = iconv.MetaDataName()
 obsValName = iconv.OvalName()
@@ -50,13 +51,9 @@ GlobalAttrs = {
 locationKeyList = [
     ("latitude", "float"),
     ("longitude", "float"),
-    ("datetime", "string"),
-    ("dateTime", "object"),
+    ("dateTime", "long"),
 ]
-#   ("dateTime", "long", "seconds since 1970-01-01T00:00:00Z", "keep"),
-meta_keys = [m_item[0] for m_item in locationKeyList]
 
-# iso8601_string = locationKeyList[meta_keys.index('dateTime')][2]
 iso8601_string = "seconds since 1970-01-01T00:00:00Z"
 epoch = datetime.fromisoformat(iso8601_string[14:-1])
 
@@ -104,10 +101,9 @@ def main(args):
 
     # prepare global attributes we want to output in the file,
     # in addition to the ones already loaded in from the input file
-    GlobalAttrs['datetimeRange'] = np.array([obs_data[('datetime', metaDataName)][0],
-                                             obs_data[('datetime', metaDataName)][-1]], dtype=object)
-#   GlobalAttrs['datetimeRange'] = np.array([obs_data[('dateTime', metaDataName)][0].strftime("%Y-%m-%dT%H:%M:%SZ"),
-#                                            obs_data[('dateTime', metaDataName)][-1].strftime("%Y-%m-%dT%H:%M:%SZ")], dtype=object)
+    GlobalAttrs['datetimeRange'] = np.array([datetime.fromtimestamp(obs_data[('dateTime', metaDataName)][0], timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ"),
+                                             datetime.fromtimestamp(obs_data[('dateTime', metaDataName)][-1], timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")],
+                                             dtype=object)
     GlobalAttrs['datetimeReference'] = dtg.strftime("%Y-%m-%dT%H:%M:%SZ")
 
     # pass parameters to the IODA writer
@@ -165,7 +161,7 @@ def get_data_nasa_disc(f, g, obs_data, add_qc=True):
     obs_data[('latitude', metaDataName)] = np.array(g['lat'][:, :].flatten(), dtype='float32')
     obs_data[('longitude', metaDataName)] = np.array(g['lon'][:, :].flatten(), dtype='float32')
     obs_data[('sensorChannelNumber', metaDataName)] = np.array(g['channel'][:], dtype='int32')
-    obs_data[('sensorScanPosition', metaDataName)] = np.tile(np.arange(nbeam_pos, dtype='float32') + 1, (nscans, 1)).flatten()
+    obs_data[('sensorScanPosition', metaDataName)] = np.tile(np.arange(nbeam_pos, dtype='int32') + 1, (nscans, 1)).flatten()
     obs_data[('solarZenithAngle', metaDataName)] = np.array(g['sol_zen'][:, :].flatten(), dtype='float32')
     obs_data[('solarAzimuthAngle', metaDataName)] = np.array(g['sol_azi'][:, :].flatten(), dtype='float32')
     obs_data[('sensorZenithAngle', metaDataName)] = np.array(g['sat_zen'][:, :].flatten(), dtype='float32')
@@ -174,7 +170,6 @@ def get_data_nasa_disc(f, g, obs_data, add_qc=True):
     nlocs = len(obs_data[('latitude', metaDataName)])
     obs_data[('satelliteId', metaDataName)] = np.full((nlocs), WMO_sat_ID, dtype='int32')
     obs_data[('dateTime', metaDataName)] = np.array(get_epoch_time(g['obs_time_utc']), dtype='int64')
-    obs_data[('datetime', metaDataName)] = np.array(get_string_dtg(g['obs_time_utc'][:, :, :]), dtype=object)
 
     # example: dimension ( 180, 96, 22 ) == dimension( nscan, nbeam_pos, nchannel )
     nchans = len(obs_data[('sensorChannelNumber', metaDataName)])
@@ -221,7 +216,7 @@ def get_data_noaa_class(f, g, obs_data, add_qc=True):
     nscans = np.shape(g['All_Data']['ATMS-SDR-GEO_All']['Latitude'])[0]
     nbeam_pos = np.shape(g['All_Data']['ATMS-SDR-GEO_All']['Latitude'])[1]
     obs_data[('sensorChannelNumber', metaDataName)] = np.array(np.arange(np.shape(g['All_Data']['ATMS-SDR_All']['BrightnessTemperature'])[2])+1, dtype='int32')
-    obs_data[('sensorScanPosition', metaDataName)] = np.tile(np.arange(nbeam_pos, dtype='float32') + 1, (nscans, 1)).flatten()
+    obs_data[('sensorScanPosition', metaDataName)] = np.tile(np.arange(nbeam_pos, dtype='int32') + 1, (nscans, 1)).flatten()
     obs_data[('solarZenithAngle', metaDataName)] = np.array(g['All_Data']['ATMS-SDR-GEO_All']['SolarZenithAngle'][:, :].flatten(), dtype='float32')
     obs_data[('solarAzimuthAngle', metaDataName)] = np.array(g['All_Data']['ATMS-SDR-GEO_All']['SolarAzimuthAngle'][:, :].flatten(), dtype='float32')
     obs_data[('sensorZenithAngle', metaDataName)] = np.array(g['All_Data']['ATMS-SDR-GEO_All']['SatelliteZenithAngle'][:, :].flatten(), dtype='float32')
@@ -233,7 +228,6 @@ def get_data_noaa_class(f, g, obs_data, add_qc=True):
     nlocs = len(obs_data[('latitude', metaDataName)])
     obs_data[('satelliteId', metaDataName)] = np.full((nlocs), WMO_sat_ID, dtype='int32')
     obs_data[('dateTime', metaDataName)] = np.array(get_epoch_time(g['obs_time_utc']), dtype='int64')
-    obs_data[('datetime', metaDataName)] = np.array(create_string_dtg(g['All_Data']['ATMS-SDR_All']['BeamTime'][:, :].flatten()), dtype=object)
 
     # example: dimension ( 180, 96, 22 ) == dimension( nscan, nbeam_pos, nchannel )
     nchans = len(obs_data[('sensorChannelNumber', metaDataName)])
@@ -382,6 +376,8 @@ def set_metadata_attributes(VarAttrs):
     VarAttrs[('solarZenithAngle', metaDataName)]['units'] = 'degree'
     VarAttrs[('sensorAzimuthAngle', metaDataName)]['units'] = 'degree'
     VarAttrs[('solarAzimuthAngle', metaDataName)]['units'] = 'degree'
+    VarAttrs[('dateTime', metaDataName)]['units'] = iso8601_string
+    VarAttrs[('dateTime', metaDataName)]['_FillValue'] = long_missing_value
 
     return VarAttrs
 
