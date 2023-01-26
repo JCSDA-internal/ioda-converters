7aca42e0f86ce57feea9966a928760f69e01d30e
Greg Thompson <63315545+gthompsnJCSDA@users.noreply.github.com>
2023-01-26 11:56:42 -0700

    Bugfix:  found times when converters crashed when testing with dozens of input files (#1151)
    * when running dozens of input files, found a few that failed and bug fixing for robustness
    
    * update test ref files after bugfix
    
    Co-authored-by: gthompsnJCSDA <gthompsn@ucar.edu>
    Co-authored-by: Patrick Nichols <patjnichols@gmail.com>

--- src/conventional/sonde_bufr2ioda.py
+++ src/conventional/sonde_bufr2ioda.py
@@ -692,16 +692,6 @@ def read_bufr_message(f, count, start_pos, data):
             meta_data['geopotentialHeight'][0] = float_missing_value
             meta_data['pressure'][0] = float_missing_value
 
-        # Very rarely the first level is way up high then followed by at surface. Toss first values.
-        if len(meta_data['pressure']) > 2:
-            if (meta_data['pressure'][0] < 60000 and meta_data['pressure'][1] > meta_data['pressure'][0]):
-                meta_data['pressure'][0] = float_missing_value
-                meta_data['geopotentialHeight'][0] = float_missing_value
-                temp_data['windDirection'][0] = float_missing_value
-                temp_data['windSpeed'][0] = float_missing_value
-                temp_data['airTemperature'][0] = float_missing_value
-                temp_data['dewpointTemperature'][0] = float_missing_value
-
         # And now processing the observed variables we care about.
         nbad = 0
         for variable in raw_obsvars:
@@ -749,7 +739,7 @@ def read_bufr_message(f, count, start_pos, data):
         for n, dewpoint in enumerate(vals['dewpointTemperature']):
             pres = meta_data['pressure'][n]
             if dewpoint and pres:
-                if (dewpoint > 50 and dewpoint < 325 and pres > 100 and pres < 109900):
+                if (dewpoint > 196 and dewpoint < 325 and pres > 100 and pres < 109900):
                     spfh[n] = met_utils.specific_humidity(dewpoint, pres)
 
         airt = np.full(target_number, float_missing_value)
@@ -761,7 +751,7 @@ def read_bufr_message(f, count, start_pos, data):
         tvirt = np.full(target_number, float_missing_value)
         for n, temp in enumerate(airt):
             pres = meta_data['pressure'][n]
-            if (temp != float_missing_value and spfh[n] and pres < 108000 and pres > 10000):
+            if (temp != float_missing_value and spfh[n] != float_missing_value and pres < 108000 and pres > 10000):
                 qvapor = max(1.0e-12, spfh[n]/(1.0-spfh[n]))
                 tvirt[n] = temp*(1.0 + 0.61*qvapor)
 
--- test/testoutput/2021081612_sonde_small.nc
+++ test/testoutput/2021081612_sonde_small.nc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b83099bb032ab09ce3001f0cad1413af66fb8535e686fd9985591f0e15e315a9
-size 54512
+oid sha256:7d738fb86b3923b7f99ceb9f9770032bb20697c0009840ced8a505a285ab1ea1
+size 50416
