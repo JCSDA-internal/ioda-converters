ec7282b61a9b918e5328e3f55d59a20a3c858a5e
BenjaminRuston <benr@ucar.edu>
2023-01-26 02:05:31 +0000

    fix coding norms

--- src/lib_python/def_jedi_utils.py
+++ src/lib_python/def_jedi_utils.py
@@ -9,6 +9,7 @@
 
 import numpy as np
 
+
 def concat_obs_dict(obs_data, append_obs_data):
     # For now we are assuming that the obs_data dictionary has the "golden" list
     # of variables. If one is missing from append_obs_data, the obs_data variable
