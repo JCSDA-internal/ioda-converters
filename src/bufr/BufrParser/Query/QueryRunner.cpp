8b35f9a2de93ca946acbc26bd0f1bf9b3df275c2
emilyhcliu <36091766+emilyhcliu@users.noreply.github.com>
2023-03-07 04:07:12 +0800

    Add FFT remapping for ATMS in bufr converter (#1170)
    * Add FFT remaping capability for ATMS
    
    * Add YAMLS for ATMS and ATMS remapping
    
    * Add auxiliary file for ATMS remapping
    
    * Modify output filename in ATMS YAMLs
    
    * Fix coding norm errors
    
    * Add test output file
    
    * Turn off debugging output
    
    * Modify file names.
    
    * Change noaa-20 to n20
    
    * fix error in path of output file
    
    * Update test output file
    
    * Two updates: (1) separate long line of code into two lines (2) add parantheses in the expression to force all coompilers to execute the subtraction in the order specified.
    
    * Reduce diagnostic output and fix a bug for uninitialized variables under specific conditions
    
    ---------
    
    Co-authored-by: Patrick Nichols <patjnichols@gmail.com>

--- src/bufr/BufrParser/Query/QueryRunner.cpp
+++ src/bufr/BufrParser/Query/QueryRunner.cpp
@@ -382,7 +382,7 @@ namespace bufr {
                             for (int pathIdx = currentPath.size() - 1; pathIdx >= 0; --pathIdx) {
                                 returnNodeIdx = dataProvider_->getLink(
                                         dataProvider_->getJmpb(currentPath[pathIdx]));
-                                lastNonZeroReturnIdx = currentPathReturns.size() - pathIdx;
+                                lastNonZeroReturnIdx = (currentPathReturns.size() - 1) - pathIdx;
 
                                 if (returnNodeIdx != 0) break;
                             }
