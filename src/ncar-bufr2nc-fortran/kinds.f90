6485e44e1926fa12f419949fe0cb9cb7c1359f51
Patrick Nichols <patjnichols@gmail.com>
2023-03-10 22:01:35 +0000

    fixed covering most files with fortran kinds

--- src/ncar-bufr2nc-fortran/kinds.f90
+++ src/ncar-bufr2nc-fortran/kinds.f90
@@ -6,7 +6,7 @@ module kinds
 !   prgmer: pnichols                                  data: 2023-03-09
 !
 ! abstract:  Module to hold specification kinds for variable declaration.
-!            This module is based on (copied from) Paul vanDelst's 
+!            This module is based on (copied from) Paul vanDelst's
 !            type_kinds module found in the community radiative transfer
 !            model. Modifed to take
 !
@@ -39,73 +39,42 @@ module kinds
 !
 !$$$ end documentation block
 !  use intrinsic fortran types
-  use,intrinsic :: iso_fortran_env
-  implicit none
-  private
+   use, intrinsic :: iso_fortran_env
+   implicit none
+   private
 
 ! Integer type definitions below
 
 ! Integer types
-  integer, parameter, public  :: i_byte  = int8       ! one byte integer
-  integer, parameter, public  :: i_short = int16      ! short integer
-  integer, parameter, public  :: i_long  = int64      ! long  integer = 64 bit on LP64 systems
-  integer, parameter, public  :: i_init = int32       ! 32 bit integer
-  integer, parameter, private :: llong_t = int64      ! llong integer
-  integer, parameter, public  :: i_llong = int64      ! long long
+   integer, parameter, public  :: i_byte = int8       ! one byte integer
+   integer, parameter, public  :: i_short = int16      ! short integer
+   integer, parameter, public  :: i_int = int32       ! 32 bit integer
+   integer, parameter, public  :: i_long = int64      ! long  integer = 64 bit on LP64 systems
+   integer, parameter, public  :: i_llong = int64      ! long long
 
 ! Expected 8-bit byte sizes of the integer kinds
-  integer, parameter, public :: num_bytes_for_i_byte  = 1
-  integer, parameter, public :: num_bytes_for_i_short = 2
-  integer, parameter, public :: num_bytes_for_i_int   = 4
-  integer, parameter, public :: num_bytes_for_i_long  = 8
-  integer, parameter, public :: num_bytes_for_i_llong = 8
+   integer, parameter, public :: num_bytes_for_i_byte = 1
+   integer, parameter, public :: num_bytes_for_i_short = 2
+   integer, parameter, public :: num_bytes_for_i_int = 4
+   integer, parameter, public :: num_bytes_for_i_long = 8
+   integer, parameter, public :: num_bytes_for_i_llong = 8
 
 ! Define arrays for default definition
-  integer, parameter, private :: num_i_kinds = 5
-  integer, parameter, dimension( num_i_kinds ), private :: integer_types = (/ &
-       i_byte, i_short, i_int, i_long,  i_llong  /) 
-  integer, parameter, dimension( num_i_kinds ), private :: integer_byte_sizes = (/ &
-       num_bytes_for_i_byte, num_bytes_for_i_short, num_bytes_for_i_int, &
-       num_bytes_for_i_long, num_bytes_for_i_llong  /)
-
-! Default values
-! **** CHANGE THE FOLLOWING TO CHANGE THE DEFAULT INTEGER TYPE KIND ***
-  integer, parameter, private :: default_integer = 3  ! 1=byte, 
-                                                      ! 2=short,
-                                                      ! 3=int 
-                                                      ! 4=long, 
-                                                      ! 5=llong
-  integer, parameter, public  :: i_kind = integer_types( default_integer )
-  integer, parameter, public  :: num_bytes_for_i_kind = &
-       integer_byte_sizes( default_integer )
-
-
-! Real definitions below
+   integer, parameter, public :: i_kind = int32
+   integer, parameter, public :: num_bytes_for_ikind = 4
 
 ! Real types
-  integer, parameter, public  :: r_single = real32  ! single precision
-  integer, parameter, public  :: r_double = real64 ! double precision
-  integer, parameter, private :: quad_t   = real128 ! quad precision
-  integer, parameter, public  :: r_quad   = real128
+   integer, parameter, public  :: r_single = real32  ! single precision
+   integer, parameter, public  :: r_double = real64 ! double precision
+   integer, parameter, public  :: r_quad = real128 ! quad precision
 
 ! Expected 8-bit byte sizes of the real kinds
-  integer, parameter, public :: num_bytes_for_r_single = 4
-  integer, parameter, public :: num_bytes_for_r_double = 8
-  integer, parameter, public :: num_bytes_for_r_quad   = 16
+   integer, parameter, public :: num_bytes_for_r_single = 4
+   integer, parameter, public :: num_bytes_for_r_double = 8
+   integer, parameter, public :: num_bytes_for_r_quad = 16
 
 ! Define arrays for default definition
-  integer, parameter, private :: num_r_kinds = 3
-  integer, parameter, dimension( num_r_kinds ), private :: real_kinds = (/ &
-       r_single, r_double, r_quad    /) 
-  integer, parameter, dimension( num_r_kinds ), private :: real_byte_sizes = (/ &
-       num_bytes_for_r_single, num_bytes_for_r_double, &
-       num_bytes_for_r_quad    /)
-
-! Default values
-! **** CHANGE THE FOLLOWING TO CHANGE THE DEFAULT REAL TYPE KIND ***
-  integer, parameter, private :: default_real = 1  ! 1=single, 
-  integer, parameter, public  :: r_kind = real_kinds( default_real )
-  integer, parameter, public  :: num_bytes_for_r_kind = &
-       real_byte_sizes( default_real )
+   integer, parameter, public  :: r_kind = r_single
+   integer, parameter, public  :: num_bytes_for_r_kind = num_bytes_for_r_single
 
 end module kinds
