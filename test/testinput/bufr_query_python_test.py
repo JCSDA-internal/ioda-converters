ee7fe957fb703387c08be5997ed5cb73936ab839
Ron McLaren <ronlaren@aol.com>
2023-03-23 15:24:18 -0400

    Added the beginning of a unit test

--- test/testinput/bufr_query_python_test.py
+++ test/testinput/bufr_query_python_test.py
@@ -1,7 +1,7 @@
 import bufr
 
 if __name__ == '__main__':
-    path = '/Users/rmclaren/Work/ioda-converters/test/testinput/gdas.t00z.1bhrs4.tm00.bufr_d'
+    path = './testinput/gdas.t00z.1bhrs4.tm00.bufr_d'
     with bufr.File(path) as f:
         q = bufr.QuerySet()
         q.add('latitude', '*/CLON')
@@ -9,7 +9,7 @@ if __name__ == '__main__':
         q.add('radiance', '*/BRIT{1}/TMBR')
         q.add('radiance_all', '*/BRIT/TMBR')
 
-    r = f.execute(q)
+        r = f.execute(q)
 
     lat = r.get('latitude')
     lon = r.get('longitude')
