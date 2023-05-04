02ed133f51e420d17eb40c4b18ac0cec99e78b45
Emily Liu <emily.liu@noaa.gov>
2023-05-03 20:53:20 -0500

    Add FOV remapping and conversion to scan angles. Update IASI yaml.

--- src/bufr/BufrParser/Exports/Variables/SensorScanAngleVariable.cpp
+++ src/bufr/BufrParser/Exports/Variables/SensorScanAngleVariable.cpp
@@ -28,6 +28,8 @@ namespace
         const char* HeightOfStation = "heightOfStation";
         const char* ScanStart = "scanStart";
         const char* ScanStep = "scanStep";
+        const char* ScanStepAdjust = "scanStepAdjust";
+        const char* Sensor = "sensor";
     }  // namespace ConfKeys
 
     const std::vector<std::string> FieldNames = {ConfKeys::FieldOfViewNumber,
@@ -51,8 +53,20 @@ namespace Ingester
         checkKeys(map);
 
         // Get input parameters for sensor scan angle calculation
+        std::string sensor;
+        if (conf_.has(ConfKeys::Sensor) )
+        {
+             sensor = conf_.getString(ConfKeys::Sensor);
+        }
+        else
+        {
+            throw eckit::BadParameter("Missing required parameters: sensor"
+                                      "Check your configuration.");
+        }
+
         float start;
         float step;
+        float stepAdj;
         if (conf_.has(ConfKeys::ScanStart) & conf_.has(ConfKeys::ScanStep))
         {
              start = conf_.getFloat(ConfKeys::ScanStart);
@@ -64,6 +78,18 @@ namespace Ingester
                                       "Check your configuration.");
         }
 
+        if (conf_.has(ConfKeys::ScanStepAdjust) & sensor == "iasi" )
+        {
+             sensor = conf_.getString(ConfKeys::Sensor);
+             stepAdj = conf_.getFloat(ConfKeys::ScanStepAdjust);
+        }
+        else
+        {
+            throw eckit::BadParameter("Missing required parameters: scan step adjust and sensor "
+                                      "Check your configuration.");
+        }
+       
+
         // Read the variables from the map
         auto& satghtObj = map.at(getExportKey(ConfKeys::HeightOfStation));
         auto& fovnObj = map.at(getExportKey(ConfKeys::FieldOfViewNumber));
@@ -71,6 +97,7 @@ namespace Ingester
         // Declare and initialize scanline array
         // scanline has the same dimension as fovn
         std::vector<float> scanang(fovnObj->size(), DataObject<float>::missingValue());
+        std::vector<int> scanpos(fovnObj->size(), DataObject<int>::missingValue());
 
         // Get field-of-view number
         std::vector<int> fovn(fovnObj->size(), DataObject<int>::missingValue());
@@ -79,10 +106,43 @@ namespace Ingester
            fovn[idx] = fovnObj->getAsInt(idx);
         }
 
-        // Calculate sensor scan angle
-        for (size_t idx = 0; idx < fovnObj->size(); idx++)
+        if (sensor == "iasi")
         {
-           scanang[idx] = start + static_cast<float>(fovn[idx]-1) * step;
+           for (size_t idx = 0; idx < fovnObj->size(); idx++)
+           {
+              scanpos[idx] = (fovnObj->getAsInt(idx) - 1) / 2 + 1;
+           }
+        }
+        else
+        {
+           for (size_t idx = 0; idx < fovnObj->size(); idx++)
+           {
+              scanpos[idx] = fovnObj->getAsInt(idx);
+           }
+        }
+
+        if (sensor == "iasi")
+        {
+           float tmp;
+           tmp = -stepAdj;
+           // Calculate sensor scan angle
+           for (size_t idx = 0; idx < fovnObj->size(); idx++)
+           {
+              if (scanpos[idx] % 2 == 1) 
+              { 
+                 tmp = stepAdj; 
+
+              } 
+              scanang[idx] = start + static_cast<float>((fovn[idx]-1)/4) * step + tmp;
+           }
+        }
+        else
+        {
+           for (size_t idx = 0; idx < fovnObj->size(); idx++)
+           {
+              scanang[idx] = start + static_cast<float>(fovn[idx]-1) * step;
+           }
+
         }
 
         // Export sensor scan angle (view angle)
