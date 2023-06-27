4b71da504955232b6dc7cffc287200c5c7e1e88c
Ron McLaren <ronlaren@aol.com>
2023-06-27 14:32:49 -0400

    Updated the bufr_query_python_to_ioda_test to use the ioda_obs_space module.

--- test/testinput/bufr_query_python_to_ioda_test.py
+++ test/testinput/bufr_query_python_to_ioda_test.py
@@ -7,7 +7,7 @@
 import numpy as np
 
 from pyiodaconv import bufr
-from pyioda import ioda
+from pyioda import ioda_obs_space as ioda_ospace
 
 DATA_PATH = './testinput/gdas.t00z.1bhrs4.tm00.bufr_d'
 OUTPUT_PATH = './testrun/bufr_query_python_to_ioda_test.nc'
@@ -37,65 +37,41 @@ def test_bufr_to_ioda():
 
     datetime = r.get_datetime('year', 'month', 'day', 'hour', 'minute', 'second')
 
-    # Write the data to an IODA file
-
-    # Create the variable creation parameters. Make sure to set the fill value and set one on
-    # each variable. Otherwise, the "missing" will be set to 0.0.
-
-    pfloat = ioda.VariableCreationParameters()
-    pfloat.setFillValue.float(rad.fill_value)
-    pfloat.compressWithGZIP()
-
-    pdatetime = ioda.VariableCreationParameters()
-    pdatetime.setFillValue.int(datetime.fill_value.astype(np.int64))
-    pdatetime.compressWithGZIP()
-
-
-    g = ioda.Engines.HH.createFile(name=OUTPUT_PATH,
-                                   mode=ioda.Engines.BackendCreateModes.Truncate_If_Exists)
-
     # Create the dimensions
-    num_locs = rad.shape[0]
-    num_chans = rad.shape[1]
+    dims = {'Location': rad.shape[0],
+            'Channel': rad.shape[1]}
 
-    dim_location = g.vars.create('Location', ioda.Types.int32, [num_locs])
-    dim_location.scales.setIsScale('Location')
+    # Create the IODA ObsSpace
+    obsspace = ioda_ospace.ObsSpace(OUTPUT_PATH, mode='w', dim_dict=dims)
 
-    dim_channel = g.vars.create('Channel', ioda.Types.int32, [num_chans])
-    dim_channel.scales.setIsScale('Channel')
-
-    # # Create some globals
-    my_global = g.atts.create('MyGlobal_str', ioda.Types.str, [1])
-    my_global.writeVector.str(['My Global String Data'])
-
-    my_global_int = g.atts.create('MyGlobal_int', ioda.Types.int, [10])
-    my_global_int.writeVector.int([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    # Create the global attributes
+    obsspace.write_attr('MyGlobal_str', 'My Global String Data')
+    obsspace.write_attr('MyGlobal_int', [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
     # Create the variables
-    timestamp = g.vars.create('MetaData/Timestamp', ioda.Types.int64, scales=[dim_location], params=pdatetime)
-    timestamp.atts.create('units', ioda.Types.str).writeVector.str(['seconds_since_epoch'])
-    timestamp.atts.create('long_name', ioda.Types.str).writeVector.str(['Timestamp'])
+    obsspace.create_var('MetaData/Timestamp', dtype=np.int64, fillval=datetime.fill_value.astype('int64')) \
+        .write_attr('units', 'seconds_since_epoch') \
+        .write_attr('long_name', 'Timestamp') \
+        .write_data(datetime.astype('int64'))
 
-    longitude = g.vars.create('MetaData/Longitude', ioda.Types.float, scales=[dim_location], params=pfloat)
-    longitude.atts.create('valid_range', ioda.Types.float, [2]).writeVector.float([-180, 180])
-    longitude.atts.create('units', ioda.Types.str).writeVector.str(['degrees_east'])
-    longitude.atts.create('long_name', ioda.Types.str).writeVector.str(['Longitude'])
+    obsspace.create_var('MetaData/Latitude', dtype=lat.dtype, fillval=lat.fill_value) \
+        .write_attr('units', 'degrees_north') \
+        .write_attr('long_name', 'Latitude') \
+        .write_attr('valid_range', [-90.0, 90.0]) \
+        .write_data(lat)
 
-    latitude = g.vars.create('MetaData/Latitude', ioda.Types.float,  scales=[dim_location], params=pfloat)
-    latitude.atts.create('valid_range', ioda.Types.float, [2]).writeVector.float([-90, 90])
-    latitude.atts.create('units', ioda.Types.str).writeVector.str(['degrees_north'])
-    latitude.atts.create('long_name', ioda.Types.str).writeVector.str(['Latitude'])
+    obsspace.create_var('MetaData/Longitude', dtype=lon.dtype, fillval=lon.fill_value) \
+        .write_attr('units', 'degrees_east') \
+        .write_attr('long_name', 'Longitude') \
+        .write_attr('valid_range', [-180.0, 180.0]) \
+        .write_data(lon)
 
-    tb = g.vars.create('ObsValue/brightnessTemperature', ioda.Types.float, scales=[dim_location, dim_channel], params=pfloat)
-    tb.atts.create('valid_range', ioda.Types.float, [2]).writeVector.float([100, 500])
-    tb.atts.create('units', ioda.Types.str).writeVector.str(['K'])
-    tb.atts.create('long_name', ioda.Types.str).writeVector.str(['ATMS Observed (Uncorrected) Brightness Temperature'])
-
-    # Write the data to the variables
-    timestamp.writeNPArray.int64(datetime.astype(np.int64))
-    longitude.writeNPArray.float(lon.flatten())
-    latitude.writeNPArray.float(lat.flatten())
-    tb.writeNPArray.float(rad.flatten())
+    obsspace.create_var('ObsValue/brightnessTemperature',  dtype=rad.dtype,
+                        dim_list=['Location', 'Channel'], fillval=rad.fill_value) \
+        .write_attr('units', 'K') \
+        .write_attr('long_name', 'ATMS Observed (Uncorrected) Brightness Temperature') \
+        .write_attr('valid_range', [100.0, 500.0]) \
+        .write_data(rad)
 
 if __name__ == '__main__':
     test_bufr_to_ioda()
--- test/testoutput/bufr_query_python_to_ioda_test.nc
+++ test/testoutput/bufr_query_python_to_ioda_test.nc
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:15f9fd7f4863e293933af3003af47e8cb2ff0488cac37a7f208f547a140ebd72
-size 317531
+oid sha256:72c8216f982b8d4da9caedd7ad699c893fdd592fd32140ab13d62529d5cb6751
+size 363412
