d0064acbb99f5bca51abb67d3d69ad3737cfb873
Patrick Nichols <patjnichols@gmail.com>
2023-07-06 21:25:12 +0000

    put in fixes for env

--- test/testinput/prepbufr_adpsfc_api.py
+++ test/testinput/prepbufr_adpsfc_api.py
@@ -4,16 +4,16 @@
 # which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
 
 import sys
-sys.path.append('/work2/noaa/da/nesposito/ioda-bundle_apifix/build/lib/python3.9/')
-sys.path.append('/work2/noaa/da/nesposito/ioda-bundle_apifix/build/lib/python3.9/pyiodaconv/')
-sys.path.append('/work2/noaa/da/nesposito/ioda-bundle_apifix/build/lib/python3.9/pyioda/')
+#sys.path.append('/work2/noaa/da/nesposito/ioda-bundle_apifix/build/lib/python3.9/')
+#sys.path.append('/work2/noaa/da/nesposito/ioda-bundle_apifix/build/lib/python3.9/pyiodaconv/')
+#sys.path.append('/work2/noaa/da/nesposito/ioda-bundle_apifix/build/lib/python3.9/pyioda/')
 
 import numpy as np
 import numpy.ma as ma
 import pyiodaconv as iconv
 import pyiodaconv as iconio
 from pyiodaconv import bufr
-import ioda
+from pyioda import ioda
 import calendar
 import time
 
