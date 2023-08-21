783b75e61f5917fb7ad7e2805ba50b6cf7375497
Patrick Nichols <patjnichols@gmail.com>
2023-08-21 16:42:36 -0600

    consolidate and move kinds.f90 to a utils directory that will be the name place to multipurpose code

--- src/utils/iodaconv_kinds.f90
+++ src/utils/iodaconv_kinds.f90
@@ -1,4 +1,4 @@
-module kinds
+module iodaconv_kinds
 !$$$  module documentation block
 !                .      .    .                                       .
 ! module:   kinds
@@ -77,4 +77,4 @@ module kinds
    integer, parameter, public  :: r_kind = r_single
    integer, parameter, public  :: num_bytes_for_r_kind = num_bytes_for_r_single
 
-end module kinds
+end module iodaconv_kinds
