3cd89cab48481da2951de089f57d9656c6422350
BenjaminRuston <benr@ucar.edu>
2023-08-30 16:11:18 -0600

    some more changes standardizing converting

--- src/land/ghcn_snod2ioda.py
+++ src/land/ghcn_snod2ioda.py
@@ -15,13 +15,14 @@ from dateutil.parser import parse
 import pyiodaconv.ioda_conv_engines as iconv
 from collections import defaultdict, OrderedDict
 from pyiodaconv.orddicts import DefaultOrderedDict
+from pyiodaconv.def_jedi_utils import epoch, iso8601_string
 from pyiodaconv.def_jedi_utils import record_time
 
 locationKeyList = [
     ("latitude", "float"),
     ("longitude", "float"),
     ("stationElevation", "float"),
-    ("dateTime", "string")
+    ("dateTime", "long"),
 ]
 
 AttrData = {
@@ -33,6 +34,18 @@ DimDict = {
 VarDims = {
     'totalSnowDepth': ['Location'],
 }
+
+float_missing_value = iconv.get_default_fill_val(np.float32)
+int_missing_value = iconv.get_default_fill_val(np.int32)
+long_missing_value = iconv.get_default_fill_val(np.int64)
+
+
+def get_epoch_time(adatetime):
+
+    # take python datetime object and convert to seconds from epoch
+    time_offset = round((adatetime - epoch).total_seconds())
+
+    return time_offset
 
 
 class ghcn(object):
@@ -58,19 +71,19 @@ class ghcn(object):
         self.varAttrs[iodavar, iconv.OvalName()]['coordinates'] = 'longitude latitude'
         self.varAttrs[iodavar, iconv.OerrName()]['coordinates'] = 'longitude latitude'
         self.varAttrs[iodavar, iconv.OqcName()]['coordinates'] = 'longitude latitude'
-        self.varAttrs[iodavar, iconv.OvalName()]['units'] = 'm'
-        self.varAttrs[iodavar, iconv.OerrName()]['units'] = 'm'
+        self.varAttrs[iodavar, iconv.OvalName()]['units'] = 'mm'
+        self.varAttrs[iodavar, iconv.OerrName()]['units'] = 'mm'
 
         def assignValue(colrowValue, df400):
             if colrowValue == '' or pd.isnull(colrowValue):
-                outList = -999.0
+                outList = float_missing_value
             else:
                 ml = df400.loc[df400['ID'] == colrowValue, "DATA_VALUE"]
             # check if the series is empty
             if not(ml.empty):
                 outList = ml.iloc[0]  # watersourceSer.append(ml.iloc[0])
             else:
-                outList = -999.0
+                outList = float_missing_value
             return outList
 
         cols = ["ID", "DATETIME", "ELEMENT", "DATA_VALUE", "M_FLAG", "Q_FLAG", "S_FLAG", "OBS_TIME"]
@@ -106,15 +119,14 @@ class ghcn(object):
         df10 = df10.drop_duplicates(subset=["ID"])
 
         # use stations list as the number of obs points
-        # if no data for a station and a given date, leave -999.0
+        # if no data for a station and a given date, leave float_missing_value
         num_obs = len(df10.index)
 
         # Initialzed data array
-        vals = np.full((num_obs), -999.0)
-        lats = np.full((num_obs), -999.0)
-        lons = np.full((num_obs), -999.0)
-        alts = np.full((num_obs), -999.0)
-        site = np.full((num_obs), -9999, dtype=np.int)
+        vals = np.full((num_obs), float_missing_value)
+        lats = np.full((num_obs), float_missing_value)
+        lons = np.full((num_obs), float_missing_value)
+        alts = np.full((num_obs), float_missing_value)
         id_array = np.chararray((num_obs))
         id_array[:] = "UNKNOWN"
 
@@ -124,7 +136,7 @@ class ghcn(object):
         id_array = df10["ID"].values
 
         df100 = pd.DataFrame(data=id_array, columns=['ID'])
-        df100.assign(DATA_VALUE=-999.0)
+        df100.assign(DATA_VALUE=float_missing_value)
         df30Temp = df30.loc[df30["DATETIME"] == new_date]
         df100["DATA_VALUE"] = df100.apply(lambda row: assignValue(row['ID'], df30Temp), axis=1)
         df30Temp = None
@@ -137,7 +149,7 @@ class ghcn(object):
         qflg = 0*vals.astype('int32')
         errs = 0.0*vals
         sites = np.empty_like(vals, dtype=object)
-        times = np.empty_like(vals, dtype=object)
+        times = np.empty_like(vals, dtype='int64')
         sites = id_array
 
         # use maskout options
@@ -156,11 +168,14 @@ class ghcn(object):
 
         # get datetime from input
         my_date = datetime.strptime(startdate, "%Y%m%d%H")
-        base_datetime = my_date.strftime('%Y-%m-%dT%H:00:00Z')
+        # base_datetime = my_date.strftime('%Y-%m-%dT%H:00:00Z')  # not needed
+        epoch_time = np.int64(get_epoch_time(my_date))
+        print(f' calculated epoch_time: {epoch_time}')
 
         # vals[vals >= 0.0] *= 0.001      # mm to meters
-        errs[:] = 0.04
-        times[:] = base_datetime
+        errs[:] = 0.04                  # error in meters
+        # errs[:] = 40.0
+        times[:] = epoch_time
         # add metadata variables
         self.outdata[('dateTime', 'MetaData')] = times
         self.outdata[('stationIdentification', 'MetaData')] = sites
@@ -168,6 +183,8 @@ class ghcn(object):
         self.outdata[('longitude', 'MetaData')] = lons
         self.outdata[('stationElevation', 'MetaData')] = alts
         self.varAttrs[('stationElevation', 'MetaData')]['units'] = 'm'
+        self.varAttrs[('dateTime', 'MetaData')]['units'] = iso8601_string
+        self.varAttrs[('dateTime', 'MetaData')]['_FillValue'] = long_missing_value
 
         self.outdata[self.varDict[iodavar]['valKey']] = vals
         self.outdata[self.varDict[iodavar]['errKey']] = errs
