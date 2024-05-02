6193c14595e098f3f8847fbcba28ca1256120492
BenjaminRuston <benr@ucar.edu>
2024-05-02 13:21:11 -0700

    add fillvalue attribute for datetime

--- src/land/imsfv3_scf2ioda.py
+++ src/land/imsfv3_scf2ioda.py
@@ -18,7 +18,10 @@ from collections import defaultdict, OrderedDict
 from pyiodaconv.orddicts import DefaultOrderedDict
 from pyiodaconv.def_jedi_utils import iso8601_string
 
+float_missing_value = iconv.get_default_fill_val(np.float32)
+int_missing_value = iconv.get_default_fill_val(np.int32)
 long_missing_value = iconv.get_default_fill_val(np.int64)
+metaDataName = iconv.MetaDataName()
 
 locationKeyList = [
     ("latitude", "float"),
@@ -66,8 +69,10 @@ class imsFV3(object):
             if iodavar == 'snowCoverFraction':
                 self.varAttrs[iodavar, iconv.OvalName()]['units'] = '1'
                 self.varAttrs[iodavar, iconv.OerrName()]['units'] = '1'
+                # should use float_missing_value
                 self.varAttrs[iodavar, iconv.OvalName()]['_FillValue'] = -999.
                 self.varAttrs[iodavar, iconv.OerrName()]['_FillValue'] = -999.
+                # should use int_missing_value
                 self.varAttrs[iodavar, iconv.OqcName()]['_FillValue'] = -999
 
             if iodavar == 'totalSnowDepth':
@@ -102,12 +107,13 @@ class imsFV3(object):
         ncd.close()
 
         # add metadata variables
-        self.outdata[('dateTime', 'MetaData')] = times
-        self.varAttrs[('dateTime', 'MetaData')]['units'] = iso8601_string
-        self.outdata[('latitude', 'MetaData')] = lats
-        self.outdata[('longitude', 'MetaData')] = lons
-        self.outdata[('height', 'MetaData')] = oros
-        self.varAttrs[('height', 'MetaData')]['units'] = 'm'
+        self.outdata[('dateTime', metaDataName)] = times
+        self.varAttrs[('dateTime', metaDataName)]['units'] = iso8601_string
+        self.varAttrs[('dateTime', metaDataName)]['_FillValue'] = long_missing_value
+        self.outdata[('latitude', metaDataName)] = lats
+        self.outdata[('longitude', metaDataName)] = lons
+        self.outdata[('height', metaDataName)] = oros
+        self.varAttrs[('height', metaDataName)]['units'] = 'm'
 
         # add output variables
         for i in range(len(sncv)):
@@ -120,7 +126,7 @@ class imsFV3(object):
                     self.outdata[self.varDict[iodavar]['valKey']] = sndv
                     self.outdata[self.varDict[iodavar]['errKey']] = errsd
                     self.outdata[self.varDict[iodavar]['qcKey']] = qdflg
-        DimDict['Location'] = len(self.outdata[('dateTime', 'MetaData')])
+        DimDict['Location'] = len(self.outdata[('dateTime', metaDataName)])
 
 
 def get_observation_time(filename, sncv, ncd):
