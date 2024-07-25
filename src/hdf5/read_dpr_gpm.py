15612a374b147124248393162f072a9c63961b74
BenjaminRuston <benr@ucar.edu>
2024-07-25 16:27:39 -0700

    use PEP8 standard syntax

--- src/hdf5/read_dpr_gpm.py
+++ src/hdf5/read_dpr_gpm.py
@@ -8,8 +8,8 @@ import sys
 import copy
 
 import numpy as np
-import scipy as sp  
-import scipy.io as sio  
+import scipy as sp
+import scipy.io as sio
 import datetime, time
 import pdb
 import pandas as pd
@@ -21,9 +21,6 @@ from pyiodaconv.def_jedi_utils import iso8601_string, epoch
 
 os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
 
-# do not remove this to avoid error in reading HDF
-#from pyhdf.HDF import *
-#from pyhdf.VS import *
 
 class H5ls:
     def __init__(self):
@@ -32,17 +29,16 @@ class H5ls:
 
     def __call__(self, name, h5obj):
         # only h5py datasets have dtype attribute, so we can search on this
-        if hasattr(h5obj,'dtype') and not name in self.names:
+        if hasattr(h5obj, 'dtype') and name not in self.names:
             self.names += [name]
-
 
         # we have no return so that the visit function is recursive
 
-        
+
 # https://hdfeos.org/zoo/OTHER/2010128055614_21420_CS_2B-GEOPROF_GRANULE_P_R04_E03.hdf.py
 def read_dpr_hdf_file(fname):
     from scipy import interpolate
-    
+
     print(fname)
     df = h5py.File(fname)
     h5ls = H5ls()
@@ -52,14 +48,15 @@ def read_dpr_hdf_file(fname):
     dims = {}
     data = {}
     for k in h5ls.names:
-        if 'AlgorithmRuntimeInfo' in k: continue
-        
-        if 'DimensionNames' in  df[k].attrs:
+        if 'AlgorithmRuntimeInfo' in k:
+            continue
+
+        if 'DimensionNames' in df[k].attrs:
             data[k] = df[k][:]
-            dims[k] = df[k].attrs['DimensionNames'].decode('UTF-8').split(',')  
+            dims[k] = df[k].attrs['DimensionNames'].decode('UTF-8').split(',')
     df.close()
-    
-    unique_dims = {}        
+
+    unique_dims = {}
     for k in data:
         shape = data[k].shape
         for idim, dname in enumerate(dims[k]):
@@ -76,58 +73,57 @@ def read_dpr_hdf_file(fname):
         for dname in dims[k]:
             dname = dname.replace('method', 'method1')
             xr_dims[dname] = unique_dims[dname]
-        xr_data[k] = xr.DataArray(data[k], dims = xr_dims)
-
+        xr_data[k] = xr.DataArray(data[k], dims=xr_dims)
 
     epoch_time = np.zeros(xr_data.nscan.size)
     for iscan in xr_data.nscan.values:
         time1 = datetime.datetime(xr_data['FS/ScanTime/Year'].values[iscan],
-                        xr_data['FS/ScanTime/Month'].values[iscan],
-                        xr_data['FS/ScanTime/DayOfMonth'].values[iscan],
-                        xr_data['FS/ScanTime/Hour'].values[iscan],
-                        xr_data['FS/ScanTime/Minute'].values[iscan],
-                        xr_data['FS/ScanTime/Second'].values[iscan],
-                        xr_data['FS/ScanTime/MilliSecond'].values[iscan])
+                                  xr_data['FS/ScanTime/Month'].values[iscan],
+                                  xr_data['FS/ScanTime/DayOfMonth'].values[iscan],
+                                  xr_data['FS/ScanTime/Hour'].values[iscan],
+                                  xr_data['FS/ScanTime/Minute'].values[iscan],
+                                  xr_data['FS/ScanTime/Second'].values[iscan],
+                                  xr_data['FS/ScanTime/MilliSecond'].values[iscan])
         epoch_time[iscan] = round((time1 - epoch).total_seconds())
-    
+
     # build output dictionary
     dprdata = xr.Dataset()
-    dprdata['lat'] = xr_data['FS/Latitude']   # "nscan,nray=nfov";
-    dprdata['lon'] = xr_data['FS/Longitude']  # "nscan,nray=nfov";
-    dprdata['height'] = xr_data['FS/PRE/height'] # "nscan,nray=nfov,nbin=nelev";
+    dprdata['lat'] = xr_data['FS/Latitude']       # "nscan,nray=nfov";
+    dprdata['lon'] = xr_data['FS/Longitude']      # "nscan,nray=nfov";
+    dprdata['height'] = xr_data['FS/PRE/height']  # "nscan,nray=nfov,nbin=nelev";
     dprdata['epoch_time'] = xr_data['FS/navigation/scLat'].copy()
     dprdata['epoch_time'].values = np.squeeze(epoch_time)
-    dprdata['obs_measured'] =  xr_data['FS/PRE/zFactorMeasured'] #"nscan,nray=nfov,nbin=nelev,nfreq=nchan"
-    dprdata['obs'] =  xr_data['FS/SLV/zFactorFinal'] #"nscan,nray=nfov,nbin=nelev,nfreq=nchan"
+    dprdata['obs_measured'] = xr_data['FS/PRE/zFactorMeasured']  # "nscan,nray=nfov,nbin=nelev,nfreq=nchan"
+    dprdata['obs'] = xr_data['FS/SLV/zFactorFinal']  # "nscan,nray=nfov,nbin=nelev,nfreq=nchan"
     dprdata['obs'].values[dprdata['obs'].values < -100] = np.nan
-    dprdata['zenith_angle'] = xr_data['FS/PRE/localZenithAngle'] #   nscan,nray=nfov,nfreq=nchan    
+    dprdata['zenith_angle'] = xr_data['FS/PRE/localZenithAngle']  # nscan,nray=nfov,nfreq=nchan
     dprdata['azimuth_angle'] = dprdata['zenith_angle'].copy() * 0
-    dprdata['centerFreq'] = xr.DataArray(np.array([13.6, 35.5]), dims={"nfreq": 2}) # GHz
-    dprdata['centerWN'] = xr.DataArray(np.array([0.453, 1.183]),  dims={"nfreq": 2}) # 1/cm
-    
+    dprdata['centerFreq'] = xr.DataArray(np.array([13.6, 35.5]), dims={"nfreq": 2})  # GHz
+    dprdata['centerWN'] = xr.DataArray(np.array([0.453, 1.183]), dims={"nfreq": 2})  # 1/cm
+
     # rename dimensions
     dprdata = dprdata.rename({'nbin': 'elevation', 'nfreq': 'channel', 'nray': 'fov', 'nscan': 'scanline'})
     dprdata['fov1'] = dprdata.fov.copy()
-    
+
     return dprdata
 
 
-def read_dpr_gpm(dpr_fnames, 
-                 dpr_dir=[], 
-                 dt_start=datetime.datetime(1900,1,1), 
-                 dt_end=datetime.datetime(2100,1,1)):
+def read_dpr_gpm(dpr_fnames,
+                 dpr_dir=[],
+                 dt_start=datetime.datetime(1900, 1, 1),
+                 dt_end=datetime.datetime(2100, 1, 1)):
 
     if len(dpr_fnames) == 0:
-       # files are partitioned into almost two hours period
-       dt_start1 = dt_start - datetime.timedelta(hours=2)
-       dt_end1 = dt_end + datetime.timedelta(hours=2)
-       dt_range = pd.date_range(dt_start1, dt_end1, freq="1H")
-       dpr_fnames = []
-       for idt in dt_range:
-           #2A.GPM.DPR.V9-20211125.20170907-S075404-E092639.020033.V07A.HDF5                                   
-           file_pattern = '*.%s*.HDF5'%idt.strftime('%Y%m%d-S%H')
-           for dir,_,_ in os.walk(dpr_dir):
-               dpr_fnames.extend(glob(os.path.join(dir,file_pattern))) 
+        # files are partitioned into almost two hours period
+        dt_start1 = dt_start - datetime.timedelta(hours=2)
+        dt_end1 = dt_end + datetime.timedelta(hours=2)
+        dt_range = pd.date_range(dt_start1, dt_end1, freq="1H")
+        dpr_fnames = []
+        for idt in dt_range:
+            # 2A.GPM.DPR.V9-20211125.20170907-S075404-E092639.020033.V07A.HDF5
+            file_pattern = '*.%s*.HDF5' % idt.strftime('%Y%m%d-S%H')
+            for dir, _, _ in os.walk(dpr_dir):
+                dpr_fnames.extend(glob(os.path.join(dir, file_pattern)))
 
     if not dpr_fnames:
         return []
@@ -139,11 +135,12 @@ def read_dpr_gpm(dpr_fnames,
     for f in dpr_fnames:
         dpr_data = read_dpr_hdf_file(f)
         logid = (dpr_data.epoch_time.values >= dt_start) & (dpr_data.epoch_time.values < dt_end)
-        if np.sum(logid) == 0: continue
+        if np.sum(logid) == 0:
+            continue
         scanid = np.arange(dpr_data.scanline.size)[logid]
-        dpr_data = dpr_data.isel(scanline = scanid)
+        dpr_data = dpr_data.isel(scanline=scanid)
 
-        if not 'dpr_data1' in vars():  # not defined yet
+        if 'dpr_data1' not in vars():  # not defined yet
             dpr_data1 = dpr_data
         else:
             dpr_data1 = xr.concat([dpr_data, dpr_data1], dim='scanline')
@@ -161,5 +158,5 @@ def read_dpr_gpm(dpr_fnames,
     dpr_data1['lon'].values = lon
 
     dpr_data1["sequenceNumber"] = xr.DataArray(np.arange(dpr_data1.obs_id.size), dpr_data1.obs_id.coords)
-        
+
     return dpr_data1
