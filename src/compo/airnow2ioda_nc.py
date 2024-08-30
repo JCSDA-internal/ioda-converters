5f4d3b9dce00b92a8c062a17cb12a314767717a8
weiwilliam <weiwilliam1987@gmail.com>
2024-08-30 16:24:25 -0500

    update the header info

--- src/compo/airnow2ioda_nc.py
+++ src/compo/airnow2ioda_nc.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 
+#
+# (C) Copyright 2024 UCAR
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
 # Read airnow text data file and convert to IODA netcdf
+
 import os, sys
 from datetime import datetime
 from pathlib import Path
