da8c9511aa366374b09cfca1b6916164bf3f7565
Maryam Abdi <maryamao@ucar.edu>
2024-09-20 15:17:21 -0600

    adding GCAS converter (#1546)
    * save
    
    * save gcas converter
    
    * add time range for icartt
    
    * coding norm fixes
    
    * coding norms fix
    
    * address comments
    
    * reduced input file
    
    ---------
    
    Co-authored-by: weiwilliam <weiwilliam1987@gmail.com>
    Co-authored-by: Francois Vandenberghe <vandenb@ucar.edu>
    Co-authored-by: Benjamin Ruston <93737224+BenjaminRuston@users.noreply.github.com>

--- src/compo/gcas_nc2ioda.py
+++ src/compo/gcas_nc2ioda.py
@@ -11,7 +11,7 @@ import sys
 import argparse
 import netCDF4 as nc
 import numpy as np
-from datetime import datetime, timezone, timedelta
+from datetime import datetime, timedelta
 import os
 from pathlib import Path
 
@@ -24,29 +24,19 @@ from collections import defaultdict, OrderedDict
 from pyiodaconv.orddicts import DefaultOrderedDict
 from pyiodaconv.def_jedi_utils import epoch, iso8601_string
 
-
 # constants
+MOLECCM_2MOLM_2 = 10000/6.0221408e+23  # molec/cm2 to mol/m2
 HPA2PA = 1E2
-ppbv2molmol = 1e-9
-pptv2molmol = 1e-12
 
 float_missing_value = iconv.get_default_fill_val(np.float32)
 
-# {'iodaName' : ['obsName', 'iodaUnit', 'obsToIodaUnivConv']
-obsvars = {'nitrogendioxideInsitu': ['NO2_ACES', 'mol mol-1', ppbv2molmol],
-           'carbonmonoxideInsitu': ['CO_ppb', 'mol mol-1', ppbv2molmol],
-           'ozoneInsitu': ['O3_CL', 'mol mol-1', ppbv2molmol],
-           'formaldehydeInsitu': ['CH2O_ISAF', 'mol mol-1', pptv2molmol],
-           'airTemperature': ['T', 'K', 1],
-           'windEastward': ['U', 'm s-1', 1],
-           'windNorthward': ['V', 'm s-1', 1]}
 
+class gcas(object):
 
-class icartt(object):
-
-    def __init__(self, filenames, time_range):
+    def __init__(self, filenames, column, time_range):
 
         self.filenames = filenames
+        self.column = column
         self.time_range = time_range
         self.make_dictionaries()      # Set up variable names for IODA
         self.DimDict = {}
@@ -65,36 +55,74 @@ class icartt(object):
                 raise Exception('Unknown error opening %s' % self.filename)
 
             # Read global attributes
-            self.AttrData['platform'] = dsFlight.attrs['PLATFORM']
-            self.AttrData['description'] = dsFlight.attrs['RA']
+            self.AttrData['platform'] = dsFlight.attrs['MeasurementPlatform']
+            self.AttrData['description'] = dsFlight.attrs['RevisionNumber']
 
-            # Read lat lon
-            lats = dsFlight['Latitude'].values
-            lons = dsFlight['Longitude'].values
+            # Read lat lon and flatten from 2d (time,xtrack) and make it 1d
+            flats = dsFlight['lat'].values.flatten()
+            flats = np.where(np.isnan(flats), float_missing_value, flats)
+            lats = flats.astype(np.float32)
+
+            flons = dsFlight['lon'].values.flatten()
+            flons = np.where(np.isnan(flons), float_missing_value, flons)
+            lons = flons.astype(np.float32)
+
+            alts_1d = dsFlight['aircraft_altitude'].values.flatten()
+            xtrack = np.arange(dsFlight.dims['xtrack'])  # pixels in swath
+            nlocs = lats.shape[0]
+            alts = np.repeat(alts_1d, len(xtrack))  # repeat alt for each swath
 
             lats = lats.astype(np.float32)
             lons = lons.astype(np.float32)
+            alts = alts.astype(np.float32)
+
+            # Read 3d model alt (level) and reshape it to 2d
+            geoscf = xr.open_dataset(filename, group='_Model_Information')
+            # Model alt label in incorrect, change km to m
+            fmodel_alt = geoscf['model_altitude']*1000
+            level_dim, time_dim, xtrack_dim = fmodel_alt.shape
+            model_alt = fmodel_alt.values.reshape(level_dim, time_dim * xtrack_dim)
+
+            # Read 3d model pressure and reshape it to 2d
+            fmodel_press = geoscf['model_pressure']*HPA2PA
+            model_press = fmodel_press.values.reshape(level_dim, time_dim * xtrack_dim)
+            model_press = np.where(np.isnan(model_press), float_missing_value, model_press)
+            model_press = model_press.astype(np.float32)
+
+            # Find aircraft P using model pressure and altitude info
+            aircraft_p = np.zeros(time_dim * xtrack_dim, dtype=np.float32)
+            for t in range(time_dim * xtrack_dim):
+                differences = np.abs(np.array(model_alt[:, t]) - np.array(alts[t]))
+                idx = np.argmin(differences)
+                aircraft_p[t] = model_press[idx, t]
+
+            # Only 2 vertice: surface pressure and flight pressure
+            # For total column flight_pressure is 0
+            num_vert = 2
+            pressure_vertice = np.zeros([time_dim * xtrack_dim, num_vert], dtype=np.float32)
+            pressure_vertice[:, 1] = model_press[level_dim-1, :]  # surface pressure from model
 
             # Read time and convert to ioda time format
-            # sec since SDATE
-            stime = dsFlight['iWAS_Start_UTC'].values
-            sdate_str = dsFlight.attrs['SDATE']
-            sdate = datetime.strptime(sdate_str, '%Y, %m, %d')
-            sdate = sdate.replace(tzinfo=timezone.utc)
-            seconds_difference = (sdate - epoch).total_seconds()
+            obs_time = dsFlight['time'].values
+            time = np.repeat(obs_time, len(xtrack))
 
-            times = stime + seconds_difference
-            stime_datetime = np.datetime64(epoch.strftime("%Y-%m-%dT%H:%M:%S")) + np.timedelta64(int(seconds_difference), 's') + stime.astype('timedelta64[s]')
+            times = (time - np.datetime64(epoch.strftime("%Y-%m-%dT%H:%M:%S"))) / np.timedelta64(1, 's')
 
-            pressure = dsFlight['P'] * HPA2PA  # hPa to Pa
-            pressure = pressure.astype(np.float32)
-            nlocs = pressure.shape[0]
+            if (self.column.strip() == 'total'):
+                var_data_below = dsFlight['no2_vertical_column_below_aircraft'].values.flatten()
+                var_data_above = dsFlight['no2_vertical_column_above_aircraft'].values.flatten()
+                var_data = (var_data_below+var_data_above) * MOLECCM_2MOLM_2  # change to mol/m2
+                var_name = 'nitrogendioxideTotal'
+                pressure_vertice[:, 0] = np.zeros(time_dim * xtrack_dim, dtype=np.float32)
+            elif (self.column.strip() == 'tropo'):
+                var_data = dsFlight['no2_vertical_column_below_aircraft'].values.flatten()*MOLECCM_2MOLM_2
+                var_name = 'nitrogendioxideColumn'
+                pressure_vertice[:, 0] = aircraft_p
 
             data = {}
-            for var in self.obsvars:
-                var_data = (dsFlight[self.obsvars[var][0]] * self.obsvars[var][2])
-                var_data = np.where(np.isnan(var_data), float_missing_value, var_data)
-                data[var] = var_data.astype(np.float32)
+            # flatten 2d (time,xtrack)
+            var_data = np.where(np.isnan(var_data), float_missing_value, var_data)
+            data[var_name] = var_data.astype(np.float32)
 
             # set flag
             flag = np.full((nlocs), True)
@@ -104,7 +132,7 @@ class icartt(object):
             # date range to fit DA window
             wbegin = np.datetime64(datetime.strptime(self.time_range[0], "%Y%m%d%H"))
             wend = np.datetime64(datetime.strptime(self.time_range[1], "%Y%m%d%H"))
-            flag = np.where((stime_datetime >= wbegin) & (stime_datetime <= wend), 1, 0)
+            flag = np.where((time >= wbegin) & (time <= wend), 1, 0)
             flag = flag.astype(bool)
 
             # ---- Write Metadata and data
@@ -114,15 +142,17 @@ class icartt(object):
                 self.outData[('dateTime', 'MetaData')] = times[flag]
                 self.outData[('latitude', 'MetaData')] = lats[flag]
                 self.outData[('longitude', 'MetaData')] = lons[flag]
-                self.outData[('pressure', 'MetaData')] = pressure[flag]
+                self.outData[('aircraft_altitude', 'MetaData')] = alts[flag]
+                self.outData[('aircraft_pressure', 'MetaData')] = aircraft_p[flag]
+                self.outData[('pressureVertice', 'RetrievalAncillaryData')] = pressure_vertice[flag]
 
-                for var in self.obsvars:
-                    self.outData[self.varDict[var]['valKey']] = \
-                        data[var][flag]
-                    self.outData[self.varDict[var]['errKey']] = \
-                        obs_error[flag]
-                    self.outData[self.varDict[var]['qcKey']] = \
-                        qa[flag]
+                varDict = self.varDict.get(var_name)
+                self.outData[varDict['valKey']] = \
+                    data[var_name][flag]
+                self.outData[self.varDict[var_name]['errKey']] = \
+                    obs_error[flag]
+                self.outData[self.varDict[var_name]['qcKey']] = \
+                    qa[flag]
 
             else:
                 self.outData[('dateTime', 'MetaData')] = np.concatenate(
@@ -131,10 +161,15 @@ class icartt(object):
                     (self.outData[('latitude', 'MetaData')], lats[flag]))
                 self.outData[('longitude', 'MetaData')] = np.concatenate(
                     (self.outData[('longitude', 'MetaData')], lons[flag]))
+                self.outData[('aircraft_altitude', 'MetaData')] = np.concatenate(
+                    (self.outData[('aircraft_altitude', 'MetaData')], alts[flag]))
+                self.outData[('aircraft_pressure', 'MetaData')] = np.concatenate(
+                    (self.outData[('aircraft_pressure', 'MetaData')], aircraft_p[flag]))
+                self.outData[('pressureVertice', 'RetrievalAncillaryData')] = np.concatenate(
+                    (self.outData[('pressureVertice', 'RetrievalAncillaryData')], pressure_vertice[flag]))
 
-                for var in self.obsvars:
-                    self.outData[(var, 'valKey')] = np.concatenate(
-                        (self.outData[(var, 'valKey')], times[flag]))
+                self.outData[(var_name, 'valKey')] = np.concatenate(
+                    (self.outData[(var_name, 'valKey')], times[flag]))
                 self.outData[(var_name, 'errKey')] = np.concatenate(
                     (self.outData[(var_name, 'errKey')], times[flag]))
                 self.outData[(var_name, 'qcKey')] = np.concatenate(
@@ -144,6 +179,14 @@ class icartt(object):
 
         self.DimDict['Location'] = len(self.outData[('dateTime', 'MetaData')])
         self.AttrData['Location'] = np.int32(self.DimDict['Location'])
+
+        self.DimDict['Vertice'] = num_vert  # surface and aircraft
+        self.AttrData['Vertice'] = np.int32(self.DimDict['Vertice'])
+
+        varname = 'pressureVertice'
+        vkey = (varname, 'RetrievalAncillaryData')
+        self.varAttrs[vkey]['coordinates'] = 'longitude latitude'
+        self.varAttrs[vkey]['units'] = 'Pa'
 
     def make_dictionaries(self):
         """
@@ -161,6 +204,8 @@ class icartt(object):
         """
         Make a dictionary of obsvars
         """
+        obsvars = {"nitrogendioxideTotal",
+                   "nitrogendioxideColumn"}
         self.obsvars = obsvars
 
     def make_AttrData(self):
@@ -188,8 +233,8 @@ class icartt(object):
         """
         self.varAttrs = DefaultOrderedDict(lambda: DefaultOrderedDict(dict))
         for item in self.obsvars:
-            self.varAttrs[item, iconv.OvalName()]['units'] = self.obsvars[item][1]
-            self.varAttrs[item, iconv.OerrName()]['units'] = self.obsvars[item][1]
+            self.varAttrs[item, iconv.OvalName()]['units'] = 'mol m-2'
+            self.varAttrs[item, iconv.OerrName()]['units'] = 'mol m-2'
             self.varAttrs[item, iconv.OqcName()]['units'] = 'unitless'
 
 
@@ -203,7 +248,7 @@ def get_parser():
     # get command line arguments
     parser = argparse.ArgumentParser(
         description=(
-            'Reads icartt_nc files'
+            'Reads GCAS column netCDF files from NASA STAQS campaign'
             'and converts into IODA formatted output files. Multiple'
             'files are able to be concatenated.'),
         formatter_class=argparse.RawDescriptionHelpFormatter
@@ -213,15 +258,21 @@ def get_parser():
     required = parser.add_argument_group(title='required arguments')
     required.add_argument(
         '-i', '--input',
-        help="path of icartt_nc observation netCDF input file(s)",
+        help="path of GCAS observation netCDF input file(s)",
         type=str,
         nargs='+',
         required=True)
+
     required.add_argument(
         '-o', '--output',
         help="path of IODA output file",
         type=str,
         required=True)
+
+    required.add_argument(
+        '-c', '--column',
+        help="type of column: total or tropo",
+        type=str, required=True)
 
     optional = parser.add_argument_group(title='optional arguments')
     optional.add_argument(
@@ -239,21 +290,20 @@ def main():
     locationKeyList = [
         ("latitude", "float", "degrees_north"),
         ("longitude", "float", "degrees_east"),
-        ("pressure", "float", "Pa"),
         ("dateTime", "long", iso8601_string),
     ]
 
-    varDims = {}
-    for key in obsvars.keys():
-        variable = obsvars[key][0]
-        varDims[variable] = ['Location']
+    varDims = {
+        'x': ['Location'],
+        'pressureVertice': ['Location', 'Vertice']
+    }
 
     # -- read command line arguments
     parser = get_parser()
     args = parser.parse_args()
 
     # Read in the flight data
-    flightData = icartt(args.input, args.time_range)
+    flightData = gcas(args.input, args.column, args.time_range)
 
     # setup the IODA writer
     writer = iconv.IodaWriter(args.output, locationKeyList, flightData.DimDict)
