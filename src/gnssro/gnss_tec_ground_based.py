2c3af01a74ddc51d0c14391b2530d79b7aed1297
BenjaminRuston <benr@ucar.edu>
2024-11-05 18:53:22 -0800

    getting there creating first draft of ground based TEC decoder not functional at this time

---
+++ src/gnssro/gnss_tec_ground_based.py
@@ -0,0 +1,444 @@
+#!/usr/bin/env python3
+
+#
+# (C) Copyright 2019-2024 UCAR
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+#
+
+#
+# decode TENET formatted ground-based GNSS Total Electron Content (TEC)
+#
+
+import sys
+import os
+import time
+from datetime import datetime, timezone
+import numpy as np
+import logging
+
+# These modules need the path to lib-python modules
+import pyiodaconv.ioda_conv_engines as iconv
+from pyiodaconv.orddicts import DefaultOrderedDict
+from pyiodaconv.def_jedi_utils import iso8601_string, epoch
+
+os.environ["TZ"] = "UTC"
+
+# these are the unique values in the raw input file
+varDict = { 'vertical total electron content': ['verticalTEC', "float", 'fractional percent'],
+           'slant total electron content': ['slantTEC', "float", "m"],
+}
+
+# these are the MetaData common to each input
+locationKeyList = [
+    ('latitude', 'float', 'degrees_north'),
+    ('longitude', 'float', 'degrees_east'),
+    ('pseudoRandomNoiseCode', 'integer'),
+    ('elevationAngle', 'float', 'GNSS transmitter satellite elevation angle in degrees'),
+    ('azimuthAngle', 'float', 'aziumuth angle viewing GNSS transmitter in degrees west'),
+    ('height', 'float'),
+    ('xECEFPosition', 'float', 'receiving station Earth Centered Earth Fixed X-coordinate in meters'),
+    ('yECEFPosition', 'float', 'receiving station Earth Centered Earth Fixed Y-coordinate in meters'),
+    ('zECEFPosition', 'float', 'receiving station Earth Centered Earth Fixed Z-coordinate in meters'),
+    ('xECEFPositionGNSS', 'float', 'GNSS transmitting satellite Earth Centered Earth Fixed X-coordinate in meters'),
+    ('yECEFPositionGNSS', 'float', 'GNSS transmitting satellite Earth Centered Earth Fixed Y-coordinate in meters'),
+    ('zECEFPositionGNSS', 'float', 'GNSS transmitting satellite Earth Centered Earth Fixed Z-coordinate in meters'),
+    ('dateTime', 'long', 'seconds from epoch 01Jan1970')
+    ('WMOstationIdentifier', 'integer', 'WMO assigned number for the site'),
+    ('stationIdentifier', 'string', 'GNSS ground-based receiving station name'),
+]
+
+meta_keys = [m_item[0] for m_item in locationKeyList]
+
+GlobalAttrs = {
+    'converter': os.path.basename(__file__),
+    'ioda_version': 3,
+    'description': 'Ground-based Total Electron Content',
+    'source': 'TENET Network',
+}
+
+metaDataName = iconv.MetaDataName()
+obsValName = iconv.OvalName()
+obsErrName = iconv.OerrName()
+qcName = iconv.OqcName()
+
+float_missing_value = iconv.get_default_fill_val(np.float32)
+int_missing_value = iconv.get_default_fill_val(np.int32)
+long_missing_value = iconv.get_default_fill_val(np.int64)
+string_missing_value = '_'
+
+missing_vals = {'string': string_missing_value,
+                'integer': int_missing_value,
+                'long': long_missing_value,
+                'float': float_missing_value}
+dtypes = {'string': object,
+          'integer': np.int32,
+          'long': np.int64,
+          'float': np.float32}
+
+
+def main(args):
+
+    file_names = args.input
+    output_file = args.output
+
+    start_time = time.time()
+
+    data = None
+    any_data = False
+    for fname in file_names:
+        logging.info(f"Reading file:  {fname}")
+        file_data, any_data = read_file(fname, any_data, qc_strict=args.qc_strict)
+        # if there is data do something
+        if file_data:
+            # first successful read
+            if not data:
+                data = file_data
+            # subsequent successful read
+            else:
+                for key in set(varDict.keys()).union(meta_keys):
+                    # data[key] = np.concatenate(data[key], file_data[key]) # why not and gotta be much better way
+                    data[key] = np.append(data[key], file_data[key])
+
+    # if all files have no data
+    if not any_data:
+        logging.error("No data to write, stopping execution.")
+        sys.exit()
+
+    dtg = datetime.fromtimestamp(data['dateTime'][0])
+    datetimeRef = dtg.isoformat() + "Z"
+
+    # prepare global attributes we want to output in the file,
+    # in addition to the ones already loaded in from the input file
+    GlobalAttrs = {
+        'sourceFiles': ", ".join(file_names),
+        'datetimeReference': datetimeRef
+    }
+
+    nlocs = len(data['height'])
+    logging.info(f" found a total of {nlocs} observations")
+    DimDict = {'Location': nlocs}
+
+    varDims = {}
+    for key in varDict.keys():
+        variable = varDict[key][0]
+        varDims[variable] = ['Location']
+
+    varAttrs = DefaultOrderedDict(lambda: DefaultOrderedDict(dict))
+
+    # Set units of the MetaData variables and all _FillValues.
+    for key in meta_keys:
+        dtype = locationKeyList[meta_keys.index(key)][1]
+        if locationKeyList[meta_keys.index(key)][2]:
+            varAttrs[(key, metaDataName)]['units'] = locationKeyList[meta_keys.index(key)][2]
+        varAttrs[(key, metaDataName)]['_FillValue'] = missing_vals[dtype]
+    for key in varDict.keys():
+        if 'electronDensity' in key:
+            continue
+        dtype = varDict[key][1]
+        units = varDict[key][2]
+        if units:
+            varAttrs[(key, metaDataName)]['units'] = units
+        varAttrs[(key, metaDataName)]['_FillValue'] = missing_vals[dtype]
+
+    # Set units and FillValue attributes for groups associated with observed variable.
+    for key in ['electronDensity', 'criticalFrequency']:
+        variable = varDict[key][0]
+        dtype = varDict[key][1]
+        units = varDict[key][2]
+        unitsErr = varDict[key+'Confidence'][2]
+        varAttrs[(variable, obsValName)]['units'] = units
+        varAttrs[(variable, obsErrName)]['units'] = unitsErr
+        varAttrs[(variable, obsValName)]['coordinates'] = 'longitude latitude'
+        varAttrs[(variable, obsErrName)]['coordinates'] = 'longitude latitude'
+        varAttrs[(variable, qcName)]['coordinates'] = 'longitude latitude'
+        varAttrs[(variable, obsValName)]['_FillValue'] = missing_vals[dtype]
+        varAttrs[(variable, obsErrName)]['_FillValue'] = missing_vals[dtype]
+        varAttrs[(variable, qcName)]['_FillValue'] = int_missing_value
+
+    # Fill the final IODA data:  MetaData then ObsValues, ObsErrors, and QC
+    ioda_data = {}
+
+#   should populate ioda_data directly rather than creating another copy
+    for key in meta_keys:
+        dtype = locationKeyList[meta_keys.index(key)][1]
+        ioda_data[(key, metaDataName)] = np.array(data[key], dtype=dtypes[dtype])
+    for key in varDict.keys():
+        variable = varDict[key][0]
+        dtype = varDict[key][1]
+        if 'electronDensity' not in key and 'criticalFrequency' not in key:
+            logging.info(f" the variable: {variable} will be placed into MetaData of ioda_data")
+            # these MetaData are arrays nlocs long already
+            ioda_data[(key, metaDataName)] = np.array(data[variable], dtype=dtypes[dtype])
+        elif 'Confidence' not in key:
+            # (electronDensityConfidence) is used as the ObsError
+            variable = varDict[key][0]
+            logging.info(f" the variable: {variable} will be placed into ObsValue of ioda_data")
+            ioda_data[(variable, obsValName)] = np.array(data[variable], dtype=np.float32)
+            ioda_data[(variable, obsErrName)] = np.array(data[variable+'Confidence'], dtype=np.float32)
+            qc_array_hack = apply_gross_quality_control(data, qc_strict=args.qc_strict)
+            ioda_data[(variable, qcName)] = np.array(qc_array_hack, dtype=np.int32)  # how to interpret AQI ?
+
+    logging.debug("Writing file: " + output_file)
+
+    # setup the IODA writer and write everything out.
+    writer = iconv.IodaWriter(output_file, locationKeyList, DimDict)
+    writer.BuildIoda(ioda_data, varDims, varAttrs, GlobalAttrs)
+
+    logging.info("--- {:9.4g} total seconds ---".format(time.time() - start_time))
+
+
+def read_file(file_name, any_data, qc_strict=True):
+
+    local_data = init_data_dict()
+
+    # Open the file
+    with open(file_name, 'r') as file:
+        # Create an iterator from the file object
+        file_iterator = iter(file)
+
+        local_data, header_read = get_header(file_iterator, local_data)
+        logging.debug(f'header was read w/o error: {header_read}')
+
+        while header_read and True:
+            try:
+                # Get the next line from the iterator
+                line = next(file_iterator)
+                local_data = populate_obsValue(line, local_data)
+            except StopIteration:
+                # If StopIteration is raised, break from the loop
+                break
+
+    # has any data been read at any point
+    any_data = any_data or header_read
+
+    # repeat all the metaData values
+    if header_read:
+        nlocs = len(local_data['height'])
+        for key in meta_keys:
+            dtype = locationKeyList[meta_keys.index(key)][1]
+            local_data[key] = np.full(nlocs, local_data[key], dtype=dtypes[dtype])
+
+    # if the header was not sucessfully read return nothing
+    if not header_read:
+        local_data = None
+
+    return local_data, any_data
+
+
+def get_header(file_iterator, local_data):
+    #####################################################
+    # get header (3 lines)
+    #####################################################
+
+    # Line #1: HIUS1 KJPL 080213
+    # This line is standard in all TENET files the only thing that changes is the numeric value 080213
+    # This value indicates the Coordinated Universal Time (UTC) JPL produces the TENET file
+    # The format is: DDHHMM (where DD = day, HH = UTC hour, and MM = minutes)
+
+    # Line #2: TENET
+    # This line is standard in all TENET files and never changes. It indicates the nature of the file – TENET
+
+    # Line #3: 23074 0694/ 271608 NRIL 000064539804 002253881156 005946178085
+    # WMO number, latitude (N), longitude (W), station name, ECEF station coordinates
+
+    # for all ECEF coordinates 0 in front indicates positive, and 1 indicates negative.
+
+    header_read = False
+    while True:
+        # read first line
+        try:
+            _, _, report_time = line.split()
+        except ValueError:
+            break
+
+        # read second line
+        line = next(file_iterator)
+        if 'TENET' not in line:
+            break  # not necessarily needed
+
+        # read third line
+        line = next(file_iterator)
+        try:
+            WMOid, latitude, longitude, stationName, xECEFPosition, yECEFPosition, zECEFPosition = line.split()
+        except ValueError:
+            break
+
+        xECEFPosition = convert_ECEF_string(xECEFPosition)
+        yECEFPosition = convert_ECEF_string(yECEFPosition)
+        zECEFPosition = convert_ECEF_string(zECEFPosition)
+
+        except ValueError:
+            break
+
+            local_data['latitude'] = np.append(local_data['latitude'], lat)
+            local_data['longitude'] = np.append(local_data['longitude'], lon)
+            local_data['stationIdentifier'] = np.append(local_data['stationIdentifier'], stationID)
+            local_data['hmF2'] = np.append(local_data['hmF2'], hmf2)
+            local_data['foF2'] = np.append(local_data['foF2'], fof2)
+            local_data['nmF2'] = np.append(local_data['nmF2'], nmf2)
+            local_data['foF2Uncertainty'] = np.append(local_data['foF2Uncertainty'], conf_fof2)
+            local_data['nmF2Uncertainty'] = np.append(local_data['nmF2Uncertainty'], conf_nmf2)
+            local_data['sequenceNumber'] = np.append(local_data['sequenceNumber'], recordNumber)
+            # populate the first row of ObsValues
+            local_data = populate_obsValue(line, local_data)
+
+            header_read = True
+        except StopIteration:
+            # If StopIteration is raised, break from the loop
+            break
+
+    return local_data, header_read
+
+
+def populate_obsValue(line, local_data):
+
+    # get the electron density retrieved from GNSS transmitter
+    # if can correctly parse all fields populate local_data otherwise do nothing
+
+    # ObsValue data row (example)
+    # Line #4: 11111 180208 021000 280771 26911 2165166150 4210322950 0233/ 3558/ 102029725204 114864753823 022550194624
+    # unknown, YYMMDD, HHMMSS, PRN/Latitude, Longitude, VOBS, SOBS, satelite elevation angle, azimuth angle, GNSS ECEF coordinates
+
+    # for all ECEF coordinates 0 in front indicates positive, and 1 indicates negative.
+
+
+    # read data lines beginning at fourth line
+    try:
+        _, yymmdd, hhmmss, PRNlatitude, longitude, vobs, sobs, elevationAngle, azimuthAngle, \
+        xECEFPositionGNSS, yECEFPositionGNSS, zECEFPositionGNSS = line.split()
+    except ValueError:
+        break
+
+    dateTime = convert_string_to_dateTime(yymmdd, hhmmss)
+    PRN, latitude = parse_station_and_latitude(PRNlatitude)
+
+    xECEFPositionGNSS = convert_ECEF_string(xECEFPositionGNSS)
+    yECEFPositionGNSS = convert_ECEF_string(yECEFPositionGNSS)
+    zECEFPositionGNSS = convert_ECEF_string(zECEFPositionGNSS)
+
+    local_data['dateTime'] = np.append(local_data['dateTime'], dateTime)
+    local_data['pseudoRandomNoiseCode'] = np.append(local_data['longitude'], PRN)
+    local_data['latitude'] = np.append(local_data['latitude'], latitude)
+    local_data['longitude'] = np.append(local_data['longitude'], longitude)
+    local_data['elevationAngle'] = np.append(local_data['elevationAngle'], elevationAngle)
+    local_data['azimuthAngle'] = np.append(local_data['azimuthAngle'], azimuthAngle)
+    local_data['verticalElectronDensity'] = np.append(local_data['verticalElectronDensity'], vobs)
+    local_data['slantElectronDensity'] = np.append(local_data['slantElectronDensity'], sobs)
+
+    return local_data
+
+
+def convert_ECEF_string(c):
+    # for all ECEF coordinates 0 in front indicates positive, and 1 indicates negative.
+    try:
+        # Attempt the conversion logic
+        result = -int(c[1:]) if c[0] == '1' else int(c[1:]) if c[0] == '0' else int(c)
+        return result
+    except (IndexError, ValueError) as e:
+        # Raise an exception with a descriptive message
+        raise ValueError(f"Invalid input string '{c}': {e}")
+
+
+def convert_string_to_dateTime(yymmdd, hhmmss):
+
+    # convert strings of time into python dateTime object
+    try:
+        # Combine and parse the string as a datetime object
+        dtg = datetime.strptime(f"{yymmdd}{hhmmss}", '%y%m%d%H%M%S')
+        # Set timezone to UTC
+        dtg = dtg.replace(tzinfo=timezone.utc)
+        # Convert to Unix time in seconds since epoch
+        dateTime = np.int64(round((dtg - epoch).total_seconds()))
+        
+        return dateTime
+    
+    except ValueError as e:
+        # Raise an informative error if parsing fails
+        raise ValueError(f"Invalid date or time format in inputs '{yymmdd}' and '{hhmmss}': {e}")
+
+
+def parse_station_and_latitude(code):
+    try:
+        # Extract the station code (first two digits)
+        station_code = int(code[:2])
+        
+        # Determine latitude sign based on the third digit
+        latitude_sign = 1 if code[2] == '0' else -1 if code[2] == '1' else None
+        if latitude_sign is None:
+            raise ValueError(f"Invalid latitude sign indicator '{code[2]}'; expected '0' for positive or '1' for negative.")
+        
+        # Extract the latitude (remaining digits) and apply sign and scale
+        latitude = latitude_sign * int(code[3:]) / 10.0
+        
+        return station_code, latitude
+    
+    except ValueError as e:
+        raise ValueError(f"Invalid input '{code}': {e}")
+
+
+def apply_gross_quality_control(data, qc_strict=False):
+    # if strict quality-control is requested
+    # apply using simple physical reality check on variables
+
+    # initialize returned variable
+    qc_array_hack = np.zeros_like(data['electronDensity'], dtype=np.int32)
+    # is requested apply check
+    if qc_strict:
+        qc_array_hack = np.where(
+            (data['electronDensity'].astype(float) < 0)
+            | (data['height'].astype(float) < 0)
+            | (data['criticalFrequency'].astype(float) < 0),
+            1,
+            0)
+    return qc_array_hack
+
+
+def init_data_dict():
+    local_data = {}              # Before assigning the output types into the above.
+    for key in set(varDict.keys()).union(meta_keys):
+        local_data[key] = []
+    return local_data
+
+
+if __name__ == "__main__":
+
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser(
+        description=('Read a satwind AMV ascii/csv file from SSEC'
+                     ' and convert into IODA output file')
+    )
+
+    required = parser.add_argument_group(title='required arguments')
+    required.add_argument('-i', '--input', nargs='+',
+                          action='store', default=None, required=True,
+                          help='input files')
+    required.add_argument('-o', '--output',
+                          action='store', default=None, required=True,
+                          help='output file')
+
+    optional = parser.add_argument_group(title='optional arguments')
+    optional.add_argument('--debug', action='store_true', default=False,
+                          help='enable debug messages')
+    optional.add_argument('--quality-control', action='store_true',
+                          default=False, dest='qc_strict',
+                          help='add PreQC values')
+    optional.add_argument('--verbose', action='store_true', default=False,
+                          help='enable verbose debug messages')
+
+    args = parser.parse_args()
+
+    if args.debug:
+        logging.basicConfig(level=logging.INFO)
+    elif args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.ERROR)
+
+    for file_name in args.input:
+        if not os.path.isfile(file_name):
+            parser.error('Input (-i option) file: ', file_name, ' does not exist')
+
+    main(args)
