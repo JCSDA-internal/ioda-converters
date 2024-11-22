388899fd0d053b83e2af52974b2be4fce324465f
BenjaminRuston <benr@ucar.edu>
2024-11-22 15:05:19 -0800

    adjust bit checking to start at 0 rather than 1

--- src/hdf5/tropics_2ioda.py
+++ src/hdf5/tropics_2ioda.py
@@ -198,7 +198,7 @@ def get_data(f, obs_data, skip=1):
     # Bit 7: Night
     # Bit 8: Payload rear orientation'
     quality_word = np.vstack(np.stack(f['calQualityFlag'], axis=2))
-    obs_data[('satelliteAscendingFlag', metaDataName)] = np.array(get_normalized_bit(quality_word[:, 0], bit_index=6), dtype='int32')
+    obs_data[('satelliteAscendingFlag', metaDataName)] = np.array(get_normalized_bit(quality_word[:, 0], bit_index=5), dtype='int32')
 
     # check some global satellite geometry will compress all data using this
     chk_geolocation = (obs_data[('latitude', metaDataName)] > 90) | (obs_data[('latitude', metaDataName)] < -90) | \
@@ -269,7 +269,7 @@ def get_data_deprecated(f, obs_data, skip=1):
     # Bit 7: Day/Night
     # Bit 8: Payload forward/aft"
     quality_word = np.vstack(np.stack(f['calQualityFlag'], axis=2))
-    obs_data[('satelliteAscendingFlag', metaDataName)] = np.array(get_normalized_bit(quality_word[:, 0], bit_index=6), dtype='int32')
+    obs_data[('satelliteAscendingFlag', metaDataName)] = np.array(get_normalized_bit(quality_word[:, 0], bit_index=5), dtype='int32')
 
     # check some global satellite geometry will compress all data using this
     chk_geolocation = (obs_data[('latitude', metaDataName)] > 90) | (obs_data[('latitude', metaDataName)] < -90) | \
@@ -289,14 +289,14 @@ def get_data_deprecated(f, obs_data, skip=1):
 def set_missing_value(nchans, chk_geolocation, quality_word, obs_key, obs_data, skip=1):
     # use quality word to determine where to set for missing values
     for jchan in np.arange(nchans):
-        i_land = get_normalized_bit(quality_word[:, jchan], bit_index=1)
-        i_intrusion = get_normalized_bit(quality_word[:, jchan], bit_index=2)
-        i_maneuver = get_normalized_bit(quality_word[:, jchan], bit_index=3)
-        i_cold_cal = get_normalized_bit(quality_word[:, jchan], bit_index=4)
-        i_hot_cal = get_normalized_bit(quality_word[:, jchan], bit_index=5)
-        i_asc = get_normalized_bit(quality_word[:, jchan], bit_index=6)
-        i_day = get_normalized_bit(quality_word[:, jchan], bit_index=7)
-        i_forward = get_normalized_bit(quality_word[:, jchan], bit_index=8)
+        i_land = get_normalized_bit(quality_word[:, jchan], bit_index=0)
+        i_intrusion = get_normalized_bit(quality_word[:, jchan], bit_index=1)
+        i_maneuver = get_normalized_bit(quality_word[:, jchan], bit_index=2)
+        i_cold_cal = get_normalized_bit(quality_word[:, jchan], bit_index=3)
+        i_hot_cal = get_normalized_bit(quality_word[:, jchan], bit_index=4)
+        i_asc = get_normalized_bit(quality_word[:, jchan], bit_index=5)
+        i_day = get_normalized_bit(quality_word[:, jchan], bit_index=6)
+        i_forward = get_normalized_bit(quality_word[:, jchan], bit_index=7)
         chk_ob = (i_cold_cal + i_hot_cal + i_intrusion + i_maneuver + chk_geolocation) > 0
         obs_data[obs_key][:, jchan][chk_ob] = float_missing_value
 
