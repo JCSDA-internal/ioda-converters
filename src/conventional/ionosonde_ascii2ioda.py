07f7c7388c23457d314d81d67168c53f610a02d5
Hui Shao <huishao@ucar.edu>
2024-12-09 20:40:11 -0600

    change electron density unit to cm-3

--- src/conventional/ionosonde_ascii2ioda.py
+++ src/conventional/ionosonde_ascii2ioda.py
@@ -18,7 +18,7 @@ os.environ["TZ"] = "UTC"
 varDict = {'criticalFrequency': ['criticalFrequency', "float", 'm s-1'],
            'criticalFrequencyConfidence': ['criticalFrequencyConfidence', "float", 'fractional percent'],
            'height': ['height', "float", "m"],
-           'electronDensity': ['electronDensity', "float", 'number / m^3'],
+           'electronDensity': ['electronDensity', "float", 'number / cm^3'],
            'electronDensityConfidence': ['electronDensityConfidence', "float", 'number / m^3']}
 
 # these are the MetaData common to each input
@@ -295,6 +295,10 @@ def populate_obsValue(line, local_data):
         height, freq, f_conf, density, density_conf = map(float, [height, freq, f_conf, density, density_conf])
 
         if all(x > 0 for x in [freq, f_conf, density, density_conf]):
+
+            # Scale the electron density by 1e6 (for cm^-3 from m^-3)
+            density = density * 1e6
+
             local_data['height'] = np.append(local_data['height'], height)
             local_data['criticalFrequency'] = np.append(local_data['criticalFrequency'], freq)
             local_data['criticalFrequencyConfidence'] = np.append(local_data['criticalFrequencyConfidence'], f_conf)
