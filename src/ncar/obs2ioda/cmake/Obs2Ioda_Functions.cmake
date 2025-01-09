8a1144fc0affe991e7fa1d9dd48c76d01ab15e8e
Andy Stokely <117313253+amstokely@users.noreply.github.com>
2025-01-09 16:41:17 -0700

    Refactor obs2ioda_fortran_target by splitting it into two functions: obs2ioda_fortran_library and obs2ioda_fortran_executable. (#31)
    Previously, the function configured both Fortran libraries and their corresponding executables, limiting flexibility. This update introduces separate functions for libraries and executables, removing their dependency and enabling standalone library creation. These changes improve modularity, clarify function use, and enhance maintainability of the CMake build system.

--- src/ncar/obs2ioda/cmake/Obs2Ioda_Functions.cmake
+++ src/ncar/obs2ioda/cmake/Obs2Ioda_Functions.cmake
@@ -1,10 +1,10 @@
 include("${CMAKE_SOURCE_DIR}/cmake/Obs2Ioda_CompilerFlags.cmake")
 
-# This CMake function, `obs2ioda_fortran_target`, configures Fortran targets for obs2ioda.
+# This CMake function, `obs2ioda_fortran_library_target`, configures Fortran library targets for obs2ioda.
 #
 # Its arguments are:
 # - target: the name of the target to configure
-# - target_main: the main source file for the executable
+# - public_link_libraries: the public link libraries associated with the target
 #
 # The function sets the following properties for the target:
 # - The directory for Fortran module files
@@ -14,17 +14,14 @@ include("${CMAKE_SOURCE_DIR}/cmake/Obs2Ioda_CompilerFlags.cmake")
 # - Compiler-specific options and flags, depending on whether the GNU Fortran or Intel Fortran compiler is used,
 #   and whether the build type is Debug or not
 #
-# The function also links the public libraries to the target and creates an executable `obs2ioda_${target}` linked
-# to the original target.
-function(obs2ioda_fortran_target target target_main)
+# The function also links the provided public libraries to the target.
+function(obs2ioda_fortran_library target public_link_libraries)
     set_target_properties(${target} PROPERTIES Fortran_MODULE_DIRECTORY ${CMAKE_BINARY_DIR}/${OBS2IODA_MODULE_DIR})
     target_include_directories(${target} INTERFACE $<BUILD_INTERFACE:${CMAKE_BINARY_DIR}/${OBS2IODA_MODULE_DIR}>
                                $<INSTALL_INTERFACE:${OBS2IODA_MODULE_DIR}>)
     #Relocatable, portable, runtime dynamic linking
     set_target_properties(${target} PROPERTIES INSTALL_RPATH "\$ORIGIN/../${CMAKE_INSTALL_LIBDIR}")
     # Global Fortran configuration
-    set(public_link_libraries_name ${target}_PUBLIC_LINK_LIBRARIES)
-    set(public_link_libraries ${${public_link_libraries_name}})
     set_target_properties(${target} PROPERTIES Fortran_FORMAT FREE)
 
     # Compiler-specific options and flags
@@ -50,9 +47,22 @@ function(obs2ioda_fortran_target target target_main)
     endif ()
     target_compile_options(${target} PRIVATE ${OBS2IODA_FORTRAN_TARGET_COMPILE_OPTIONS_PRIVATE})
     target_link_libraries(${target} PUBLIC ${public_link_libraries})
-    add_executable(obs2ioda_${target} ${target_main})
-    target_link_libraries(obs2ioda_${target} PUBLIC ${target})
+endfunction()
 
+# This CMake function, `obs2ioda_fortran_executable`, configures the installation and linking of Fortran executables for obs2ioda.
+#
+# Its arguments are:
+# - target: the name of the executable target to configure
+# - public_link_libraries: the public link libraries associated with the target
+#
+# The function performs the following:
+# - Sets the install RPATH for the target to enable finding shared libraries relative to the executable's location.
+# - Links the provided public libraries to the target using `target_link_libraries`.
+#
+# This ensures that the target has the correct runtime library paths and is properly linked with its public dependencies.
+function(obs2ioda_fortran_executable target public_link_libraries)
+    set_target_properties(${target} PROPERTIES INSTALL_RPATH "\$ORIGIN/../${CMAKE_INSTALL_LIBDIR}")
+    target_link_libraries(${target} PUBLIC ${public_link_libraries})
 endfunction()
 
 # This CMake function, `obs2ioda_cxx_library`, configures C++ targets for obs2ioda.
