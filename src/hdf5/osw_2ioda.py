da563a617e1a493f0bff3f1328ecddaeba25a727
BenjaminRuston <benr@ucar.edu>
2025-01-17 18:01:52 -0800

    and return a one if bit is set

--- src/hdf5/osw_2ioda.py
+++ src/hdf5/osw_2ioda.py
@@ -387,7 +387,7 @@ def quality_control(obs_data, qc_strict=False):
 
 
 def get_normalized_bit(value, bit_index):
-    return (int(value) >> bit_index) & 1 == 0
+    return (int(value) >> bit_index) & 1 == 1
 
 
 if __name__ == "__main__":
