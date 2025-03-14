d2eecec9c822aa0b4f42aee4e980a89b63fe7620
Andy Stokely <117313253+amstokely@users.noreply.github.com>
2025-03-14 15:37:41 -0600

    Define Global Variables for NetCDF Dimensions (#57)
    * Created qa branch.
    
    * Create global vars for all dims.
    
    * Create global var for each dim.
    
    * Reverted to original logic for creating nchans dim var.
    
    * Added ws around if statement ( and ).
    
    * Fixed dimension variable name error in write_obs.
    
    * Simplified netcdfPutAtt call in write_obs.
    
    * Updated logic for string array variables that formerly relied on the variable being an nf90_char.
    
    * Fixed coordinate variable dimension name argument bug.
    
    Rebased on `main` to undo changes in `hsd.f90`, `prepbufr_mod.f90`, `radiance_mod.f90`, and `satwnd_mod.f90`, which came from the `feature/char_to_string_netcdf_vars` branch.

--- src/ncar/obs2ioda/obs2ioda-v2/src/ncio_mod.f90
+++ src/ncar/obs2ioda/obs2ioda-v2/src/ncio_mod.f90
@@ -137,13 +137,12 @@ subroutine write_obs (filedate, write_opt, outdir, itim)
 
       status = netcdfAddDim(netcdfID, trim(ncname), val_ncdim(1), ncid_ncdim(1))
       status = netcdfPutAtt(netcdfID, trim(ncname), val_ncdim(ncid_ncdim(1)))
-      if ( trim(ncname) == 'nchans' ) then
-         status = netcdfAddVar(netcdfID, trim(ncname), NF90_INT, 1, [trim(ncname)], fillValue = -999)
-      end if
+      status = netcdfAddVar(netcdfID, trim(ncname), NF90_INT, 1, [trim(ncname)])
 
       do i = 2, n_ncdim
          status = netcdfAddDim(netcdfID, trim(name_ncdim(i)), val_ncdim(i), ncid_ncdim(i))
-         status = netcdfPutAtt(netcdfID, trim(name_ncdim(ncid_ncdim(i))), val_ncdim(i))
+         status = netcdfPutAtt(netcdfID, trim(name_ncdim(i)), val_ncdim(i))
+         status = netcdfAddVar(netcdfID, trim(name_ncdim(i)), NF90_INT, 1, [trim(name_ncdim(i))])
       end do
 
       ! define global attributes
