f820afb4b6487100d3ac9d722c27dc4a783b5a31
yuanxue2870 <yuan.xue@noaa.gov>
2025-05-12 13:47:16 +0000

    change errsd from 40 to 80 based on emails on 05/12/2025

--- src/land/imsfv3_scf2ioda.py
+++ src/land/imsfv3_scf2ioda.py
@@ -100,7 +100,7 @@ class imsFV3(object):
         qdflg = 0*sndv.astype('int32')
         errsc = 0.0*sncv
         errsd = 0.0*sndv
-        errsd[:] = 40.
+        errsd[:] = 80.
 
         times = get_observation_time(self.filename, sncv, ncd)
 
