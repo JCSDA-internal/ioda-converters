bfc639841b34e277de8005c0fbf33edfd8d228bd
weiwilliam <weiwilliam1987@gmail.com>
2025-09-29 15:44:27 -0600

    update caliop converter and its testoutput

--- src/compo/caliop_l2_ext2ioda.py
+++ src/compo/caliop_l2_ext2ioda.py
@@ -149,6 +149,7 @@ class caliop_l2ext(object):
         self.outdata[('longitude', metaDataName)] = np.array([], dtype=np.float32)
         self.outdata[('dateTime', metaDataName)] = np.array([], dtype=np.int64)
         self.outdata[('pressure', metaDataName)] = np.array([], dtype=np.float32)
+        self.outdata[('sequenceNumber', metaDataName)] = np.array([], dtype=np.int32)
         self.outdata[('cloudAerosolDiscriminationHigher', metaDataName)] = np.array([], dtype=np.int32)
         self.outdata[('cloudAerosolDiscriminationLower', metaDataName)] = np.array([], dtype=np.int32)
         for iodavar in obsvars:
@@ -184,22 +185,26 @@ class caliop_l2ext(object):
             thickness[tmpidx] = thickness[tmpidx + 1]
             thickness[tmpidx - 1] = thickness[tmpidx - 1] + np.abs(thickness[tmpidx] - oldthick)
 
+        prev_nloc = 0
         for f in self.filenames:
             sd = SD(f, SDC.READ)
 
             pres = sd.select('Pressure').get().ravel() * 1e2  # hPa to Pa
             lats = sd.select('Latitude').get()[:, 1]
-            nloc = lats.size
-            lats = np.repeat(lats, nlev)
             lons = sd.select('Longitude').get()[:, 1]
-            lons = np.repeat(lons, nlev)
-            profidx = np.arange(lats.size)
-            profidx = np.repeat(profidx, nlev)
             proftime = sd.select('Profile_UTC_Time').get()[:, 1]
             obs_time = np.array([(pt - epoch).total_seconds() for pt in self.caliop_time2dt(proftime)],
                                 dtype=np.int64)
+
+            nloc = lats.size
+            profidx = np.arange(prev_nloc, prev_nloc + nloc)
+
+            lats = np.repeat(lats, nlev)
+            lons = np.repeat(lons, nlev)
+            profidx = np.repeat(profidx, nlev)
             obs_time = np.repeat(obs_time, nlev)
             winmsk = ((obs_time >= self.wbeg) & (obs_time <= self.wend))
+            prev_nloc += nloc
 
             obs = np.zeros((nloc * nlev, nchan))
             err = np.zeros_like(obs)
@@ -241,6 +246,8 @@ class caliop_l2ext(object):
                                                                  np.array(obs_time[winmsk], dtype=np.int64))
             self.outdata[('pressure', metaDataName)] = np.append(self.outdata[('pressure', metaDataName)],
                                                                  np.array(pres[winmsk], dtype=np.float32))
+            self.outdata[('sequenceNumber', metaDataName)] = np.append(self.outdata[('sequenceNumber', metaDataName)],
+                                                                       np.array(profidx[winmsk], dtype=np.int32))
             self.outdata[('cloudAerosolDiscriminationHigher', metaDataName)] = np.append(
                 self.outdata[('cloudAerosolDiscriminationHigher', metaDataName)], np.array(cad1[winmsk], dtype=np.int32))
             self.outdata[('cloudAerosolDiscriminationLower', metaDataName)] = np.append(
@@ -260,7 +267,10 @@ class caliop_l2ext(object):
         self.outdata[('sensorCentralFrequency', metaDataName)] = np.array(frequency, dtype=np.float32)[output_chidx]
         self.outdata[('height', metaDataName)] = np.array(alt, dtype=np.float32)
         self.outdata[('atmosphereLayerThicknessZ', metaDataName)] = np.array(thickness, dtype=np.float32)
-        self.outdata[('sequenceNumber', metaDataName)] = profidx
+
+        tmpseq = self.outdata[('sequenceNumber', metaDataName)]
+        self.outdata[('sequenceNumber', metaDataName)] = tmpseq - min(tmpseq)
+
         DimDict['Location'] = len(self.outdata[('dateTime', metaDataName)])
         DimDict['Channel'] = np.array(channels)
         DimDict['Level'] = np.arange(nlev)
