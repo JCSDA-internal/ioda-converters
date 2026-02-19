26de1d56c5b25a8de0087390e568b6237b32f36d
Lindsey Hayden <haydenlj@ucar.edu>
2026-02-19 08:46:25 -0700

    Fix window issues in GNSS TEC converters (#1742)
    * Fix window issues in GNSS TEC converters
    
    * remove debuging statements
    
    * remove debuging statements
    
    * coding norms
    
    * fix bad auto merge

--- src/space_weather/gnss_gridded_ground_tec_madrigal2ioda.py
+++ src/space_weather/gnss_gridded_ground_tec_madrigal2ioda.py
@@ -41,6 +41,7 @@ def main(args):
     print(f'{len(files)} files to read')
     obs_data = {}
     part_one = None
+    og_sindex = 0
     for ifile in files:
         ds = h5py.File(ifile)
 
@@ -57,7 +58,6 @@ def main(args):
         nout = 0
         for sindex in range(0, times, int(inc/2)):
             obs_data = get_obs_data(ds, (sindex, sindex + int(inc/2)), file_type, seqStart)
-            nout = nout + 1
 
             if not obs_data:
                 print(f"INFO: non-nominal file skipping")
@@ -65,17 +65,20 @@ def main(args):
 
             if nout % 2 == 0:
                 part_one = obs_data.copy()
+                og_sindex = sindex
+                nout = nout + 1
                 continue
             elif nout % 2 != 0 and part_one:
                 for k in part_one.keys():
                     obs_data[k] = np.append(part_one[k], obs_data[k])
+                nout = nout + 1
             # prepare global attributes we want to output in the file,
             # in addition to the ones already loaded in from the input file
             GlobalAttrs = {}
             if file_type == 'nc':
-                dtg = datetime.utcfromtimestamp(ds['timestamps'][sindex + int(inc/2)])
+                dtg = datetime.utcfromtimestamp(ds['timestamps'][og_sindex + int(inc/2)])
             else:
-                dtg = datetime.utcfromtimestamp(ds['Data']['Array Layout']['timestamps'][sindex + int(inc/2)])
+                dtg = datetime.utcfromtimestamp(ds['Data']['Array Layout']['timestamps'][og_sindex + int(inc/2)])
 
             GlobalAttrs['datetimeReference'] = dtg.strftime("%Y-%m-%dT%H:%M:%SZ")
             date_time = np.array(int(dtg.strftime("%Y%m%d%H%M")), dtype=str)
@@ -114,7 +117,7 @@ def main(args):
             writer.BuildIoda(obs_data, VarDims, VarAttrs, GlobalAttrs)
 
 
-def get_meta_data(ds, indices, file_type):
+def get_meta_data(ds, indices, file_type, seqStart):
     # these are the MetaData we are interested in
     meta_data = {}
 
--- src/space_weather/gnss_los_ground_tec_madrigal2ioda.py
+++ src/space_weather/gnss_los_ground_tec_madrigal2ioda.py
@@ -50,9 +50,9 @@ def main(args):
             continue
 
         ctime = times[0]
-        while ctime <= times[-1] + int(window / 2):
-            wbegin = ctime - int(window / 2)
-            wend = ctime + int(window / 2)
+        while ctime <= times[-1]:
+            wbegin = ctime
+            wend = ctime + window
 
             tindex = (times > wbegin) & (times <= wend)
             obs_data = get_obs_data(ds, tindex, seqStart)
@@ -72,7 +72,7 @@ def main(args):
             # prepare global attributes we want to output in the file,
             # in addition to the ones already loaded in from the input file
             GlobalAttrs = {}
-            dtg = datetime.fromtimestamp(ctime)
+            dtg = datetime.fromtimestamp(ctime + int(window / 2))
             GlobalAttrs['datetimeReference'] = dtg.strftime("%Y-%m-%dT%H:%M:%SZ")
             date_time = np.array(int(dtg.strftime("%Y%m%d%H%M")), dtype=str)
             GlobalAttrs['date_time'] = date_time.item()
@@ -106,6 +106,7 @@ def main(args):
             VarAttrs[('longitude', 'MetaData')]['_FillValue'] = float_missing_value
 
             # final write to IODA file
+            print(f'writing {output}')
             writer.BuildIoda(obs_data, VarDims, VarAttrs, GlobalAttrs)
 
             ctime = ctime + window
@@ -203,8 +204,9 @@ if __name__ == "__main__":
         type=int, default=60)
     optional.add_argument(
         '--sequence',
-        help="Value to use as first sequence number. SequenceNumber variable will count up by one from this number."
-             "Default 1"
+        help="Value to use as first sequence number."
+             "SequenceNumber variable will count up by one from this number."
+             "Default 1",
         type=int, default=1)
 
     args = parser.parse_args()
