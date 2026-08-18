c59deca9d1ce11363be1df9df49728db8d4b009a
BenjaminRuston <benr@ucar.edu>
2026-08-18 10:28:24 -0700

    remove import of compute_scan_angle when not used

--- src/hdf5/wsfm_2ioda.py
+++ src/hdf5/wsfm_2ioda.py
@@ -23,7 +23,6 @@ import numpy as np
 import pyiodaconv.ioda_conv_engines as iconv
 from pyiodaconv.orddicts import DefaultOrderedDict
 from pyiodaconv.def_jedi_utils import set_metadata_attributes, set_obspace_attributes
-from pyiodaconv.def_jedi_utils import compute_scan_angle
 from pyiodaconv.def_jedi_utils import ioda_int_type, ioda_float_type, epoch
 from pyiodaconv.def_jedi_utils import concat_obs_dict
 
